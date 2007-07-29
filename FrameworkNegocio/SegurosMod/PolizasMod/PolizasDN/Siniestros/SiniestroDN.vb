1714aa8d56ad0345347e9ef53ba450ffbe26da0d
Alejandro Cano Martin <alejandro.cano@signumsoftware.com>
2007-07-29 09:51:53 +0000

    (TFS CS 63)

--- FrameworkNegocio/SegurosMod/PolizasMod/PolizasDN/Siniestros/SiniestroDN.vb
+++ FrameworkNegocio/SegurosMod/PolizasMod/PolizasDN/Siniestros/SiniestroDN.vb
@@ -3,21 +3,45 @@ Imports FN.Empresas.DN
 
 <Serializable()> _
 Public Class SiniestroDN
-    Inherits EntidadDN
+    Inherits EntidadTemporalDN
 
 #Region "Atributos"
+    ' fi fecha de apertura
+    ' ff fecha de cierre
     Protected mFOcurrencia As Date
-
+    Protected mPeridoCobertura As FN.Seguros.Polizas.DN.PeriodoCoberturaDN
+    Protected mEstadoTramitSiniestro As EstadoTramitacionDN
+    Protected mFComunicacion As Date
 
 
 #End Region
 
 #Region "Propiedades"
 
+    ''' <summary>
+    ''' la minica fecha de comunicación de las reclamaciones 
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public ReadOnly Property FComunicacion() As Date
+
+        Get
+            Return mFComunicacion
+        End Get
 
 
+    End Property
 
+    Public Sub ActualizarEstadoTramitSiniestro()
 
+    End Sub
+    Public ReadOnly Property EstadoTramitSiniestro() As EstadoTramitacionDN
+        Get
+
+            Return mEstadoTramitSiniestro
+        End Get
+    End Property
 
     Public Property FOcurrencia() As Date
 
@@ -30,6 +54,25 @@ Public Class SiniestroDN
 
         End Set
     End Property
+
+
+
+
+
+
+    <RelacionPropCampoAtribute("mPeridoCobertura")> _
+    Public Property PeridoCobertura() As FN.Seguros.Polizas.DN.PeriodoCoberturaDN
+        Get
+            Return mPeridoCobertura
+        End Get
+        Set(ByVal value As FN.Seguros.Polizas.DN.PeriodoCoberturaDN)
+            CambiarValorRef(Of FN.Seguros.Polizas.DN.PeriodoCoberturaDN)(value, mPeridoCobertura)
+        End Set
+    End Property
+
+
+
+
 
 
 
