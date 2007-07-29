263fe36cf637b22b289cf02ab6def74abdc5b94d
Oscar Gonzalez <oscar.gonzalez@signumsoftware.com>
2007-07-29 11:55:09 +0000

    Nuevo operador multiplicador + redondeo a 2 decimales
    (TFS CS 66)

---
+++ Framework/OperacionesMod/OperacionesDN/MultRedondeoOperadorDN.vb
@@ -0,0 +1,15 @@
+
+<Serializable()> _
+Public Class MultRedondeoOperadorDN
+    Inherits Framework.DatosNegocio.EntidadDN
+    Implements IOperadorDN
+
+    Public Sub New()
+        Me.mNombre = "Multiplicar Redondeo a 2"
+    End Sub
+
+    Public Function Ejecutar(ByVal valor1 As Object, ByVal valor2 As Object) As Object Implements IOperadorDN.Ejecutar
+        Return Math.Round(valor1 * valor2, 2)
+    End Function
+
+End Class
