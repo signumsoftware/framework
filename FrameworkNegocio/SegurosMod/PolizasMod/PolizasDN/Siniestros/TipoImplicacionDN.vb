65b53b895a51290d57aa60eb550817cb33a9cb05
Alejandro Cano Martin <alejandro.cano@signumsoftware.com>
2007-07-29 09:55:34 +0000

    (TFS CS 65)

---
+++ FrameworkNegocio/SegurosMod/PolizasMod/PolizasDN/Siniestros/TipoImplicacionDN.vb
@@ -0,0 +1,58 @@
+
+
+
+
+Imports Framework.DatosNegocio
+
+
+<Serializable()> _
+Public Class TipoImplicacionDN
+    Inherits EntidadDN
+
+
+#Region "Atributos"
+
+#End Region
+
+
+#Region "Constructores"
+
+#End Region
+
+#Region "Propiedades"
+
+#End Region
+
+#Region "Metodos"
+
+#End Region
+
+
+
+End Class
+
+
+
+
+<Serializable()> _
+Public Class ColTipoImplicacionDN
+    Inherits ArrayListValidable(Of TipoImplicacionDN)
+End Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
