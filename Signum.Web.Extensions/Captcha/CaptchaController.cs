81c96267f764a405e8fa99df4b90f7fbda4646b1
Jose Manuel Perez <jmperez@signumsoftware.com>
2009-07-15 15:58:57 +0000

    (TFS CS 3059)

---
+++ Signum.Web.Extensions/Captcha/CaptchaController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Drawing;
+using System.Web;
+using System.Drawing.Imaging;
+using Signum.Web.Captcha;
+
+namespace Signum.Web.Controllers
+{
+    public class CaptchaController : Controller
+    {
+        [AcceptVerbs(HttpVerbs.Get)]
+        public void Image(string guid)
+        {
+            HttpContextBase context=ControllerContext.RequestContext.HttpContext;
+            CaptchaImage ci = CaptchaImage.GetCachedCaptcha(guid);
+
+            if (String.IsNullOrEmpty(guid) || ci == null)
+            {
+                context.Response.StatusCode = 404;
+                context.Response.StatusDescription = "Not Found";
+                context.ApplicationInstance.CompleteRequest();
+                return;
+            }
+
+            // write the image to the HTTP output stream as an array of bytes
+            using (Bitmap b = ci.RenderImage())
+            {
+                b.Save(context.Response.OutputStream, ImageFormat.Gif);
+            }
+
+            context.Response.ContentType = "image/png";
+            context.Response.StatusCode = 200;
+            context.Response.StatusDescription = "OK";
+            context.ApplicationInstance.CompleteRequest();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public PartialViewResult Refresh()
+        {
+            //this.ViewData.Add(ViewDataKeys.ResourcesRoute, ConfigurationManager.AppSettings["RutaResources"]);
+            return new PartialViewResult()
+            {
+                ViewData = this.ViewData,
+                TempData = this.TempData,
+                ViewName = "captcha"
+            };
+        }
+    }
+}
---
+++ Signum.Web.Extensions/Captcha/CaptchaHtmlHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Signum.Web;
+using Signum.Entities;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Reflection;
+using System.Text;
+using System.Configuration;
+using System.Web.Caching;
+
+namespace Signum.Web.Captcha
+{
+    public static class CaptchaButtonLineHelper
+    {
+        public static string CaptchaImage(this HtmlHelper helper, int height, int width,string route)
+        {
+            CaptchaImage image = new CaptchaImage
+            {
+                Height = height,
+                Width = width,                
+            };
+
+            HttpRuntime.Cache.Add(
+                image.UniqueId,
+                image,
+                null,
+                DateTime.Now.AddSeconds(Signum.Web.Captcha.CaptchaImage.CacheTimeOut.Value),
+                Cache.NoSlidingExpiration,
+                CacheItemPriority.NotRemovable,
+                null);
+
+            StringBuilder stringBuilder = new StringBuilder(256);
+            stringBuilder.Append("<input type=\"hidden\" name=\"captcha-guid\" value=\"");
+            stringBuilder.Append(image.UniqueId);
+            stringBuilder.Append("\" />");
+            stringBuilder.AppendLine();
+            stringBuilder.Append("<img src=\"");
+            //stringBuilder.Append(/*HttpContext.Current.Server.MapPath("~")+*/ConfigurationManager.AppSettings["RoutePrefix"] + "/View/captcha.ashx?guid=" + image.UniqueId);
+            stringBuilder.Append(route + VirtualPathUtility.ToAbsolute("~/" + "Captcha.aspx/Image") + "?guid=" + image.UniqueId);
+            stringBuilder.Append("\" alt=\"CAPTCHA\" width=\"");
+            stringBuilder.Append(width);
+            stringBuilder.Append("\" height=\"");
+            stringBuilder.Append(height);
+            stringBuilder.Append("\" />");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
