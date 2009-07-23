29e68d08f6844916aac1335f6c33db14e498f958
Antonio Herranz <antonio.herranz@signumsoftware.com>
2009-07-23 13:31:24 +0000

    (TFS CS 3138)

---
+++ Signum.Web.Extensions/Operations/OperationClient.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Signum.Engine.Operations;
+using Signum.Entities.Operations;
+using Signum.Utilities;
+
+namespace Signum.Web.Operations
+{
+    public static class OperationClient
+    {
+        public static OperationManager Manager { get; private set; }
+
+        public static void Start(OperationManager operationManager)
+        {
+            Manager = operationManager;
+
+            //ButtonBarHelper.GetButtonBarElement += Manager.ButtonBar_GetButtonBarElement;
+
+            Constructor.ConstructorManager.GeneralConstructor += Manager.ConstructorManager_GeneralConstructor;
+
+            //SearchControl.GetCustomMenuItems += (qn, type) =>
+            //{
+            //    var list = OperationLogic.ServiceGetConstructorOperationInfosnfos(type).Where(oi =>
+            //    {
+            //        ConstructorFromManySettings set = (ConstructorFromManySettings)Manager.Settings.TryGetC(oi.Key);
+            //        return set == null || set.IsVisible == null || set.IsVisible(qn, oi);
+            //    }).ToList();
+
+            //    if (list.Count == 0)
+            //        return null;
+
+            //    return new ConstructFromMenuItem { OperationInfos = list };
+            //};
+        }
+    }
+
+    public class OperationManager
+    {
+        public Dictionary<Enum, WebMenuItem> Settings = new Dictionary<Enum, WebMenuItem>();
+
+        internal object ConstructorManager_GeneralConstructor(Type type, Controller controller)
+        {
+            List<OperationInfo> list = OperationLogic.ServiceGetConstructorOperationInfos(type);
+
+            var dic = (from oi in list
+                       let os = (ConstructorSettings)Settings.TryGetC(oi.Key)
+                       where os == null || os.IsVisible == null || os.IsVisible(oi)
+                       select new { OperationInfo = oi, OperationSettings = os }).ToDictionary(a => a.OperationInfo.Key);
+
+            if (dic.Count == 0)
+                return null;
+
+            Enum selected = null;
+            if (list.Count == 1)
+            {
+                selected = dic.Keys.Single();
+            }
+            else
+            {
+                throw new ApplicationException("Not implemented: Todavía no se permiten varios constructores");
+                //ConstructorSelectorWindow sel = new ConstructorSelectorWindow();
+                //sel.ConstructorKeys = dic.Keys.ToArray();
+                //if (sel.ShowDialog() != true)
+                //    return null;
+
+                //selected = sel.SelectedKey;
+            }
+
+            var pair = dic[selected];
+
+            if (pair.OperationSettings != null && pair.OperationSettings.Constructor != null)
+                return pair.OperationSettings.Constructor(pair.OperationInfo, controller);
+            else
+                return Constructor.Construct(type, controller);
+        }   
+    }
+}
---
+++ Signum.Web.Extensions/Operations/OperationSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Signum.Entities.Operations;
+using System.Web.Mvc;
+using Signum.Entities;
+
+namespace Signum.Web.Operations
+{
+    //Constructor
+    public class ConstructorSettings : WebMenuItem
+    {
+        public Func<OperationInfo, Controller, IdentifiableEntity> Constructor { get; set; }
+        public Func<OperationInfo, bool> IsVisible { get; set; }
+    }
+
+    public class EntityOperationSettings : WebMenuItem
+    {
+        //public Func<EntityOperationEventArgs, IdentifiableEntity> Click { get; set; }
+        public Func<IdentifiableEntity, bool> IsVisible { get; set; }
+    }
+}
