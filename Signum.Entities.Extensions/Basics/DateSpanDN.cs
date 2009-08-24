8e4f67a2f291e7caafa534ed260ed0f23fbf7550
Alejandro Cano Martin <alejandro.cano@signumsoftware.com>
2009-08-24 16:28:50 +0000

    muchas cosas
    (TFS CS 3313)

---
+++ Signum.Entities.Extensions/Basics/DateSpanDN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Signum.Entities.Extensions.Basics
+{
+    [Serializable]
+    public class DateSpanDN : EmbeddedEntity
+    {
+        int year;
+        public int Year
+        {
+            get { return year; }
+            set { Set(ref year, value, "Year"); }
+        }
+
+    }
+}
