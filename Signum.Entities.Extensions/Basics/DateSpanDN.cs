ab8eaa3a1e583c67279b4ebd2c7e7b465277c3a9
Alejandro Cano Martin <alejandro.cano@signumsoftware.com>
2009-08-25 07:58:00 +0000

    xx
    (TFS CS 3315)

--- Signum.Entities.Extensions/Basics/DateSpanDN.cs
+++ Signum.Entities.Extensions/Basics/DateSpanDN.cs
@@ -8,11 +8,30 @@ namespace Signum.Entities.Extensions.Basics
     [Serializable]
     public class DateSpanDN : EmbeddedEntity
     {
-        int year;
-        public int Year
+        int years;
+        public int Years
         {
-            get { return year; }
-            set { Set(ref year, value, "Year"); }
+            get { return years; }
+            set { Set(ref years, value, "Years"); }
+        }
+
+        int months;
+        public int Months
+        {
+            get { return months; }
+            set { Set(ref months, value, "Months"); }
+        }
+
+        int days;
+        public int Days
+        {
+            get { return days; }
+            set { Set(ref days, value, "Days"); }
+        }
+
+        public DateTime Add(DateTime date)
+        {
+            return date.AddYears(years).AddMonths(months).AddDays(days);
         }
 
     }
