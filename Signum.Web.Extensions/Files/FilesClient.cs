9431c4dbb5e4b1e29b953bf65f23db408598c94f
Antonio Herranz <antonio.herranz@signumsoftware.com>
2009-10-15 17:25:31 +0000

    (TFS CS 3666)

---
+++ Signum.Web.Extensions/Files/FilesClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Signum.Engine.Operations;
+using Signum.Entities.Operations;
+using Signum.Utilities;
+using Signum.Entities;
+using System.Web;
+using Signum.Entities.Basics;
+using System.Reflection;
+using Signum.Entities.Files;
+
+namespace Signum.Web.Files
+{
+    public static class FilesClient
+    {
+        public static string ViewPrefix = "~/Plugin/Signum.Web.Extensions.dll/Signum.Web.Extensions.Files.";
+
+        public static void Start()
+        {
+            if (Navigator.Manager.NotDefined(MethodInfo.GetCurrentMethod()))
+            {
+                Navigator.Manager.EntitySettings.Add(typeof(FilePathDN), new EntitySettings(false) { PartialViewName = ViewPrefix + "FilePathIU.ascx" }); 
+            }
+        }
+    }
+}
