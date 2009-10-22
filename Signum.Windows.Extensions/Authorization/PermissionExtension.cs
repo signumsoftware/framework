3189760699496655c71387a8dd5ed02bead93fb8
Jose Manuel Perez <jmperez@signumsoftware.com>
2009-10-22 14:50:37 +0000

    (TFS CS 3725)

---
+++ Signum.Windows.Extensions/Authorization/PermissionExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+using Signum.Services;
+using System.Windows;
+
+namespace Signum.Windows.Authorization
+{
+    [MarkupExtensionReturnType(typeof(bool))]
+    public class PermissionExtension : MarkupExtension
+    {
+        Enum permission;
+        public PermissionExtension(Enum value)
+        {
+            this.permission = permission;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Server.Service<IPermissionAuthServer>().IsAuthorizedFor(permission);
+        }
+    }
+
+    [MarkupExtensionReturnType(typeof(Visibility))]
+    public class PermissionVisiblityExtension : MarkupExtension
+    {
+        Enum permission;
+        public PermissionVisiblityExtension(Enum value)
+        {
+            this.permission = permission;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Server.Service<IPermissionAuthServer>().IsAuthorizedFor(permission) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
