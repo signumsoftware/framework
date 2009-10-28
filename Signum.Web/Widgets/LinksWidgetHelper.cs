fa750e0f3491500d9528b184ed93ffa0a6e54635
Antonio Herranz <antonio.herranz@signumsoftware.com>
2009-10-28 11:23:30 +0000

    (TFS CS 3796)

---
+++ Signum.Web/Widgets/LinksWidgetHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Signum.Web
+{
+    public delegate List<QuickLink> GetLinksDelegate(HtmlHelper helper, object entity, string partialViewName);
+
+    public static class LinksWidgetHelper
+    {
+        public static event GetLinksDelegate GetLinks; 
+
+        public static void Start()
+        {
+            WidgetsHelper.GetWidgetsForView += (helper, entity, partialViewName) => WidgetsHelper_GetWidgetsForView(helper, entity, partialViewName);
+        }
+
+        private static List<IWidget> WidgetsHelper_GetWidgetsForView(HtmlHelper helper, object entity, string partialViewName)
+        {
+            return null;
+            //List<QuickLink> links = new List<QuickLink>();
+            //if (GetLinks != null)
+            //    links.AddRange(GetLinks.GetInvocationList()
+            //        .Cast<GetButtonBarElementDelegate>()
+            //        .Select(d => d(helper, entity, mainControlUrl))
+            //        .NotNull().SelectMany(d => d).ToList());
+
+            //return ListMenuItemsToString(helper, links, prefix);  
+        }
+    }
+
+    public class QuickLink
+    {
+        public QuickLink(string label)
+        {
+            this.Label = label;
+        }
+
+        /// <summary>
+        /// Display name of the item
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Action to be executed on the mouseDoubleClick of the item
+        /// </summary>
+        public Action Action { get; set; }
+    }
+}
---
+++ Signum.Web/Widgets/WidgetsHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web;
+using Signum.Utilities;
+
+namespace Signum.Web
+{
+    public delegate List<IWidget> GetWidgetDelegate(HtmlHelper helper, object entity, string partialViewName);
+
+    public interface IWidget
+    {
+        event Action ForceShow;
+    }
+
+    public static class WidgetsHelper
+    {
+        public static event GetWidgetDelegate GetWidgetsForView;
+
+        public static string GetWidgetsForViewName(this HtmlHelper helper, object entity, string partialViewName)
+        {
+            List<IWidget> widgets = new List<IWidget>();
+            if (GetWidgetsForView != null)
+                widgets.AddRange(GetWidgetsForView.GetInvocationList()
+                    .Cast<GetWidgetDelegate>()
+                    .Select(d => d(helper, entity, partialViewName))
+                    .NotNull().SelectMany(d => d).ToList());
+
+            return WidgetsToString(helper, widgets);
+        }
+
+        private static string WidgetsToString(HtmlHelper helper, List<IWidget> widgets)
+        {
+            if (widgets == null || widgets.Count == 0)
+                return "";
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (IWidget widget in widgets)
+            { 
+                
+            }
+
+            return sb.ToString();
+        }
+    }
+}
