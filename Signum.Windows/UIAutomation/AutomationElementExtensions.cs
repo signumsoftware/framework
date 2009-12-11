0dc7b0c007a2b8e22e086d5b0f81932260e263d8
Jose Manuel Perez <jmperez@signumsoftware.com>
2009-12-11 16:52:16 +0000

    (TFS CS 4168)

---
+++ Signum.Windows/UIAutomation/AutomationElementExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Signum.Windows.UIAutomation
+{
+ /*   public static class AutomationElementExtensions
+    {
+        public static T GetCurrentPattern<T>(this AutomationElement element)
+            where T : BasePattern
+        {
+            return element.GetCurrentPattern(typeof(T));
+        }
+    }*/
+}
