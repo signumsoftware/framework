50676078b56876708b8f4ce7b52abf5509429b18
Luis Delgado Villanueva <luis.delgado@signumsoftware.com>
2009-12-17 15:51:16 +0000

    (TFS CS 4192)

---
+++ Signum.Utilities/FileExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Signum.Utilities
+{
+    class FileExtensions
+    {
+        private static string AvailableFileName(string fileName)
+        {
+            if (!File.Exists(fileName))
+                return fileName;
+            string path = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName));
+            string extension = Path.GetExtension(fileName);
+            for (int i = 1; ; i++)
+            {
+                string fullPath = "{0}({1}){2}".Formato(path, i, extension);
+                if (!File.Exists(fullPath))
+                    return fullPath;
+            }
+        }
+    }
+}
