df0293342db09c730bc72282751bf39112eb3cde
Antonio Herranz <antonio.herranz@signumsoftware.com>
2010-01-11 11:51:37 +0000

    (TFS CS 4286)

--- Signum.Web.Extensions/Files/FileController.cs
+++ Signum.Web.Extensions/Files/FileController.cs
@@ -56,7 +56,7 @@ namespace Signum.Web.Files
                 this.ViewData[ViewDataKeys.WriteSFInfo] = true;
 
                 Type ts = typeof(TypeSubContext<>).MakeGenericType(new Type[] { entity.GetType() });
-                TypeContext tc = (TypeContext)Activator.CreateInstance(ts, new object[] { entity, Modification.GetTCforEmbeddedEntity(Request.Form, entity, ref prefix), new PropertyInfo[] { } });
+                TypeContext tc = (TypeContext)Activator.CreateInstance(ts, new object[] { entity, Modification.GetTCforEmbeddedEntity(this, Request.Form, entity, ref prefix), new PropertyInfo[] { } });
 
                 return Navigator.PartialView(this, tc, "", sfUrl); //No prefix as its info is in the TypeContext
             }
