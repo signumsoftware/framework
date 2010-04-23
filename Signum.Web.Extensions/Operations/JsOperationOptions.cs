c91a615f98312642c5b7b258bcadf5a3d0c3619b
Alejandro Cano Martin <alejandro.cano@signumsoftware.com>
2010-04-23 09:52:17 +0000

    solicitar alta
    (TFS CS 5139)

--- Signum.Web.Extensions/Operations/JsOperationOptions.cs
+++ Signum.Web.Extensions/Operations/JsOperationOptions.cs
@@ -35,13 +35,12 @@ namespace Signum.Web.Operations
                 //if (AvoidDefaultOk && OnOk.HasText())
                 //    throw new ArgumentException("JsOperationOptions cannot have both AvoidDefaultOk and OnOk specified");
 
-                return new JsOptionsBuilder(false)
+                var builder = new JsOptionsBuilder(false)
                 {
                     {"prefix", Prefix.TryCC(a=>a.ToJS())},
                     {"operationKey", OperationKey.TryCC(a=>a.ToJS())},
                     {"isLite", IsLite.TryCC(a=>a.ToJS())},
                     {"returnType",ReturnType.TryCC(a=>a.ToJS())},
-                    {"controllerUrl", ControllerUrl.TryCC(a=>a.ToJS())},
                     {"requestExtraJsonData", RequestExtraJsonData.TryCC(a=>a.ToJS())},
                     //{"validationControllerUrl", ValidationControllerUrl.TrySingleQuote()},
                     //{"avoidValidation", AvoidValidation == true? "true": null},
@@ -54,7 +53,12 @@ namespace Signum.Web.Operations
                     //{"closePopupOnSuccess", ClosePopupOnSuccess== true? "true": null},
                     //{"confirmMsg", ConfirmMessage.TrySingleQuote() },
                     
-                }.ToJS();
+                };
+
+                if (ControllerUrl.ToJS() != "null")
+                    builder.Add("controllerUrl", ControllerUrl.ToJS());
+                
+                return builder.ToJS();
             };
         }
     }
