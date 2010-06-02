b849cc3b215c30cf4218078d3c0195910759695d
Jose Manuel Perez <jmperez@signumsoftware.com>
2010-06-02 16:26:00 +0000

    (TFS CS 5640)

---
+++ Signum.Web.Sample/Scripts/main.js
@@ -0,0 +1,12 @@
+﻿$(function() {
+    $.address.change(function(event) {
+        if (event.value != "/") {
+            $('#content').load($("base").attr("href") + event.value + " #content");
+        }
+    });
+
+    $('body').delegate("a", "click", function() {
+        $.address.value($(this).attr('href'));
+        return false;
+    });
+});
