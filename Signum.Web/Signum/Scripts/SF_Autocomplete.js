7eb79596e4ca50bed6da05890dc75d69ddbb5b3e
Jose Manuel Perez <jmperez@signumsoftware.com>
2010-06-14 10:16:16 +0000

    (TFS CS 5704)

--- Signum.Web/Signum/Scripts/SF_Autocomplete.js
+++ Signum.Web/Signum/Scripts/SF_Autocomplete.js
@@ -35,8 +35,9 @@ Autocompleter = function(controlId, url, _options) {
     self.currentInput = undefined;
     self.resultClass = "ddlAuto";
     self.resultSelectedClass = "ddlAutoOn";
-    self.$resultDiv = $("<div/>").addClass(self.resultClass);
     self.create();
+
+    self.extraObj = undefined;  //data object passed to extra div when clicked
 };
 
 Autocompleter.prototype = {
@@ -48,13 +49,12 @@ Autocompleter.prototype = {
                 self.clear(e.which ? e.which : e.keyCode);
             },
             keydown: function(e) {
-                self.keydown(e);
+                return self.keydown(e);
             },
-            click: function(e) {
-                if (e.preventDefault) e.preventDefault();
-                if (e.stopPropagation) e.stopPropagation();
+            click: function() {
+                return false;
             },
-            focusin: function(e) {
+            focusin: function() {
                 if (self.currentResults.length) {
                     self.$dd.show();
                 }
@@ -115,6 +115,7 @@ Autocompleter.prototype = {
         self.request = $.getJSON(
             self.url, data,
             function(results) {
+                self.request = undefined;
                 if (results) {
                     self.showResults(results, input);
                 }
@@ -124,56 +125,32 @@ Autocompleter.prototype = {
     showResults: function(results, input) {
         var self = this;
         self.currentInput = input;
-        self.request = undefined;
         self.currentText = self.$control.val();
 
         var prevCount = self.currentResults.length;
-        var newCount = results.length;
         if (prevCount == 0) self.$dd.hide();
-        var $divsCurrentResults = self.$dd.children("." + self.resultClass);
 
-        var i = 0;
-        for (var l = results.length; i < l; i++) {
-            if (i < prevCount) {
-                $divsCurrentResults.eq(i).html(self.process(input, results[i])).data("data", results[i]);
-            }
-            else {
-                var $rD = self.$resultDiv.clone();
-                $rD.append(self.process(input, results[i])).data("data", results[i]);
-                self.$dd.append($rD);
-            }
+        var content = "";
+        for (var i = 0, l = results.length; i < l; i++) {
+            content += "<div class=\"" + self.resultClass + "\">" + self.process(input, results[i]) + "</div>";
         }
 
-        var j;
-        for (j = i; j < prevCount; j++) {
-            $divsCurrentResults.eq(j).remove();
-        }
+        self.$dd[0].innerHTML = content;
 
         self.currentResults = results;
         if (self.options.cacheResults)
             self.cacheResults[input.toLowerCase()] = results;
 
-        var $previousExtra = self.$dd.find(".extra");
-
         //add extra result
         if (self.options.extraResult != null) {
-            var $rD = self.$resultDiv.clone();
             var obj = {};
             obj.input = input;
-            obj.results = newCount;
+            obj.results = results.length;
 
-            var $rD = self.$resultDiv.clone()
-                            .addClass("extra")
-                            .data("data", obj)
-                            .click(function() {
-                                if (self.options.extraResultClick != null)
-                                    self.options.extraResultClick(obj);
-                            });
-
-            $rD.append(self.options.extraResult(obj));
-            if ($previousExtra.length) $previousExtra.remove();
-            self.$dd.append($rD);
-
+            var extraDiv = "<div class=\"" + self.resultClass + " " + " extra\">" + self.options.extraResult(obj) + "</div>";
+            self.extraObj = obj;
+            
+            self.$dd[0].innerHTML += extraDiv;
         }
 
         var offset = self.$control.position();
@@ -185,26 +162,25 @@ Autocompleter.prototype = {
 
         self.$control.removeClass('loading');
 
-        if (prevCount == 0 && !$previousExtra.length)
+        if (prevCount == 0)
             self.$dd.slideDown("fast");
         else
             self.$dd.show();
-
     },
+    
     keydown: function(e) {
         var key = e.which ? e.which : e.keyCode;
         if (key == 13 || key == 9) {    //enter or tab
             var selectedOption = this.$dd.find("." + this.resultSelectedClass);
             if (selectedOption.length > 0) {
                 if (selectedOption.hasClass("extra")) selectedOption.click();
-                else this.onOk(selectedOption.data("data"));
+                else this.onOk(selectedOption.index());
             }
             if (key == 9) {
                 this.$dd.hide();
             }
             if (key == 13) {    //no bubble for enter
-                if (e.preventDefault) e.preventDefault();
-                if (e.stopPropagation) e.stopPropagation();
+                return false;
             }
             return;
         }
@@ -241,7 +217,10 @@ Autocompleter.prototype = {
         var target = e.srcElement || e.target;
         if (target != null) {
             if (!$(target).closest(".extra").length)
-                this.onOk($(target).closest("." + this.resultSelectedClass).data("data"));
+                this.onOk($(target).closest("." + this.resultSelectedClass).index());
+            else
+                if (this.options.extraResultClick != null)
+                this.options.extraResultClick(this.extraObj);
             this.$dd.hide();
         }
     },
@@ -263,8 +242,10 @@ Autocompleter.prototype = {
         }
         return s;
     },
-    onOk: function(data) {
+    onOk: function(index) {
         this.$dd.hide();
+
+        var data = this.currentResults[index];
         this.$control.val(data[this.options.textField]);
 
         if (this.options.onSuccess != null) {
