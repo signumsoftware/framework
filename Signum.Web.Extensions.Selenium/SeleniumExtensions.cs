87bf96b05d6c2dd466951a0418babe5bf2fe08b7
Antonio Herranz <antonio.herranz@signumsoftware.com>
2011-02-08 15:23:33 +0000

    (TFS CS 7029)

--- Signum.Web.Extensions.Selenium/SeleniumExtensions.cs
+++ Signum.Web.Extensions.Selenium/SeleniumExtensions.cs
@@ -19,7 +19,7 @@ namespace Signum.Web.Selenium
 
     public static class SeleniumExtensions
     {
-        public static WebExplorer Explorer = WebExplorer.Chrome;
+        public static WebExplorer Explorer = WebExplorer.Firefox;
 
         public static Process LaunchSeleniumProcess()
         {
