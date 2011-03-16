e01e8f5733e3b73520c45a205cf6b608112150ca
Alejandro Cano Martin <alejandro.cano@signumsoftware.com>
2011-03-16 19:58:55 +0000

    muchas cosas pendientes dificl de especificar
    (TFS CS 7253)

--- Signum.Windows/Fluent/ControlExtensions.cs
+++ Signum.Windows/Fluent/ControlExtensions.cs
@@ -19,18 +19,6 @@ namespace Signum.Windows
             return DependencyPropertyHelper.GetValueSource(depObj, prop).BaseValueSource != BaseValueSource.Local;
         }
 
-        public static T Set<T>(this T depObj, DependencyProperty prop, object value) where T : DependencyObject
-        {
-            depObj.SetValue(prop, value);
-            return depObj;
-        }
-
-        public static T Handle<T>(this T uiElement, RoutedEvent routedEvent, RoutedEventHandler handler) where T : UIElement
-        {
-            uiElement.AddHandler(routedEvent, handler);
-            return uiElement; 
-        }
-
         public static Visibility ToVisibility(this bool val)
         {
             return val ? Visibility.Visible : Visibility.Collapsed;
@@ -46,36 +34,5 @@ namespace Signum.Windows
             tabControl.Items.Add(new TabItem { Header = header, Content = content });
             return tabControl;
         }
-
-        public static T Hide<T>(this T uiElement) where T : UIElement
-        {
-            uiElement.Visibility = Visibility.Hidden;
-            return uiElement;
-        }
-
-        public static T Collapse<T>(this T uiElement) where T : UIElement
-        {
-            uiElement.Visibility = Visibility.Collapsed;
-            return uiElement;
-        }
-
-        public static T Visible<T>(this T uiElement) where T : UIElement
-        {
-            uiElement.Visibility = Visibility.Collapsed;
-            return uiElement;
-        }
-
-        public static T ReadOnly<T>(this T uiElement) where T : UIElement
-        {
-            Common.SetIsReadOnly(uiElement, true);
-            return uiElement;
-        }
-
-        public static T Editable<T>(this T uiElement) where T : UIElement
-        {
-            Common.SetIsReadOnly(uiElement, false); 
-            return uiElement;
-        }
-
     }
 }
