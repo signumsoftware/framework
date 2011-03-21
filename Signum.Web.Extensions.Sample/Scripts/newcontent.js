a35234385efbb165f48ce5df4b45a59aa4f23add
Antonio Herranz <antonio.herranz@signumsoftware.com>
2011-03-21 08:00:41 +0000

    NewContent en local refactorizado a SF_Utils.js
    (TFS CS 7341)

--- Signum.Web.Extensions.Sample/Scripts/newcontent.js
+++ Signum.Web.Extensions.Sample/Scripts/newcontent.js
@@ -1,39 +1,11 @@
 ﻿$(function () {
     $("body").bind("sf-new-content", function (e) {
         var $newContent = $(e.target);
-        //buttons
-        $newContent.find(".sf-entity-button, .sf-query-button, .sf-line-button, .sf-chooser-button, .sf-button").each(function (i, val) {
-            var $txt = $(val);
-            var data = $txt.data();
-            $txt.button({ text: (!("text" in data) || SF.isTrue(data.text)), icons: { primary: data.icon, secondary: data["icon-secondary"]} });
-        });
 
-        //datepicker
-        $newContent.find(".sf-datepicker").each(function (i, val) {
-            var $txt = $(val);
-            $txt.datepicker(jQuery.extend({}, SF.Locale.defaultDatepickerOptions, { dateFormat: $txt.attr("data-format") }));
-        });
-
-        //dropdown
-        $newContent.find(".sf-dropdown .sf-menu-button")
-            .addClass("ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all")
-            .find("li")
-            .addClass("ui-menu-item")
-            .find("a")
-            .addClass("ui-corner-all");
-
-        //autocomplete
-        $newContent.find(".sf-entity-autocomplete").each(function (i, val) {
-            var $txt = $(val);
-            var data = $txt.data();
-            SF.entityAutocomplete($txt, { delay: 200, types: data.types, url: data.url, count: 5 });
-        });
-
-        //input placeholder
-        $newContent.find('input[placeholder], textarea[placeholder]').placeholder();
-
-        //tabs
-        $newContent.find(".sf-tabs").tabs();
+        SF.NewContentProcessor.defaultButtons($newContent);
+        SF.NewContentProcessor.defaultDatepicker($newContent);
+        SF.NewContentProcessor.defaultDropdown($newContent);
+        SF.NewContentProcessor.defaultAutocomplete($newContent);
     });
 
     $("body").trigger("sf-new-content");
