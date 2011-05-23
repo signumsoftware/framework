290f1e42d2326d106344d13e40e5b56d83049d73
Luis Delgado <luis.delgado@signumsoftware.com>
2011-05-23 17:41:32 +0200

    sms logic and operations

--- Signum.Engine.Extensions/SMS/SMSLogic.cs
+++ Signum.Engine.Extensions/SMS/SMSLogic.cs
@@ -10,6 +10,7 @@ using Signum.Engine.Extensions.Properties;
 using Signum.Entities;
 using Signum.Utilities;
 using Signum.Utilities.Reflection;
+using Signum.Engine.Operations;
 
 namespace Signum.Engine.SMS
 {
@@ -17,13 +18,14 @@ namespace Signum.Engine.SMS
     {
         static Func<SMSMessageDN, string> SMSSendAction;
         static Func<SMSTemplateDN, List<string>, List<string>> SMSMultipleSendAction;
+        static Func<SMSMessageDN, SendState> SMSUpdateStatusAction;
 
         public static void AssertStarted(SchemaBuilder sb)
         {
-            sb.AssertDefined(ReflectionTools.GetMethodInfo(() => Start(null, null)));
+            sb.AssertDefined(ReflectionTools.GetMethodInfo(() => Start(null, null, false)));
         }
 
-        public static void Start(SchemaBuilder sb, DynamicQueryManager dqm)
+        public static void Start(SchemaBuilder sb, DynamicQueryManager dqm, bool registerGraph)
         {
             if (sb.NotDefined(MethodInfo.GetCurrentMethod()))
             {
@@ -56,7 +58,11 @@ namespace Signum.Engine.SMS
                                                   t.EndDate,
                                               }).ToDynamic();
 
-                SMSTemplateGraph.Register();
+                if (registerGraph)
+                {
+                    SMSMessageGraph.Register();
+                    SMSTemplateGraph.Register();
+                }
             }
         }
 
@@ -70,6 +76,11 @@ namespace Signum.Engine.SMS
             SMSMultipleSendAction = action;
         }
 
+        public static void RegisterSMSUpdateStatusAction(Func<SMSMessageDN, SendState> action)
+        {
+            SMSUpdateStatusAction = action;
+        }
+
         public static void SendSMS(SMSMessageDN message)
         {
             if (SMSSendAction == null)
@@ -78,10 +89,10 @@ namespace Signum.Engine.SMS
         }
 
         //Allows concurrent custom sendProviders for one application
-        public static void SendSMS(SMSMessageDN message, Func<SMSMessageDN, string> send) 
+        public static void SendSMS(SMSMessageDN message, Func<SMSMessageDN, string> send)
         {
             message.MessageID = send(message);
-            message.SendDate = DateTime.Now;
+            message.SendDate = DateTime.Now.TrimToSeconds();
             message.State = SMSMessageState.Sent;
             message.Save();
         }
@@ -93,11 +104,11 @@ namespace Signum.Engine.SMS
 
         //Allows concurrent custom sendProviders for one application
         public static List<SMSMessageDN> CreateAndSendMultipleSMSMessages(SMSTemplateDN template,
-            List<string> phones, Func<SMSTemplateDN, List<string>, List<string>> send) 
+            List<string> phones, Func<SMSTemplateDN, List<string>, List<string>> send)
         {
             var messages = new List<SMSMessageDN>();
             var IDs = send(template, phones);
-            var sendDate = DateTime.Now;
+            var sendDate = DateTime.Now.TrimToSeconds();
             for (int i = 0; i < phones.Count; i++)
             {
                 var message = template.CreateSMSMessage();
@@ -112,5 +123,49 @@ namespace Signum.Engine.SMS
             return messages;
         }
 
+        public static void UpdateMessageStatus(SMSMessageDN message)
+        {
+            if (SMSUpdateStatusAction == null)
+                throw new InvalidOperationException("SMSUpdateStatusAction was not established");
+            UpdateMessageStatus(message, SMSUpdateStatusAction);
+        }
+
+        //Allows concurrent custom updateStatusProviders for one application
+        public static void UpdateMessageStatus(SMSMessageDN message, Func<SMSMessageDN, SendState> updateAction)
+        {
+            message.SendState = updateAction(message);
+        }
+
+    }
+
+    public class SMSMessageGraph : Graph<SMSMessageDN, SMSMessageState>
+    {
+        public static void Register()
+        {
+            GetState = m => m.State;
+
+            new ConstructFrom<SMSTemplateDN>(SMSMessageOperations.Create, SMSMessageState.Created)
+            {
+                CanConstruct = t => !t.Active ? "The template must be Active to allow constructing SMS messages" : null,
+                Construct = (t, args) =>
+                {
+                    var message = t.CreateSMSMessage();
+                    message.DestinationNumber = args.GetArg<string>(0);
+                    return t.CreateSMSMessage();
+                }
+            }.Register();
+
+            new Goto(SMSMessageOperations.Send, SMSMessageState.Sent)
+            {
+                FromStates = new[] { SMSMessageState.Created },
+                Execute = (t, args) => { SMSLogic.SendSMS(t, args.TryGetArgC<Func<SMSMessageDN, string>>(0)); }
+            }.Register();
+
+            new Goto(SMSMessageOperations.UpdateStatus, SMSMessageState.Sent)
+            {
+                FromStates = new[] { SMSMessageState.Sent },
+                Execute = (t, _) => { SMSLogic.UpdateMessageStatus(t, _.TryGetArgC<Func<SMSMessageDN, SendState>>(0)); }
+            }.Register();
+        }
     }
 }
