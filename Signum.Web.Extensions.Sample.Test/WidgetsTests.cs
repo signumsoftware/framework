19384e5431d854d24a90a313c7657a4db310a247
Antonio Herranz <antonio.herranz@signumsoftware.com>
2011-06-24 17:29:06 +0200

    QuickLinks ui tests

---
+++ Signum.Web.Extensions.Sample.Test/WidgetsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Signum.Web.Selenium;
+using Selenium;
+using System.Diagnostics;
+using Signum.Engine;
+using Signum.Entities.Authorization;
+using Signum.Test;
+using Signum.Web.Extensions.Sample.Test.Properties;
+using Signum.Engine.Maps;
+using Signum.Engine.Authorization;
+using Signum.Utilities;
+
+namespace Signum.Web.Extensions.Sample.Test
+{
+    [TestClass]
+    public class WidgetsTests : Common
+    {
+        public WidgetsTests()
+        {
+
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Common.Start(testContext);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Common.MyTestCleanup();
+        }
+
+        [TestMethod]
+        public void Widgets001_QuickLinkFind()
+        {
+            CheckLoginAndOpen(ViewRoute("Label", 1));
+
+            selenium.QuickLinkClick(1);
+            
+            string popupPrefix = "New_";
+            selenium.WaitAjaxFinished(() => selenium.IsElementPresent(SeleniumExtensions.PopupSelector(popupPrefix)));
+            selenium.WaitAjaxFinished(() => selenium.IsElementPresent(SearchTestExtensions.RowSelector(selenium, 1, popupPrefix)));
+        }
+    }
+}
---
+++ Signum.Web.Extensions.Selenium/WidgetsTestExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Signum.Utilities;
+using Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Signum.Web.Selenium
+{
+    public static class WidgetsTestExtensions
+    {
+        public static string WidgetContainerSelector(string prefix)
+        {
+            if (prefix.HasText())
+            {
+                throw new NotImplementedException("WidgetContainerSelector not implemented for popups");
+            }
+            else
+                return "jq=#divNormalControl .sf-widgets-container";
+        }
+
+        public static void QuickLinkClick(this ISelenium selenium, int quickLinkIndexBase1)
+        {
+            QuickLinkClick(selenium, quickLinkIndexBase1, "");
+        }
+
+        public static void QuickLinkClick(this ISelenium selenium, int quickLinkIndexBase1, string prefix)
+        {
+            selenium.MouseOver("{0} .sf-quicklink-toggler".Formato(WidgetContainerSelector(prefix)));
+            selenium.Click("{0} .sf-quicklinks > .sf-quicklink:nth-child({1}) > a".Formato(WidgetContainerSelector(prefix), quickLinkIndexBase1));
+        }
+    }
+}
