1b980abc6a5cbc4204af417e3449604f1aba1f78
Alejandro Cebollada <alejandro.cebollada@signumsoftware.com>
2011-08-02 10:58:55 +0200

    Fix margin scheduler

--- Signum.Engine/Schema/Schema.Basics.cs
+++ Signum.Engine/Schema/Schema.Basics.cs
@@ -769,8 +769,8 @@ namespace Signum.Engine.Maps
             Columns = Fields.Values.SelectMany(c => c.Field.Columns()).ToDictionary(c => c.Name);
 
             inserter = new Lazy<InsertCache>(InitializeInsert, LazyThreadSafetyMode.PublicationOnly);
-            updater = new Lazy<UpdateCache>(InitializeUpdate, LazyThreadSafetyMode.None);
-            saveCollections = new Lazy<Action<IdentifiableEntity,Forbidden,bool>>(InitializeCollections, LazyThreadSafetyMode.None);
+            updater = new Lazy<UpdateCache>(InitializeUpdate, LazyThreadSafetyMode.PublicationOnly);
+            saveCollections = new Lazy<Action<IdentifiableEntity,Forbidden,bool>>(InitializeCollections, LazyThreadSafetyMode.PublicationOnly);
         }
 
         public Field GetField(MemberInfo value, bool throws)
