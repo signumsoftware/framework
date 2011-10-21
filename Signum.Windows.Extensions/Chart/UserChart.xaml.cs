3500a91bbe4e103d965811dec1b5371274e75522
Antonio Herranz <antonio.herranz@signumsoftware.com>
2011-10-21 19:08:09 +0200

    Windows.Chart - UserCharts fix

--- Signum.Windows.Extensions/Chart/UserChart.xaml.cs
+++ Signum.Windows.Extensions/Chart/UserChart.xaml.cs
@@ -24,17 +24,23 @@ namespace Signum.Windows.Chart
     /// </summary>
     public partial class UserChart : UserControl
     {
-        public UserChart()
-        {
-            InitializeComponent();
-        }
-
         public static readonly DependencyProperty QueryDescriptionProperty =
-            DependencyProperty.Register("QueryDescription", typeof(QueryDescription), typeof(UserChart), new UIPropertyMetadata(null));
+           DependencyProperty.Register("QueryDescription", typeof(QueryDescription), typeof(UserChart), new UIPropertyMetadata(null));
         public QueryDescription QueryDescription
         {
             get { return (QueryDescription)GetValue(QueryDescriptionProperty); }
             set { SetValue(QueryDescriptionProperty, value); }
+        }
+
+        public UserChart()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(UserChart_Loaded);
+        }
+
+        void UserChart_Loaded(object sender, RoutedEventArgs e)
+        {
+            chartBuilder.Description = QueryDescription;
         }
 
         private QueryToken[] QueryTokenBuilderFilter_SubTokensEvent(QueryToken token)
