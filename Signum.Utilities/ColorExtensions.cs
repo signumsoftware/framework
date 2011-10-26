1064d898f162f1a1160faf4292151eba2282393f
Olmo del Corral <olmo@signumsoftware.com>
2011-10-26 13:29:25 +0200

    Add Color.Interpolate in utilities

---
+++ Signum.Utilities/ColorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Signum.Utilities
+{
+    public static class ColorExtensions
+    {
+        public static Color Interpolate(this Color from, float ratio, Color to)
+        {
+            var ratioNeg = 1 - ratio;
+
+            return Color.FromArgb(
+                (int)(from.A * ratioNeg + to.A * ratio),
+                (int)(from.R * ratioNeg + to.R * ratio),
+                (int)(from.G * ratioNeg + to.G * ratio),
+                (int)(from.B * ratioNeg + to.B * ratio));
+        }
+    }
+}
