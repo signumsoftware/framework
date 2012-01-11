90a6e206175d24e002cbb9ee78c438358fefbabb
Jesus <jesus.frances@signumsoftware.com>
2012-01-11 13:54:10 +0100

    Fixes Windows

--- Signum.Windows.Extensions/SMS/SMSMessage.xaml.cs
+++ Signum.Windows.Extensions/SMS/SMSMessage.xaml.cs
@@ -15,7 +15,7 @@ using Signum.Windows;
 using Signum.Entities;
 using Signum.Entities.SMS;
 
-namespace Signum.Windows.SMS
+namespace Signum.Windows.Extensions.SMS
 {
     /// <summary>
     /// Interaction logic for SMSMessage.xaml
@@ -25,52 +25,6 @@ namespace Signum.Windows.SMS
         public SMSMessage()
         {
             InitializeComponent();
-            foreach (var ch in LogicalTreeHelper.GetChildren(textMessage))
-            {
-                if (ch is TextBox)
-                {
-                    ((TextBox)ch).TextChanged += new TextChangedEventHandler(SMSMessage_TextChanged);
-                    text = (TextBox)ch;
-                    break;
-                }
-            }
-            BrushConverter bc = new BrushConverter();
-            greenBrush = (Brush)bc.ConvertFromString("Green");
-            redBrush = (Brush)bc.ConvertFromString("Red");
-            pinkBackground = new LinearGradientBrush(new GradientStopCollection 
-            { 
-                new GradientStop(Color.FromRgb(255, 237, 237), 0),
-                new GradientStop(Color.FromRgb(255, 231, 231), 0.527),
-                new GradientStop(Color.FromRgb(255, 204, 204), 1)
-            }, new Point(0.5, 1), new Point(0.5, 0));
-            normalBackGround = text.Background;
-        }
-
-        private void SMSMessage_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            VisualCharactersToEnd();
-        }
-
-        LinearGradientBrush pinkBackground = null;
-        TextBox text = null;
-        Brush greenBrush = null;
-        Brush redBrush = null;
-        Brush normalBackGround = null;
-
-        private void VisualCharactersToEnd()
-        {
-            int chLeft = SMSCharacters.RemainingLength(text.Text);
-            charactersLeft.Text = chLeft.ToString();
-            if (chLeft < 0)
-            {
-                charactersLeft.Foreground = redBrush;
-                charactersLeft.Background = pinkBackground;
-            }
-            else
-            {
-                charactersLeft.Foreground = greenBrush;
-                charactersLeft.Background = normalBackGround;
-            }
         }
     }
 }
