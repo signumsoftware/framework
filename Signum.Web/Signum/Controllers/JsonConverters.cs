9e2b9ca0160bc86b61fd0ff56ae81924158c856c
Antonio Herranz <antonio.herranz@signumsoftware.com>
2012-03-22 17:08:08 +0100

    EnumJavaScriptConverter

--- Signum.Web/Signum/Controllers/JsonConverters.cs
+++ Signum.Web/Signum/Controllers/JsonConverters.cs
@@ -45,4 +45,30 @@ namespace Signum.Web
             return result;
         }
     }
+
+    public class EnumJavaScriptConverter : JavaScriptConverter
+    {
+        public override IEnumerable<Type> SupportedTypes
+        {
+            get { return new[] { typeof(Enum) }; }
+        }
+
+        public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
+        {
+            var result = new Dictionary<string, object>();
+
+            Enum myEnum = obj as Enum;
+            if (myEnum != null)
+            {
+                result["Value"] = myEnum.ToString();
+            }
+
+            return result;
+        }
+    }
 }
