c5c762c11c04c5e7e7f51c2b6c6fbd3981ea4fb3
Olmo del Corral <olmo@signumsoftware.com>
2012-04-01 23:51:00 +0200

    add Sigum.Extensions.Sample.Test

---
+++ Signum.Windows.Extensions.Sample.Test/Common.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Signum.Windows.UIAutomation;
+using System.Diagnostics;
+using System.Windows.Automation;
+
+namespace Signum.Windows.Extensions.Sample.Test
+{
+    class Common
+    {
+        public static WindowProxy StartAndLogin()
+        {
+            return StartAndLogin("su", "su");
+        }
+
+        public static WindowProxy StartAndLogin(string userName, string password)
+        {
+            using (WindowProxy login = new WindowProxy(FindOrStartWindow()))
+            {
+                login.Owner.ChildById("tbUserName").Pattern<ValuePattern>().SetValue(userName);
+                login.Owner.ChildById("tbPassword").Pattern<ValuePattern>().SetValue(password);
+
+                login.Owner.ChildById("btLogin").Pattern<InvokePattern>().Invoke();
+            }
+
+            return new WindowProxy(AutomationElement.RootElement.WaitChild(10000, a => a.Current.Name == "Music Database"));
+        }
+
+        private static AutomationElement FindOrStartWindow()
+        {
+            var win = AutomationElement.RootElement.TryChild(a => a.Current.Name == "Login on Music Database");
+
+            if (win != null)
+                return win;
+
+            Process p = Process.Start(@"D:\Signum\Extensions\Signum.Windows.Extensions.Sample\bin\Debug\Signum.Windows.Extensions.Sample.exe");
+
+            p.WaitForInputIdle();
+
+            return AutomationElement.RootElement.WaitChild(5000, a => a.Current.ProcessId == p.Id);
+        }
+    }
+}
---
+++ Signum.Windows.Extensions.Sample.Test/SearchWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using Signum.Windows.UIAutomation;
+using System.Windows.Automation;
+using Signum.Entities.DynamicQuery;
+
+namespace Signum.Windows.Extensions.Sample.Test
+{
+    [TestClass]
+    public class SearchWindow
+    {
+        [TestMethod]
+        public void Search()
+        {
+            using (WindowProxy win = Common.StartAndLogin())
+            {
+                //win.Core.MenuItemInvoke("Music");
+                using (SearchWindowProxy albums = new SearchWindowProxy(win.Owner.MenuItemOpenWindow("Music", "Albumes")))
+                {
+                    albums.SelectToken("Entity.Name");
+                    var filter = albums.AddFilter();
+                    filter.SetOperation(FilterOperation.Contains);
+                    albums.Search();
+                    using (NormalWindowProxy album = albums.OpenItemByIndex(0))
+                    {
+
+
+                    }
+                    //win.Core.ChildById("txtA").Pattern<ValuePattern>().SetValue("10");
+                    //win.Core.ChildById("txtB").Pattern<ValuePattern>().SetValue("5");
+
+                }
+            }
+
+        }
+    }
+}
