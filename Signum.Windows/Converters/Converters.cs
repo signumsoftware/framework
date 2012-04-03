6bc045d041d880aa652296beb07f6563cc17a336
Olmo del Corral <olmo@signumsoftware.com>
2012-04-03 15:51:57 +0200

    fix color windows

--- Signum.Windows/Converters/Converters.cs
+++ Signum.Windows/Converters/Converters.cs
@@ -120,7 +120,7 @@ namespace Signum.Windows
 
         public static readonly IValueConverter ColorConverter = ConverterFactory.New(
             (ColorDN c) => c == null ? null : (Color?)Color.FromArgb(c.A, c.R, c.G, c.B),
-            (Color? c) => c == null ? null : new ColorDN(c.Value.A, c.Value.R, c.Value.G, c.Value.B));
+            (Color? c) => c == null ? null : ColorDN.FromARGB(c.Value.A, c.Value.R, c.Value.G, c.Value.B));
 
         public static readonly IMultiValueConverter And = ConverterFactory.New(
             (bool a, bool b) => a && b);
