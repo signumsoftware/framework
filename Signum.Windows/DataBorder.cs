09e82158d8d83975d9d8ea8e8538cd504d720f38
Jesus <jesus.frances@signumsoftware.com>
2012-05-14 09:04:15 +0200

    Merge branch 'master' of //gitserver/Git/Signum

--- Signum.Windows/DataBorder.cs
+++ Signum.Windows/DataBorder.cs
@@ -50,7 +50,7 @@ namespace Signum.Windows
             if(AutoChild)
             {
                 // when datacontext change is fired but its not loaded, it's quite possible that some Common.Routes are not working yet
-                if (newValue == null || !IsLoaded) 
+                if (newValue == null/* || !IsLoaded*/) 
                     Child = null;
                 else
                 {
