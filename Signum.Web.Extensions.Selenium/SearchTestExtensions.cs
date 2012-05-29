1f4b5f8de33f32b35b2f8da6420a1aa7f0a20669
Antonio Herranz <antonio.herranz@signumsoftware.com>
2012-05-29 18:30:22 +0200

    test fix

--- Signum.Web.Extensions.Selenium/SearchTestExtensions.cs
+++ Signum.Web.Extensions.Selenium/SearchTestExtensions.cs
@@ -559,7 +559,7 @@ namespace Signum.Web.Selenium
         public static string QueryMenuOptionLocatorByAttr(string menuId, string optionLocator)
         {
             //check of menu and item classes is redundant but it must be in the html, so good for testing
-            return "jq=#{0}.sf-query-button.sf-dropdown ul.sf-menu-button li.ui-menu-item a.sf-query-button[{1}]".Formato(menuId, optionLocator);
+            return "jq=#{0}.sf-dropdown ul.sf-menu-button li.ui-menu-item a.sf-query-button[{1}]".Formato(menuId, optionLocator);
         }
 
         public static void QueryButtonClick(this ISelenium selenium, string id)
