dde2917a72b2daf969ea2916878427176370840c
Olmo del Corral <olmo@signumsoftware.com>
2012-06-05 12:28:40 +0200

    initial omnibox

---
+++ Signum.Entities.Extensions/Omnibox/OmniboxParser.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Signum.Entities.Omnibox
+{
+    public static class OmniboxParser
+    {
+        public static List<OmniboxProvider> Providers;
+
+        static readonly Regex tokenizer = new Regex(
+@"^(
+(?<space>\s+)|
+(?<ident>[a-zA-Z_][a-zA-Z0-9_]*)|
+(?<number>[+-]?\d+(\.\d+)?)|
+(?<string>("".*?""|\'.*?\'))|
+(?<dot>\.)|
+(?<comparer>(==?|<=|>=|<|>|\^=|\$=|%=|\!=|\!\^=|\!\$=|\!%=))
+)*$", RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
+
+
+        public List<OmniboxResult> Results(string omniboxQuery)
+        {
+            List<OmniboxResult> result = new List<OmniboxResult>();
+
+            var m = tokenizer.Match(omniboxQuery);
+
+                List<OmniboxToken> tokens = new List<OmniboxToken>();
+
+                if (m.Success)
+                {
+                    AddTokens(tokens, m, "ident", OmniboxTokenType.Identifier);
+                    AddTokens(tokens, m, "dot", OmniboxTokenType.Dot);
+                    AddTokens(tokens, m, "comparer", OmniboxTokenType.Comparer);
+                    AddTokens(tokens, m, "number", OmniboxTokenType.Number);
+                    AddTokens(tokens, m, "string", OmniboxTokenType.String);
+                    AddTokens(tokens, m, "date", OmniboxTokenType.String);
+                    
+                    tokens.Sort((a, b) => a.Index.CompareTo(b.Index));
+                }
+
+              foreach (var provider in Providers)
+	          {
+                  provider.AddResults(result, omniboxQuery, tokens);
+              }
+
+            return result;
+        }
+
+        void AddTokens(List<OmniboxToken> tokens, Match m, string groupName, OmniboxTokenType type)
+        {
+            var group = m.Groups["groupName"];
+
+            if (group.Success)
+            {
+                foreach (Capture c in group.Captures)
+                {
+                    tokens.Add(new OmniboxToken(OmniboxTokenType.Identifier, c.Index, c.Value));
+                }
+            }
+        }
+    }
+
+    public abstract class OmniboxResult
+    {
+        public int Priority; 
+
+    }
+
+    public abstract class OmniboxProvider
+    {
+        public void AddResults(List<OmniboxResult> results, string rawQuery, List<OmniboxToken> tokens); 
+    }
+
+    public struct OmniboxToken
+    {
+        public OmniboxToken(OmniboxTokenType type, int index, string value)
+        {
+            this.Type = type;
+            this.Index = index;
+            this.Value = value;
+        }
+
+        public readonly OmniboxTokenType Type;
+        public readonly int Index;
+        public readonly string Value; 
+    }
+
+    public enum OmniboxTokenType
+    {
+        Identifier,
+        Dot,
+        Comparer,
+        Number,
+        String,
+        Date,
+    }
+}
---
+++ Signum.Entities.Extensions/Omnibox/OmniboxUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Signum.Entities.Extensions.Omnibox
+{
+    public static class OmniboxUtils
+    {
+        public static bool IsPascalCasePattern(string ident)
+        {
+            if (string.IsNullOrEmpty(ident))
+                return false;
+
+            for (int i = 0; i < ident.Length; i++)
+            {
+                if (!char.IsUpper(ident[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static int? PascalMatches(string identifier, string pattern)
+        {
+            int distance = identifier.Length;
+            int j = 0;
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                char c = pattern[i];
+
+                for (; j < identifier.Length; j++)
+                {
+                    char c2 = identifier[i];
+                    if (char.IsUpper(c2))
+                    {
+                        if (identifier[j] == c)
+                        {
+                            distance--;
+                            break;
+                        }
+                    }
+                }
+
+                if (j == identifier.Length)
+                    break;
+            }
+
+            return distance == identifier.Length ? (int?)null : distance;
+        }
+
+        
+
+    }
+}
