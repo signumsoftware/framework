111fd1a75b911a7c440792cd2973bece6a64455c
Olmo del Corral <olmo@signumsoftware.com>
2012-07-19 12:06:56 +0200

    fixes left/right outer join

--- Signum.Engine/Linq/ExpressionVisitor/QueryBinder.cs
+++ Signum.Engine/Linq/ExpressionVisitor/QueryBinder.cs
@@ -518,8 +518,8 @@ namespace Signum.Engine.Linq
 
         protected virtual Expression BindJoin(Type resultType, Expression outerSource, Expression innerSource, LambdaExpression outerKey, LambdaExpression innerKey, LambdaExpression resultSelector)
         {
-            bool rightOuter = OverloadingSimplifier.ExtractDefaultIfEmpty(ref outerSource);
-            bool leftOuter = OverloadingSimplifier.ExtractDefaultIfEmpty(ref innerSource);
+            bool leftOuter = OverloadingSimplifier.ExtractDefaultIfEmpty(ref outerSource);
+            bool rightOuter = OverloadingSimplifier.ExtractDefaultIfEmpty(ref innerSource);
 
             ProjectionExpression outerProj = VisitCastProjection(outerSource);
             ProjectionExpression innerProj = VisitCastProjection(innerSource);
@@ -529,9 +529,9 @@ namespace Signum.Engine.Linq
 
             Expression condition = DbExpressionNominator.FullNominate(SmartEqualizer.EqualNullable(outerKeyExpr, innerKeyExpr));
 
-            JoinType jt = rightOuter && leftOuter ? JoinType.FullOuterJoin :
-                          rightOuter ? JoinType.RightOuterJoin :
+            JoinType jt = leftOuter && rightOuter ? JoinType.FullOuterJoin :
                           leftOuter ? JoinType.LeftOuterJoin :
+                          rightOuter ? JoinType.RightOuterJoin :
                           JoinType.InnerJoin;
 
             Alias alias = NextSelectAlias();
