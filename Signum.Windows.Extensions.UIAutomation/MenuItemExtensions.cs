b262140e6884ac7a03223baf7146180afb42e6c4
Olmo del Corral <olmo@signumsoftware.com>
2012-09-03 18:21:09 +0200

    fixes UIAutomation

--- Signum.Windows.Extensions.UIAutomation/MenuItemExtensions.cs
+++ Signum.Windows.Extensions.UIAutomation/MenuItemExtensions.cs
@@ -9,25 +9,24 @@ namespace Signum.Windows.UIAutomation
 {
     public static class MenuItemExtensions
     {
-        public static AutomationElement MenuItemOpenWindow(AutomationElement window, params string[] menuNames)
+        public static AutomationElement MenuItemOpenWindow(AutomationElement menu, params string[] menuNames)
         {
-            var menuItem = MenuItemFind(window, menuNames);
+            var menuItem = MenuItemFind(menu, menuNames);
 
-            AutomationElement newWindow = window.CaptureWindow(
+            AutomationElement newWindow = menu.CaptureWindow(
                 () => menuItem.ButtonInvoke(),
                 () => "New windows opened after menu  " + menuNames.ToString(" -> "));
 
             return newWindow;
         }
 
-        public static AutomationElement MenuItemFind(AutomationElement element, params string[] menuNames)
+        public static AutomationElement MenuItemFind(AutomationElement menu, params string[] menuNames)
         {
             if (menuNames == null || menuNames.Length == 0)
                 throw new ArgumentNullException("menuNames");
 
-            var menuBar = element.Child(c => c.Current.ControlType == ControlType.Menu);
 
-            var menuItem = menuBar.ChildByCondition(new PropertyCondition(AutomationElement.NameProperty, menuNames[0]));
+            var menuItem = menu.ChildByCondition(new PropertyCondition(AutomationElement.NameProperty, menuNames[0]));
 
             for (int i = 1; i < menuNames.Length; i++)
             {
