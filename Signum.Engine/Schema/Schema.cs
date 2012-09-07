aec8d91b4cd181b2aaa361fabacf16a763afd432
Alejandro <alejandro.cano@signumsoftware.com>
2012-09-08 01:13:59 +0200

    prueba

--- Signum.Engine/Schema/Schema.cs
+++ Signum.Engine/Schema/Schema.cs
@@ -506,8 +506,8 @@ namespace Signum.Engine.Maps
                 return null;
 
             //TODO: mirar con olmo
-            //Expression e = MetadataVisitor.JustVisit(lambda, new MetaExpression(route.Type.UnNullify(), new CleanMeta(new[] { route.Parent })));
-            Expression e = MetadataVisitor.JustVisit(lambda, new MetaExpression(route.Parent.Type, new CleanMeta(new[] { route.Parent })));
+            Expression e = MetadataVisitor.JustVisit(lambda, new MetaExpression(route.Type.UnNullify(), new CleanMeta(new[] { route.Parent })));
+            //Expression e = MetadataVisitor.JustVisit(lambda, new MetaExpression(route.Parent.Type, new CleanMeta(new[] { route.Parent })));
 
             MetaExpression me = e as MetaExpression;
             if (me == null)
