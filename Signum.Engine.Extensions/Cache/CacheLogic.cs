4e9cda4819552ff0591dd0685317765c92e05fcb
Alejandro <alejandro.cano@signumsoftware.com>
2013-01-23 11:39:26 +0100

    fix cache message

--- Signum.Engine.Extensions/Cache/CacheLogic.cs
+++ Signum.Engine.Extensions/Cache/CacheLogic.cs
@@ -59,7 +59,9 @@ namespace Signum.Engine.Cache
             {
                 if (ex.Message.Contains("SQL Server Service Broker"))
                     throw new InvalidOperationException(@"CacheLogic requires SQL Server Service Broker to be activated. Execute: 
-ALTER DATABASE {0} SET ENABLE_BROKER\r\nIf you have problems, try: \r\nALTER DATABASE {0} SET ENABLE_BROKER".Formato(Connector.Current.DatabaseName()));
+ALTER DATABASE {0} SET ENABLE_BROKER
+If you have problems, try: 
+ALTER DATABASE {0} SET NEW_BROKER".Formato(Connector.Current.DatabaseName()));
             }
         }
 
