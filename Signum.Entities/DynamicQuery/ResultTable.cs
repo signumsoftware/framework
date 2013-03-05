2e341cc91304df1e70691ff633acf87d03dbde6c
Olmo del Corral <olmo@signumsoftware.com>
2013-03-05 17:58:02 +0100

    Improve Serialization on Lite<T> and ResultTable

--- Signum.Entities/DynamicQuery/ResultTable.cs
+++ Signum.Entities/DynamicQuery/ResultTable.cs
@@ -8,58 +8,75 @@ using System.Collections;
 using System.Reflection;
 using Signum.Utilities;
 using Signum.Entities.Properties;
+using System.Runtime.Serialization;
 
 namespace Signum.Entities.DynamicQuery
 {
     [Serializable]
     public class ResultColumn
     {
-        public ResultColumn(Column column, Array values)
+        Column column;
+        public Column Column
         {
-            this.Column = column;
+            get { return column; }
+        }
+
+        int index;
+        public int Index
+        {
+            get { return index; }
+            internal set { index = value; }
+        }
+
+        internal IList Values;
+
+        public ResultColumn(Column column, IList values)
+        {
+            this.column = column;
             this.Values = values;
         }
-        public Column Column { get; private set; }
-        public int Index { get; internal set; }
-        internal Array Values;
+      
     }
 
     [Serializable]
-    public class ResultTable
+    public class ResultTable 
     {
+        internal IList entityValues;
         public bool HasEntities
         {
             get { return entityValues != null; }
         }
 
-        internal Array entityValues;
-        public ColumnDescription EntityColumn { get; private set; }
-        public ResultColumn[] Columns { get; private set; }
+        public ColumnDescription entityColumn;
+        public ColumnDescription EntityColumn { get { return entityColumn; } }
 
-        public ResultRow[] Rows { get; private set; }
+        ResultColumn[] columns;
+        public ResultColumn[] Columns { get { return columns; } }
+
+        ResultRow[] rows;
+        public ResultRow[] Rows { get { return rows; } }
 
         public ResultTable(ResultColumn[] columns, int totalElements, int currentPage, int elementsPerPage)
         {
-            int rows = columns.Select(a => a.Values.Length).Distinct().SingleEx(() => "Unsyncronized number of rows in the results");
+            int rows = columns.Select(a => a.Values.Count).Distinct().SingleEx(() => "Unsyncronized number of rows in the results");
 
-            ResultColumn entityColumn = columns.Where(c => c.Column is _EntityColumn).SingleOrDefaultEx(); ;
-            if (entityColumn != null)
+            ResultColumn entity = columns.Where(c => c.Column is _EntityColumn).SingleOrDefaultEx(); ;
+            if (entity != null)
             {
-                this.EntityColumn = ((ColumnToken)entityColumn.Column.Token).Column;
-                entityValues = entityColumn.Values;
+                this.entityColumn = ((ColumnToken)entity.Column.Token).Column;
+                entityValues = entity.Values;
             }
-            this.Columns = columns.Where(c => !(c.Column is _EntityColumn) && c.Column.Token.IsAllowed() == null).ToArray();
+            this.columns = columns.Where(c => !(c.Column is _EntityColumn) && c.Column.Token.IsAllowed() == null).ToArray();
 
             for (int i = 0; i < Columns.Length; i++)
                 Columns[i].Index = i;
 
-            this.Rows = 0.To(rows).Select(i => new ResultRow(i, this)).ToArray();
+            this.rows = 0.To(rows).Select(i => new ResultRow(i, this)).ToArray();
 
-            this.TotalElements = totalElements;
-            this.CurrentPage = currentPage;
-            this.ElementsPerPage = elementsPerPage;
+            this.totalElements = totalElements;
+            this.currentPage = currentPage;
+            this.elementsPerPage = elementsPerPage;
         }
-
 
         public DataTable ToDataTable()
         {
@@ -81,10 +98,14 @@ namespace Signum.Entities.DynamicQuery
             return p;
         }
 
-        public int TotalElements { get; private set; }
-        public int CurrentPage { get; private set; }
+        int totalElements;
+        public int TotalElements { get { return totalElements; } }
 
-        public int ElementsPerPage { get; private set; }
+        int currentPage;
+        public int CurrentPage { get { return currentPage; } }
+
+        int elementsPerPage;
+        public int ElementsPerPage { get { return elementsPerPage; } }
 
         public int TotalPages
         {
@@ -100,6 +121,44 @@ namespace Signum.Entities.DynamicQuery
         {
             get { return ElementsPerPage == -1 ? (int?)null : StartElementIndex.Value + Rows.Count() - 1; }
         }
+
+        [OnSerializing]
+        private void OnSerializing(StreamingContext context)
+        {
+            Dictionary<string, string> strings = new Dictionary<string, string>();
+            Dictionary<Lite<IdentifiableEntity>, Lite<IdentifiableEntity>> lites = new Dictionary<Lite<IdentifiableEntity>, Lite<IdentifiableEntity>>();
+
+            if (entityValues != null)
+                NormalizeLites(entityValues, lites);
+
+            foreach (var col in columns)
+            {
+                if (col.Values is IEnumerable<Lite<IIdentifiable>>)
+                    NormalizeLites(col.Values, lites);
+                else if (col.Values is IEnumerable<string>)
+                    NormalizeStrings(col.Values, strings);
+            }
+        }
+
+        static void NormalizeStrings(IList values, Dictionary<string, string> strings)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                var val = (string)values[i];
+                if (val != null)
+                    values[i] = strings.GetOrCreate(val, val);
+            }
+        }
+
+        static void NormalizeLites(IList values, Dictionary<Lite<IdentifiableEntity>, Lite<IdentifiableEntity>> lites)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                var val = (Lite<IdentifiableEntity>)values[i];
+                if (val != null)
+                    values[i] = lites.GetOrCreate(val, val);
+            }
+        }
     }
 
 
@@ -111,12 +170,12 @@ namespace Signum.Entities.DynamicQuery
 
         public object this[int columnIndex]
         {
-            get { return Table.Columns[columnIndex].Values.GetValue(Index); }
+            get { return Table.Columns[columnIndex].Values[Index]; }
         }
 
         public object this[ResultColumn column]
         {
-            get { return column.Values.GetValue(Index); }
+            get { return column.Values[Index]; }
         }
 
         internal ResultRow(int index, ResultTable table)
@@ -127,7 +186,7 @@ namespace Signum.Entities.DynamicQuery
 
         public Lite<IdentifiableEntity> Entity
         {
-            get { return (Lite<IdentifiableEntity>)Table.entityValues.GetValue(Index); }
+            get { return (Lite<IdentifiableEntity>)Table.entityValues[Index]; }
         }
 
         public T GetValue<T>(string columnName)
