fe6c6301c6bf8030bd1e638551e9a6120b4dad6f
Luis Delgado <luis.delgado@signumsoftware.com>
2013-03-25 12:46:16 +0100

    more DateTime extensions on Signum.Utilities

--- Signum.Utilities/DateTimeExtensions.cs
+++ Signum.Utilities/DateTimeExtensions.cs
@@ -373,7 +373,17 @@ namespace Signum.Utilities
                 throw new InvalidOperationException("dateTime should be UTC"); 
 
             return (long)new TimeSpan(dateTime.Ticks - new DateTime(1970, 1, 1).Ticks).TotalMilliseconds;
-        } 
+        }
+
+        public static string ToMonthName(this DateTime dateTime)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dateTime.Month);
+        }
+
+        public static string ToShortMonthName(this DateTime dateTime)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(dateTime.Month);
+        }
     }
 
     public enum DateTimePrecision
