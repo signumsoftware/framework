573adfe39c648da11d4ca3611c39addd0da052f7
alejandro <alejandro.cano@signumsoftware.com>
2013-07-16 20:28:15 +0200

    fix sendEmail

--- Signum.Engine.Extensions/Mailing/EmailLogic.cs
+++ Signum.Engine.Extensions/Mailing/EmailLogic.cs
@@ -57,6 +57,8 @@ namespace Signum.Engine.Mailing
 
     public static class EmailLogic
     {
+        public static bool SendEmails = false;
+
         public static string DoNotSend = "null@null.com";
 
         public static Func<string> OverrideEmailAddress = () => null;
@@ -264,6 +266,9 @@ namespace Signum.Engine.Mailing
 
         public static SmtpClient SafeSmtpClient()
         {
+            if (!SendEmails)
+                throw new InvalidOperationException("EmailLogic.EmailConfigurationDN.SendEmails is set to false");
+
             //http://weblogs.asp.net/stanleygu/archive/2010/03/31/tip-14-solve-smtpclient-issues-of-delayed-email-and-high-cpu-usage.aspx
             return new SmtpClient()
             {
@@ -273,6 +278,9 @@ namespace Signum.Engine.Mailing
 
         internal static SmtpClient SafeSmtpClient(string host, int port)
         {
+            if (!SendEmails)
+                throw new InvalidOperationException("EmailLogic.SendEmails is set to false");
+
             //http://weblogs.asp.net/stanleygu/archive/2010/03/31/tip-14-solve-smtpclient-issues-of-delayed-email-and-high-cpu-usage.aspx
             return new SmtpClient(host, port)
             {
