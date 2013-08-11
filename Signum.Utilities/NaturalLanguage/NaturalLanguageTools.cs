6617c59f8d3750a8e8ad0c28f3f08e166b526a12
Olmo del Corral <olmo@signumsoftware.com>
2013-08-11 19:46:18 +0200

    NextString(chars), SpanishNumberWriter, and move NaturalLanguageTools

--- Signum.Utilities/NaturalLanguage/NaturalLanguageTools.cs
+++ Signum.Utilities/NaturalLanguage/NaturalLanguageTools.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Resources;
 using System.Text.RegularExpressions;
 using System.Collections.ObjectModel;
+using Signum.Utilities.NaturalLanguage;
 
 namespace Signum.Utilities
 {
@@ -21,6 +22,11 @@ namespace Signum.Utilities
         public static Dictionary<string, IGenderDetector> GenderDetectors = new Dictionary<string, IGenderDetector>
         {
             {"es", new SpanishGenderDetector()},
+        };
+
+        public static Dictionary<string, INumberWriter> NumberWriters = new Dictionary<string, INumberWriter>
+        {
+            {"es", new SpanishNumberWriter()},
         };
 
         public static char? GetGender(string name, CultureInfo culture = null)
@@ -225,89 +231,6 @@ namespace Signum.Utilities
         string MakePlural(string singularName);
     }
 
-    public class EnglishPluralizer : IPluralizer
-    {
-        //http://www.csse.monash.edu.au/~damian/papers/HTML/Plurals.html
-        Dictionary<string, string> exceptions = new Dictionary<string, string>
-        {
-            {"an", "en"}, // woman -> women 
-            {"ch", "ches"}, // church -> churches 
-            {"eau", "eaus"},  //chateau -> chateaus
-            {"en", "ens"}, //foramen -> foramens
-            {"ex", "exes"}, //index -> indexes
-            {"f", "ves"}, //wolf -> wolves 
-            {"fe", "ves"}, //wolf -> wolves 
-            {"ieu", "ieus milieu"}, //milieu-> mileus
-            {"is", "is"}, //basis -> basis 
-            {"ix", "ixes"}, //matrix -> matrixes
-            {"nx", "nxes"}, //phalanx -> phalanxes 
-            {"s", "s"}, //series -> series 
-            {"sh", "shes"}, //wish -> wishes 
-            {"us",  "us"},// genus -> us 
-            {"x",  "xes"},// box -> boxes 
-            {"y", "ies"}, //ferry -> ferries 
-        };
-
-        public string MakePlural(string singularName)
-        {
-            if (string.IsNullOrEmpty(singularName))
-                return singularName;
-
-            int index = singularName.LastIndexOf(' ');
-
-            if (index != -1)
-                return singularName.Substring(0, index + 1) + MakePlural(singularName.Substring(index + 1));
-
-            var result = exceptions.FirstOrDefault(r => singularName.EndsWith(r.Key));
-            if (result.Value != null)
-                return singularName.Substring(0, singularName.Length - result.Key.Length) + result.Value;
-
-            return singularName + "s";
-        }
-    }
-
-    public class SpanishPluralizer : IPluralizer
-    {
-        //http://es.wikipedia.org/wiki/Formaci%C3%B3n_del_plural_en_espa%C3%B1ol
-        Dictionary<string, string> exceptions = new Dictionary<string, string>
-        {
-            {"x", "x"}, // tórax -> tórax
-            {"s", "s"}, // suponemos que el nombre ya está pluralizado
-            {"z", "ces"},  //vez -> veces
-            {"g", "gues"}, //zigzag -> zigzagues
-            {"c", "ques"}, //frac -> fraques
-            {"t", "ts"}, //mamut -> mamuts
-            {"án", "anes"},
-            {"én", "enes"},
-            {"ín", "ines"},
-            {"ón", "ones"},
-            {"ún", "unes"},
-        };
-
-        char[] vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú' };
-
-        public string MakePlural(string singularName)
-        {
-            if (string.IsNullOrEmpty(singularName))
-                return singularName;
-
-            int index = singularName.IndexOf(' ');
-
-            if (index != -1)
-                return MakePlural(singularName.Substring(0, index)) + singularName.Substring(index);
-
-            var result = exceptions.FirstOrDefault(r => singularName.EndsWith(r.Key));
-            if (result.Value != null)
-                return singularName.Substring(0, singularName.Length - result.Key.Length) + result.Value;
-
-            char lastChar = singularName[singularName.Length - 1];
-            if (vowels.Contains(lastChar))
-                return singularName + "s";
-            else
-                return singularName + "es";
-        }
-    }
-
     public interface IGenderDetector
     {
         char? GetGender(string name);
@@ -329,64 +252,23 @@ namespace Signum.Utilities
         }
     }
 
-    public class SpanishGenderDetector : IGenderDetector
+    public interface INumberWriter
     {
-        //http://roble.pntic.mec.es/acid0002/index_archivos/Gramatica/genero_sustantivos.htm
-        Dictionary<string, char> terminationIsFemenine = new Dictionary<string, char>()
-        {
-            {"umbre", 'f' },
-           
-            {"ión", 'f' },
-            {"dad", 'f' },
-            {"tad", 'f' },
-            
-            {"ie", 'f' },
-            {"is", 'f' }, 
-
-            {"pa", 'f'},
-            //{"ta", Gender.Masculine}, Cuenta, Nota, Alerta... son femeninos
-            {"ma", 'f'},
-
-            {"a", 'f'},
-            {"n", 'm'},
-            {"o", 'm'},
-            {"r", 'm'},
-            {"s", 'm'},
-            {"e", 'm'},
-            {"l", 'm'},
-
-            {"", 'm'},
-        };
-
-
-        public char? GetGender(string name)
-        {
-            if (string.IsNullOrEmpty(name))
-                return null;
-
-            int index = name.IndexOf(' ');
-
-            if (index != -1)
-                return GetGender(name.Substring(0, index));
-
-            foreach (var kvp in terminationIsFemenine)
-            {
-                if (name.EndsWith(kvp.Key))
-                    return kvp.Value;
-            }
-
-            return null;
-        }
-
-        ReadOnlyCollection<PronomInfo> pronoms = new ReadOnlyCollection<PronomInfo>(new []
-        {
-            new PronomInfo('m', "el", "los"),
-            new PronomInfo('f', "la", "las")
-        });
-
-        public ReadOnlyCollection<PronomInfo> Pronoms
-        {
-            get { return pronoms; }
-        }
+        string ToNumber(decimal number, NumberWriterSettings settings);
     }
+
+    public class NumberWriterSettings
+    {
+        public string Unit;
+        public string UnitPlural;
+        public char? UnitGender;
+
+        public string DecimalUnit;
+        public string DecimalUnitPlural; 
+        public char? DecimalUnitGender;
+
+        public int NumeroDecimales;
+        public bool OmitirDecimalZeros; 
+    }
+
 }
