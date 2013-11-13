1d9b8bcffb62a718470bf7e604f884111e3c0f86
Antonio Herranz <antonio.herranz@signumsoftware.com>
2013-11-13 12:42:26 +0100

    WaitForPageToLoad fix

--- Signum.Web.Extensions.Selenium/OldApi/SeleniumExtensions.cs
+++ Signum.Web.Extensions.Selenium/OldApi/SeleniumExtensions.cs
@@ -108,9 +108,9 @@ namespace Signum.Web.Selenium
         public static int AjaxTimeout = 10000;
         public static int AjaxWait = 200;
 
-        public static void WaitForPageToLoad( this ISelenium selenium)
+        public static void WaitForPageToLoad(this ISelenium selenium)
         {
-            selenium.WaitForPageToLoad();
+            selenium.WaitForPageToLoad(PageLoadTimeout);
         }
 
         public static void Wait(this ISelenium selenium, Func<bool> condition, Func<string> actionDescription = null, int? timeout = null)
