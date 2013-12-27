1f30016d60ce695147f7d429b8f87d9eafd4c28e
Olmo del Corral <olmo@signumsoftware.com>
2013-12-27 12:55:59 +0100

    UserQuery.js and .map compiled

---
+++ Signum.Web.Extensions/UserQueries/Scripts/SF_UserQuery.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../../../Framework/Signum.Web/Signum/Headers/jquery/jquery.d.ts"/>
+/// <reference path="../../../../Framework/Signum.Web/Signum/Scripts/references.ts"/>
+var SF;
+(function (SF) {
+    (function (UserQuery) {
+        once("SF-UserQuery", function () {
+            $(document).on("click", ".sf-userquery", function (e) {
+                e.preventDefault();
+                var findOptionsQueryString = SF.FindNavigator.getFor("").requestDataForSearchInUrl();
+                var url = $(this).attr("href") + findOptionsQueryString;
+
+                if (e.ctrlKey || e.which == 2) {
+                    window.open(url);
+                } else if (e.which == 1) {
+                    window.location.href = url;
+                }
+            });
+        });
+    })(SF.UserQuery || (SF.UserQuery = {}));
+    var UserQuery = SF.UserQuery;
+})(SF || (SF = {}));
+//# sourceMappingURL=SF_UserQuery.js.map
