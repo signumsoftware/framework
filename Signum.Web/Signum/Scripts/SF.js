e84a4c188f59e75bdd4bac2ba8bc50b0b11cc6af
Olmo del Corral <olmo@signumsoftware.com>
2014-02-16 14:34:39 +0100

    fix $.serializeObject

--- Signum.Web/Signum/Scripts/SF.js
+++ Signum.Web/Signum/Scripts/SF.js
@@ -314,10 +314,7 @@ $.fn.serializeObject = function () {
     var a = this.serializeArray();
     $.each(a, function () {
         if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
+            o[this.name] += "," + (this.value || '');
         } else {
             o[this.name] = this.value || '';
         }
