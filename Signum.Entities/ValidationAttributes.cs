61a38c08805018dcc957afb9ee17fa0c3a3b1061
Olmo del Corral <olmo@signumsoftware.com>
2014-04-03 18:03:38 +0200

    change EMailValidatorAttribute to allow bla@bla.co (no m)

--- Signum.Entities/ValidationAttributes.cs
+++ Signum.Entities/ValidationAttributes.cs
@@ -181,12 +181,8 @@ namespace Signum.Entities
     public class EMailValidatorAttribute : RegexValidatorAttribute
     {
         public static readonly Regex EmailRegex = new Regex(
-                          @"^(([^<>()[\]\\.,;:\s@\""]+"
-                        + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
-                        + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
-                        + @"\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+"
-                        + @"[a-zA-Z]{2,}))$", RegexOptions.IgnoreCase);
-
+                          @"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+                          + @"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", RegexOptions.IgnoreCase);
         public EMailValidatorAttribute()
             : base(EmailRegex)
         {
