91026644a2a1585351b3281c399809a67fcfcc7a
Olmo del Corral <olmo@signumsoftware.com>
2014-04-15 11:04:42 +0200

    fix SymbolLogic and SemiSymbolLogic  Sync

--- Signum.Engine/Basics/SymbolLogic.cs
+++ Signum.Engine/Basics/SymbolLogic.cs
@@ -86,7 +86,7 @@ namespace Signum.Engine
                 {
                     var originalName = c.Key;
                     c.Key = s.Key;
-                    return table.UpdateSqlSync(c, comment: c.Key);
+                    return table.UpdateSqlSync(c, comment: originalName);
                 }, Spacing.Double);
         }
 
