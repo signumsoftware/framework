d7662ce6c49448b295d0ed32d2ede08ef6758f8d
Olmo del Corral <olmo@signumsoftware.com>
2014-04-18 10:12:21 +0200

    IsolationLogic

---
+++ Signum.Engine.Extensions/Isolation/IsolationLogic.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using Signum.Engine.DynamicQuery;
+using Signum.Engine.Maps;
+using Signum.Engine.Operations;
+using Signum.Entities;
+using Signum.Entities.Isolation;
+using Signum.Utilities;
+
+namespace Signum.Engine.Isolation
+{
+    public enum IsolationStrategy
+    {
+        Isolated,
+        None,
+    }
+
+    public static class IsolationLogic
+    {
+        static Expression<Func<IdentifiableEntity, Lite<IsolationDN>>> IsolationExpression =
+            entity => entity.Mixin<IsolationMixin>().Isolation;
+        public static Lite<IsolationDN> Isolation(this IdentifiableEntity entity)
+        {
+            return IsolationExpression.Evaluate(entity);
+        }
+
+        internal static Dictionary<Type, IsolationStrategy> strategies = new Dictionary<Type, IsolationStrategy>();
+
+        public static void Start(SchemaBuilder sb, DynamicQueryManager dqm)
+        {
+            if (sb.NotDefined(MethodInfo.GetCurrentMethod()))
+            {
+                sb.Include<IsolationDN>();
+
+                dqm.RegisterQuery(typeof(IsolationDN), () =>
+                    from iso in Database.Query<IsolationDN>()
+                    select new
+                    {
+                        Entity = iso,
+                        iso.Id,
+                        iso.Name
+                    });
+
+                new Graph<IsolationDN>.Execute(IsolationOperation.Save)
+                {
+                    AllowsNew = true,
+                    Lite = false,
+                    Execute = (e, _) => { }
+                }.Register();
+
+                sb.Schema.EntityEventsGlobal.Saving += EntityEventsGlobal_Saving;
+
+                sb.Schema.Initializing[InitLevel.Level0SyncEntities] += AssertIsolationStrategies;
+            }
+        }
+
+        static void EntityEventsGlobal_Saving(IdentifiableEntity ident)
+        {
+            if (strategies.TryGet(ident.GetType(), IsolationStrategy.None) == IsolationStrategy.Isolated)
+            {
+                if (ident.Mixin<IsolationMixin>().Isolation == null)
+                    ident.Mixin<IsolationMixin>().Isolation = IsolationDN.Current;
+            }
+        }
+
+        static void AssertIsolationStrategies()
+        {
+            var result = EnumerableExtensions.JoinStrict(
+                strategies.Keys,
+                Schema.Current.Tables.Keys.Where(a => !a.IsEnumEntity() && !typeof(Symbol).IsAssignableFrom(a) && !typeof(Symbol).IsAssignableFrom(a)),
+                a => a,
+                a => a,
+                (a, b) => 0);
+
+            var extra = result.Extra.OrderBy(a => a.Namespace).ThenBy(a => a.Name).ToString(t => "  IsolationLogic.Register<{0}>(IsolationStrategy.None);".Formato(t.Name), "\r\n");
+
+            var lacking = result.Missing.GroupBy(a => a.Namespace).OrderBy(gr => gr.Key).ToString(gr => "  //{0}\r\n".Formato(gr.Key) +
+                gr.ToString(t => "  IsolationLogic.Register<{0}>(IsolationStrategy.None);".Formato(t.Name), "\r\n"), "\r\n\r\n");
+
+            if (extra.HasText() || lacking.HasText())
+                throw new InvalidOperationException("IsolationLogic's strategies are not synchronized with the Schema.\r\n" +
+                        (extra.HasText() ? ("Remove something like:\r\n" + extra + "\r\n\r\n") : null) +
+                        (lacking.HasText() ? ("Add something like:\r\n" + lacking + "\r\n\r\n") : null));
+        }
+
+
+        public static void Register<T>(IsolationStrategy strategy) where T : IdentifiableEntity
+        {
+            strategies.Add(typeof(T), strategy);
+
+            if (strategy == IsolationStrategy.Isolated)
+                MixinDeclarations.Register(typeof(T), typeof(IsolationMixin));
+        }
+    }
+}
---
+++ Signum.Entities.Extensions/Isolation/IsolationDN.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Signum.Utilities;
+
+namespace Signum.Entities.Isolation
+{
+    [Serializable, EntityKind(EntityKind.String, EntityData.Master)]
+    public class IsolationDN : Entity
+    {
+        [NotNullable, SqlDbType(Size = 100), UniqueIndex]
+        string name;
+        [StringLengthValidator(AllowNulls = false, Min = 3, Max = 100)]
+        public string Name
+        {
+            get { return name; }
+            set { SetToStr(ref name, value); }
+        }
+
+        static Expression<Func<IsolationDN, string>> ToStringExpression = e => e.name;
+        public override string ToString()
+        {
+            return ToStringExpression.Evaluate(this);
+        }
+
+        public static readonly SessionVariable<Lite<IsolationDN>> DefaultVariable = Statics.SessionVariable<Lite<IsolationDN>>("CurrentIsolation");
+        public static Lite<IsolationDN> Default
+        {
+            get { return DefaultVariable.Value; }
+            set { DefaultVariable.Value = value; }
+        }
+
+        static readonly ThreadVariable<Lite<IsolationDN>> CurrentThreadVariable = Statics.ThreadVariable<Lite<IsolationDN>>("CurrentIsolation");
+
+        public IDisposable OverrideCurrentIsolation(Lite<IsolationDN> isolation)
+        {
+            var old = CurrentThreadVariable.Value; 
+
+            CurrentThreadVariable.Value = isolation;
+
+            return new Disposable(() => CurrentThreadVariable.Value = old); 
+        }
+
+        public static Lite<IsolationDN> Current
+        {
+            get { return CurrentThreadVariable.Value ?? Default; }
+        }
+    }
+
+    public static class IsolationOperation
+    {
+        public static readonly ExecuteSymbol<IsolationDN> Save = OperationSymbol.Execute<IsolationDN>(); 
+    }
+
+
+    [Serializable]
+    public class IsolationMixin : MixinEntity
+    {
+        IsolationMixin(IdentifiableEntity mainEntity, MixinEntity next) : base(mainEntity, next) { }
+
+        [NotNullable]
+        Lite<IsolationDN> isolation;
+        [NotNullValidator]
+        public Lite<IsolationDN> Isolation
+        {
+            get { return isolation; }
+            set { Set(ref isolation, value); }
+        }
+
+        protected override void CopyFrom(MixinEntity mixin, object[] args)
+        {
+            Isolation = ((IsolationMixin)mixin).Isolation;
+        }
+    }
+}
