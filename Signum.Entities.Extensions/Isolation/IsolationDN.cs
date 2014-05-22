2b5029e3fa5d22638d6c91bf46fd36acbd164258
Adrián Farina Piera <adrian.farina@signumsoftware.com>
2014-05-22 18:07:42 +0200

    IsolationServiceBehaviorAttribute

--- Signum.Entities.Extensions/Isolation/IsolationDN.cs
+++ Signum.Entities.Extensions/Isolation/IsolationDN.cs
@@ -33,7 +33,7 @@ namespace Signum.Entities.Isolation
             set { DefaultVariable.Value = value; }
         }
 
-        static readonly ThreadVariable<Lite<IsolationDN>> CurrentThreadVariable = Statics.ThreadVariable<Lite<IsolationDN>>("CurrentIsolation");
+        internal static readonly ThreadVariable<Lite<IsolationDN>> CurrentThreadVariable = Statics.ThreadVariable<Lite<IsolationDN>>("CurrentIsolation");
 
         public static IDisposable OverrideIfNecessary(Lite<IsolationDN> isolation)
         {
