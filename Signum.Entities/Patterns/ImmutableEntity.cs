67c79acbc00ddc02d9626f5ac1638070eb9c2f98
Adrián Farina Piera <adrian.farina@signumsoftware.com>
2014-06-09 17:21:01 +0200

    ImmutableEntity IDisposable AllowChanges()

--- Signum.Entities/Patterns/ImmutableEntity.cs
+++ Signum.Entities/Patterns/ImmutableEntity.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Linq.Expressions;
 using System.ComponentModel;
+using Signum.Utilities;
 
 namespace Signum.Entities
 {
     [Serializable]
-    public class ImmutableEntity: IdentifiableEntity
+    public class ImmutableEntity : IdentifiableEntity
     {
         [Ignore]
         bool allowTemporaly = false;
-        
+
         public bool AllowChange
         {
             get { return allowTemporaly || IsNew; }
-            set { allowTemporaly = value; Notify(()=>AllowChange); }
+            set { allowTemporaly = value; Notify(() => AllowChange); }
         }
 
         protected override bool Set<T>(ref T variable, T value, Expression<Func<T>> property)
@@ -34,6 +35,13 @@ namespace Signum.Entities
             else
                 if (Modified == ModifiedState.SelfModified)
                     throw new InvalidOperationException("Attempt to save a not new modified ImmutableEntity");
+        }
+
+        public IDisposable AllowChanges()
+        {
+            bool old = this.AllowChange;
+            this.AllowChange = true;
+            return new Disposable(() => this.AllowChange = old);
         }
     }
 
