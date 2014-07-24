4f7fbd8dcb2ba19013d525b90f491ae01a00e4a5
Olmo del Corral <olmo@signumsoftware.com>
2014-07-24 18:21:18 +0200

    { TextAlign = "center" } in WebDownload

--- Signum.Web.Extensions/Files/FilesClient.cs
+++ Signum.Web.Extensions/Files/FilesClient.cs
@@ -181,15 +181,14 @@ namespace Signum.Web.Files
                 QuerySettings.FormatRules.Add(new FormatterRule(
                        col => col.Type == typeof(WebImage),
                        col => new CellFormatter((help, obj) => ((WebImage)obj).FullWebPath == null ? null :
-                           new MvcHtmlString("<img src='" +
-                               RouteHelper.New().Content(((WebImage)obj).FullWebPath) +
-                               "' alt='" + typeof(WebImage).NiceName() + "' class='sf-search-control-image' />"))
+                           new HtmlTag("img").Attr("src", RouteHelper.New().Content(((WebImage)obj).FullWebPath)).Attr("alt", typeof(WebImage).NiceName())
+                           .Attr("style", "width:80px").ToHtmlSelf()) { TextAlign = "center" }
                  ));
 
                 QuerySettings.FormatRules.Add(new FormatterRule(
                        col => col.Type == typeof(WebDownload),
                        col => new CellFormatter((help, obj) => ((WebDownload)obj).FullWebPath == null ? null :
-                          new MvcHtmlString("<a href='{0}'>{1}</a>".Formato(RouteHelper.New().Content(((WebDownload)obj).FullWebPath), typeof(WebDownload).NiceName())))
+                          new MvcHtmlString("<a href='{0}'>{1}</a>".Formato(RouteHelper.New().Content(((WebDownload)obj).FullWebPath), typeof(WebDownload).NiceName()))) { TextAlign = "center" }
                 ));
 
             }
