e080caca209a94d69edd1b8d80ddca03625f5423
Olmo del Corral <olmo@signumsoftware.com>
2014-09-20 20:48:14 +0200

    fix OperationServer covariance

--- Signum.Windows/Operations/OperationServer.cs
+++ Signum.Windows/Operations/OperationServer.cs
@@ -10,29 +10,29 @@ namespace Signum.Windows
     public static class OperationServer
     {
         public static T Execute<T, B>(this T entity, ExecuteSymbol<B> symbol, params object[] args)
-            where T : class, IIdentifiable
-            where B : class, IIdentifiable, T
+            where T : class, IIdentifiable, B
+            where B : class, IIdentifiable
         {
             return (T)(IIdentifiable)Server.Return((IOperationServer s) => s.ExecuteOperation(entity, symbol.Symbol, args)); 
         }
 
         public static T ExecuteLite<T, B>(this Lite<T> lite, ExecuteSymbol<B> symbol, params object[] args)
-            where T : class, IIdentifiable
-            where B : class, IIdentifiable, T
+            where T : class, IIdentifiable, B
+            where B : class, IIdentifiable
         {
             return (T)(IIdentifiable)Server.Return((IOperationServer s) => s.ExecuteOperationLite(lite, symbol.Symbol, args)); 
         }
 
         public static void Delete<T, B>(this T entity, DeleteSymbol<B> symbol, params object[] args)
-            where T : class, IIdentifiable
-            where B : class, IIdentifiable, T
+            where T : class, IIdentifiable, B
+            where B : class, IIdentifiable
         {
             Server.Execute((IOperationServer s) => s.Delete(entity, symbol.Symbol, args));
         }
 
         public static void DeleteLite<T, B>(this Lite<T> lite, DeleteSymbol<B> symbol, params object[] args)
-            where T : class, IIdentifiable
-            where B : class, IIdentifiable, T
+            where T : class, IIdentifiable, B
+            where B : class, IIdentifiable
         {
             Server.Execute((IOperationServer s) => s.DeleteLite(lite, symbol.Symbol, args)); 
         }
