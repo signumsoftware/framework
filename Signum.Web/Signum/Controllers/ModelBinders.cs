4610907b207315a6192ea3aefcc877675b1f7358
Olmo del Corral <olmo@signumsoftware.com>
2014-10-06 13:54:17 +0200

    fix LiteModelBinder

--- Signum.Web/Signum/Controllers/ModelBinders.cs
+++ Signum.Web/Signum/Controllers/ModelBinders.cs
@@ -9,6 +9,7 @@ using Signum.Utilities;
 using System.Text.RegularExpressions;
 using Signum.Engine;
 using System.Globalization;
+using Signum.Engine.Maps;
 
 namespace Signum.Web.Controllers
 {
@@ -25,13 +26,15 @@ namespace Signum.Web.Controllers
                      object routeValue = controllerContext.RouteData.Values[bindingContext.ModelName];
                      if (routeValue is Lite<Entity>)
                          return routeValue;
-                     else if(routeValue is int || routeValue is long || routeValue is Guid)
+                     else if (routeValue is PrimaryKey || routeValue is long || routeValue is Guid)
+                         return Lite.Create(cleanType, (PrimaryKey)routeValue);
+                     else if (routeValue is int || routeValue is long || routeValue is Guid)
                          return Lite.Create(cleanType, new PrimaryKey((IComparable)routeValue));
                      else
                          value = (string)routeValue;
                  }
                  PrimaryKey id;
-                 if (PrimaryKey.TryParse(value, cleanType, out id))
+                 if (value.HasText() && Schema.Current.Tables.ContainsKey(cleanType) && PrimaryKey.TryParse(value, cleanType, out id))
                      return Lite.Create(cleanType, id);
 
                  return Lite.Parse(value);
