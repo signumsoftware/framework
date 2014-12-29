b09e5017f80d259c6b90345d2b079e6669cfb93a
Olmo del Corral <olmo@signumsoftware.com>
2014-12-29 23:21:25 +0100

    add Migrations module!

---
+++ Signum.Entities.Extensions/Migrations/Migration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Signum.Utilities;
+
+namespace Signum.Entities.Migrations
+{
+    [Serializable, EntityKind(EntityKind.System, EntityData.Transactional)]
+    public class MigrationEntity : Entity
+    {
+        string versionNumber;
+        public string VersionNumber
+        {
+            get { return versionNumber; }
+            set { Set(ref versionNumber, value); }
+        }
+
+        DateTime executionDate;
+        public DateTime ExecutionDate
+        {
+            get { return executionDate; }
+            set { SetToStr(ref executionDate, value); }
+        }
+    }
+}
