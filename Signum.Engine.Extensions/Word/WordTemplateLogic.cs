450764da4e38a104f87e29907c8be8e95542a5f7
Olmo del Corral <olmo@signumsoftware.com>
2015-01-24 15:29:46 +0100

    fix WordTemplateLogic

--- Signum.Engine.Extensions/Word/WordTemplateLogic.cs
+++ Signum.Engine.Extensions/Word/WordTemplateLogic.cs
@@ -214,7 +214,7 @@ namespace Signum.Engine.Word
 
         static SqlPreCommand Schema_Synchronize_Tokens(Replacements replacements)
         {
-            if (!Database.Query<WordTemplateEntity>().Any() || !SafeConsole.Ask("Synchronize WordTemplates?"))
+            if (!Database.Query<WordTemplateEntity>().Any() || !replacements.Interactive || !SafeConsole.Ask("Synchronize WordTemplates?"))
                 return null;
 
             StringDistance sd = new StringDistance();
