8ad43f13a99a45440e9b6322d04652f5785a7688
Olmo del Corral <o.corral@controlexpert.com>
2015-01-27 13:43:44 +0100

    EntityListCheckboxHelper

---
+++ Signum.Web/Lines/EntityCheckboxList.cs
@@ -0,0 +1,30 @@
+﻿using Signum.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Signum.Web
+{
+    public class EntityListCheckbox : EntityListBase
+    {
+        public IEnumerable<Lite<IEntity>> Data { get; set; }
+        public int? ColumnCount { get; set; }
+        public int? ColumnWidth { get; set; }
+
+        public EntityListCheckbox(Type type, object untypedValue, Context parent, string prefix, PropertyRoute propertyRoute)
+            : base(type, untypedValue, parent, prefix, propertyRoute)
+        {
+            View = false;
+            Create = false;
+            Remove = false;
+            Find = false;
+            Move = false;
+            ColumnWidth = 300;
+        }
+
+        protected override void SetReadOnly()
+        {
+        }
+    }
+}
