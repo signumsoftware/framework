14d05aa565617c0bc8d8d296729d4b984d385e9d
Engin Akin <e.akin@controlexpert.com>
2015-05-26 15:58:50 +0200

    fix EnumSynchronizer

--- Signum.Engine/Engine/SchemaSynchronizer.cs
+++ Signum.Engine/Engine/SchemaSynchronizer.cs
@@ -493,18 +493,27 @@ JOIN {3} {4} ON {2}.{0} = {4}.Id".FormatWith(tabCol.Name,
 
                     if (middleByName.Any())
                     {
-                        var moveToAux = SyncEnums(schema, table, currentByName.Where(a => middleByName.ContainsKey(a.Key)).ToDictionary(), middleByName);
+                        var moveToAux = SyncEnums(schema, table, 
+                            currentByName.Where(a => middleByName.ContainsKey(a.Key)).ToDictionary(), 
+                            middleByName);
                         if (moveToAux != null)
                             commands.Add(moveToAux);
                     }
 
-                    var currentMiddleByName = currentByName.ToDictionary();
-
-                    currentMiddleByName.SetRange(middleByName);
-
-                    var com = SyncEnums(schema, table, currentMiddleByName, shouldByName);
+                    var com = SyncEnums(schema, table, 
+                        currentByName.Where(a=>!middleByName.ContainsKey(a.Key)).ToDictionary(), 
+                        shouldByName.Where(a=>!middleByName.ContainsKey(a.Key)).ToDictionary());
                     if (com != null)
                         commands.Add(com);
+
+                    if (middleByName.Any())
+                    {
+                        var backFromAux = SyncEnums(schema, table, 
+                            middleByName,
+                            shouldByName.Where(a => middleByName.ContainsKey(a.Key)).ToDictionary());
+                        if (backFromAux != null)
+                            commands.Add(backFromAux);
+                    }
                 }
             }
 
@@ -513,11 +522,18 @@ JOIN {3} {4} ON {2}.{0} = {4}.Id".FormatWith(tabCol.Name,
 
         private static SqlPreCommand SyncEnums(Schema schema, Table table, Dictionary<string, Entity> current, Dictionary<string, Entity> should)
         {
-            return Synchronizer.SynchronizeScript(
+            var deletes = Synchronizer.SynchronizeScript(
                        should,
                        current,
-                       (str, s) => table.InsertSqlSync(s),
+                       null,
                        (str, c) => table.DeleteSqlSync(c, comment: c.toStr),
+                       null, Spacing.Double);
+
+            var moves = Synchronizer.SynchronizeScript(
+                       should,
+                       current,
+                       null,
+                       null,
                        (str, s, c) =>
                        {
                            if (s.id == c.id)
@@ -536,6 +552,15 @@ JOIN {3} {4} ON {2}.{0} = {4}.Id".FormatWith(tabCol.Name,
 
                            return SqlPreCommand.Combine(Spacing.Simple, insert, move, delete);
                        }, Spacing.Double);
+
+            var creates = Synchronizer.SynchronizeScript(
+                   should,
+                   current,
+                  (str, s) => table.InsertSqlSync(s),
+                   null,
+                   null, Spacing.Double);
+
+            return SqlPreCommand.Combine(Spacing.Double, deletes, moves, creates);
         }
 
         private static Entity Clone(Entity current)
