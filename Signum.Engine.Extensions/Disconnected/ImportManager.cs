c22f49952c7e09c2fadf7cee4b436d53011db664
Olmo del Corral <olmo@signumsoftware.com>
2015-07-17 23:27:14 +0200

    add ServerSeed in DisconnectedLogic

--- Signum.Engine.Extensions/Disconnected/ImportManager.cs
+++ Signum.Engine.Extensions/Disconnected/ImportManager.cs
@@ -379,7 +379,7 @@ namespace Signum.Engine.Disconnected
             using (Transaction tr = new Transaction())
             {
                 int result;
-                using (PrepareTable(table))
+                using (DisableIdentityIfNecessary(table))
                 {
                     SqlPreCommandSimple sql = InsertTableScript(table, newDatabaseName);
 
@@ -388,7 +388,7 @@ namespace Signum.Engine.Disconnected
 
                 foreach (var rt in table.TablesMList())
                 {
-                    using (PrepareTable(rt))
+                    using (DisableIdentityIfNecessary(rt))
                     {
                         SqlPreCommandSimple rsql = InsertRelationalTableScript(table, newDatabaseName, rt);
 
@@ -400,12 +400,12 @@ namespace Signum.Engine.Disconnected
             }
         }
 
-        protected IDisposable PrepareTable(ITable table)
+        protected IDisposable DisableIdentityIfNecessary(ITable table)
         {
             if (!table.PrimaryKey.Identity)
                 return null;
 
-            return Disposable.Combine(DisconnectedTools.SaveAndRestoreNextId(table), Administrator.DisableIdentity(table.Name));
+            return Administrator.DisableIdentity(table.Name);
         }
 
         protected virtual SqlPreCommandSimple InsertRelationalTableScript(Table table, DatabaseName newDatabaseName, TableMList rt)
@@ -487,7 +487,7 @@ table.PrimaryKey.Name.SqlEscape());
 
                     Executor.ExecuteNonQuery(delete);
 
-                    using (PrepareTable(rt))
+                    using (DisableIdentityIfNecessary(rt))
                     {
                         SqlPreCommandSimple insert = InsertUpdatedRelationalTableScript(machine, table, rt, newDatabaseName);
 
