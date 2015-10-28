03f59592fdbbdb63f0fd88e3616f37de54c65cde
Olmo del Corral <olmo@signumsoftware.com>
2015-10-28 19:33:12 +0100

    add ResourcesController

---
+++ Signum.React/Controllers/ResourcesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Reflection;
+using Signum.Utilities;
+using System.IO;
+using System.Web.Hosting;
+using System.Web.UI;
+using System.Web;
+using System.IO.Compression;
+using Signum.React.PortableAreas;
+
+namespace Signum.React.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        public ActionResult GetFile(string file)
+        {
+            return FileRepositoryManager.GetFile("~/" + file);
+        }
+    }
+}
---
+++ Signum.React/Facades/ServerBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Signum.React.PortableAreas;
+using Signum.Utilities;
+
+namespace Signum.React
+{
+    public class ServerBuilder
+    {
+        public ServerBuilder()
+        {
+            RegisterArea(typeof(ServerBuilder), "signum");
+        }
+
+        public static void RegisterArea(Type clientType,
+            string areaName = null,
+            string controllerNamespace = null,
+            string resourcesNamespace = null)
+        {
+            if (areaName == null)
+                areaName = clientType.Namespace.AfterLast('.');
+
+            if (areaName.Start(1) == "/")
+                throw new SystemException("Invalid start character / in {0}".FormatWith(areaName));
+
+            if (controllerNamespace == null)
+                controllerNamespace = clientType.Namespace;
+
+            if (resourcesNamespace == null)
+                resourcesNamespace = clientType.Namespace;
+
+            var assembly = clientType.Assembly;
+            
+            //SignumControllerFactory.RegisterControllersIn(assembly, controllerNamespace, areaName);
+
+            EmbeddedFilesRepository rep = new EmbeddedFilesRepository(assembly, "~/" + areaName + "/", resourcesNamespace);
+            if (!rep.IsEmpty)
+                FileRepositoryManager.Register(rep);
+        }
+
+    }
+}
