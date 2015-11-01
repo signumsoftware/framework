4528e1fa4f167334972ce51b82b506cc79f389e3
Olmo del Corral <olmo@signumsoftware.com>
2015-11-01 14:41:57 +0100

    add QueryController

---
+++ Signum.React/ApiControllers/QueryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Signum.Engine.Basics;
+using Signum.Engine.DynamicQuery;
+using Signum.Entities.DynamicQuery;
+
+namespace Signum.React.ApiControllers
+{
+    public class QueryController : ApiController
+    {
+        [Route("api/query/description/{queryName}")]
+        public QueryDescription GetQuery(string queryName)
+        {
+            var qn = QueryLogic.TryToQueryName(queryName);
+
+            return DynamicQueryManager.Current.QueryDescription(qn);
+        }
+    }
+}
