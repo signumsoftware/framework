b8fc627d864526831e5638231e9d9c42a55d65a0
Olmo del Corral <olmo@signumsoftware.com>
2015-12-19 21:32:42 +0100

    change setState order for better intellisense

--- Signum.React/Typings/react/react.d.ts
+++ Signum.React/Typings/react/react.d.ts
@@ -117,8 +117,8 @@ declare namespace __React {
     // Base component for plain JS classes
     class Component<P, S> implements ComponentLifecycle<P, S> {
         constructor(props?: P, context?: any);
-        setState(f: (prevState: S, props: P) => S, callback?: () => any): void;
         setState(state: S, callback?: () => any): void;
+        setState(f: (prevState: S, props: P) => S, callback?: () => any): void;
         forceUpdate(callBack?: () => any): void;
         render(): JSX.Element;
         props: P;
