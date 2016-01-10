ec7d6e5b1dcb7cfa813a99e4a1b01c1668f1f9dd
Olmo del Corral <olmo@signumsoftware.com>
2016-01-10 16:05:43 +0100

    add Assembly.CompilationDate

--- Signum.Utilities/Extensions/ReflectionExtensions.cs
+++ Signum.Utilities/Extensions/ReflectionExtensions.cs
@@ -9,6 +9,7 @@ using System.Reflection.Emit;
 using System.Collections;
 using Signum.Utilities.ExpressionTrees;
 using System.Runtime.CompilerServices;
+using System.Collections.Concurrent;
 
 namespace Signum.Utilities
 {
@@ -130,6 +131,42 @@ namespace Signum.Utilities
         public static bool IsStaticClass(this Type type)
         {
             return type.IsAbstract && type.IsSealed;
+        }
+
+
+
+        static ConcurrentDictionary<Assembly, DateTime> CompilationDatesCache = new ConcurrentDictionary<Assembly, DateTime>();
+
+        public static DateTime CompilationDate(this Assembly assembly)
+        {
+            return CompilationDatesCache.GetOrAdd(assembly, a =>
+            {
+                string filePath = a.Location;
+                const int c_PeHeaderOffset = 60;
+                const int c_LinkerTimestampOffset = 8;
+                byte[] b = new byte[2048];
+                System.IO.Stream s = null;
+
+                try
+                {
+                    s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                    s.Read(b, 0, 2048);
+                }
+                finally
+                {
+                    if (s != null)
+                    {
+                        s.Close();
+                    }
+                }
+
+                int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+                int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                dt = dt.AddSeconds(secondsSince1970);
+                dt = dt.ToLocalTime();
+                return dt;
+            });
         }
     }
 }
