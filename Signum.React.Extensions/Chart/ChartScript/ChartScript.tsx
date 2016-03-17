3c8f34cf6b9ad07de566d93c95d3180cd7d2948e
Olmo del Corral <olmo@signumsoftware.com>
2016-03-17 21:15:48 +0100

    initial ChartScript

---
+++ Signum.React.Extensions/Chart/ChartScript/ChartScript.tsx
@@ -0,0 +1,90 @@
+﻿import * as React from 'react'
+import { UserQueryEntity, UserQueryEntity_Type, UserQueryMessage, QueryFilterEntity, QueryOrderEntity, QueryColumnEntity } from '../../UserQueries/Signum.Entities.UserQueries'
+import ChartBuilder from '../Templates/ChartBuilder'
+import { ChartScriptEntity, ChartScriptEntity_Type, ChartScriptColumnEntity, ChartScriptParameterEntity } from '../Signum.Entities.Chart'
+import { FormGroup, FormControlStatic, EntityComponent, ValueLine, ValueLineType, EntityLine, EntityCombo, EntityList, EntityRepeater, EntityFrame} from '../../../../Framework/Signum.React/Scripts/Lines'
+import * as Finder from '../../../../Framework/Signum.React/Scripts/Finder'
+import { QueryDescription, SubTokensOptions } from '../../../../Framework/Signum.React/Scripts/FindOptions'
+import { getQueryNiceName } from '../../../../Framework/Signum.React/Scripts/Reflection'
+import { TypeContext, FormGroupStyle } from '../../../../Framework/Signum.React/Scripts/TypeContext'
+import QueryTokenEntityBuilder from '../../UserAssets/Templates/QueryTokenEntityBuilder'
+
+require("!style!css!../Chart.css");
+
+export default class UserChart extends EntityComponent<ChartScriptEntity> {
+
+    renderEntity() {
+
+        return (
+            <div>
+
+                <div className="row">
+                    <div className="col-sm-11">
+                        <ValueLine ctx={this.subCtx(c => c.name) }  />
+                        {/* @Html.FileLineLite(cc, c => c.Icon, fl =>
+                        {
+                            fl.LabelText = Html.PropertyNiceName(() => cc.Value.Icon);
+                        fl.AttachFunction = ChartClient.ModuleScript["refreshIcon"](fl, imageRoute);
+                        }) */}
+                        <ValueLine ctx={this.subCtx(c => c.groupBy) }  />
+                    </div>
+                    <div className="col-sm-1">
+                            <div className="col-sm-6">
+                                <img src="@Url.Action((FileController fc) => fc.Download(new RuntimeInfo(cc.Value.Icon).ToString()))" />
+                            </div>
+                    </div>
+                </div>
+
+                <div className="sf-chartscript-columns">
+                    <EntityRepeater ctx={this.subCtx(c => c.columns) } getComponent={this.renderColumn}  />
+                </div>
+                <EntityRepeater ctx={this.subCtx(c => c.parameters) } getComponent={this.renderParameter}  />
+                @Html.Partial(Signum.Web.Chart.ChartClient.ChartScriptCodeView, cc)
+            </div>
+        );
+    }
+
+    renderColumn = (ctx: TypeContext<ChartScriptColumnEntity>) => {
+        return (
+            <div>
+                <ValueLine ctx={ctx.subCtx(c => c.displayName) } valueColumns={{ sm: 8 }} />
+                <div className="row">
+                    <div className="col-sm-6">
+                        <ValueLine ctx={ctx.subCtx(c => c.columnType) } labelColumns={{ sm: 4 }} />
+                    </div>
+                    <div className="col-sm-3">
+                        <ValueLine ctx={ctx.subCtx(c => c.isGroupKey) } labelColumns={{ sm: 6 }} />
+                    </div>
+                    <div className="col-sm-3">
+                        <ValueLine ctx={ctx.subCtx(c => c.isOptional) } labelColumns={{ sm: 6 }} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderParameter = (ctx: TypeContext<ChartScriptParameterEntity>) => {
+        var cc = ctx.subCtx({ formGroupStyle: FormGroupStyle.Basic });
+        return (
+            <div>
+                <div className="form-vertical">
+                    <div className="col-sm-2">
+                        <ValueLine ctx={cc.subCtx(c => c.name) }  />
+                    </div>
+
+                    <div className="col-sm-2">
+                        <ValueLine ctx={cc.subCtx(c => c.type) }  />
+                    </div>
+
+                    <div className="col-sm-6">
+                        <ValueLine ctx={cc.subCtx(c => c.valueDefinition) }  />
+                    </div>
+                    <div className="col-sm-2">
+                        <ValueLine ctx={cc.subCtx(c => c.columnIndex) }  />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
---
+++ Signum.React.Extensions/Chart/ChartScript/CodeMirror.tsx
@@ -0,0 +1,87 @@
+﻿/// <reference path="codemirror.d.ts" />
+import * as React from 'react'
+import * as CM from 'codemirror'
+import { Dic, classes } from '../../../../Framework/Signum.React/Scripts/Globals'
+
+
+export interface CodeMirrorProps {
+    onChange: (value: string) => void,
+    onFocusChange: (focused: boolean) => void,
+    options: CM.EditorConfiguration,
+    path: string,
+    value: string,
+    className: string,
+    defaultValue: string;
+}
+
+export default class CodeMirror extends React.Component<CodeMirrorProps, { isFocused: boolean }> {
+
+    codeMirror: CM.EditorFromTextArea;
+    _currentCodemirrorValue: string;
+
+    constructor(props) {
+        super(props);
+        this.state = { isFocused: false, };
+    }
+
+    componentDidMount() {
+        const textareaNode = this.refs["textarea"] as HTMLTextAreaElement;
+        this.codeMirror = CM.fromTextArea(textareaNode, this.props.options);
+        this.codeMirror.on('change', this.codemirrorValueChanged);
+        this.codeMirror.on('focus', this.focusChanged.bind(this, true));
+        this.codeMirror.on('blur', this.focusChanged.bind(this, false));
+        this._currentCodemirrorValue = this.props.defaultValue || this.props.value || '';
+        this.codeMirror.setValue(this._currentCodemirrorValue);
+    }
+    componentWillUnmount() {
+        // todo: is there a lighter-weight way to remove the cm instance?
+        if (this.codeMirror) {
+            this.codeMirror.toTextArea();
+        }
+    }
+    componentWillReceiveProps(nextProps: CodeMirrorProps) {
+        if (this.codeMirror && nextProps.value !== undefined && this._currentCodemirrorValue !== nextProps.value) {
+            this.codeMirror.setValue(nextProps.value);
+        }
+        if (typeof nextProps.options === 'object') {
+            for (let optionName in nextProps.options) {
+                if (nextProps.options.hasOwnProperty(optionName)) {
+                    this.codeMirror.setOption(optionName, nextProps.options[optionName]);
+                }
+            }
+        }
+    }
+    getCodeMirror() {
+        return this.codeMirror;
+    }
+
+    focus() {
+        if (this.codeMirror) {
+            this.codeMirror.focus();
+        }
+    }
+    focusChanged(focused) {
+        this.setState({
+            isFocused: focused,
+        });
+        this.props.onFocusChange && this.props.onFocusChange(focused);
+    }
+
+    codemirrorValueChanged(doc, change) {
+        const newValue = doc.getValue();
+        this._currentCodemirrorValue = newValue;
+        this.props.onChange && this.props.onChange(newValue);
+    }
+    render() {
+        const editorClassName = classes(
+            'ReactCodeMirror',
+            this.state.isFocused ? 'ReactCodeMirror--focused' : null,
+            this.props.className
+        );
+        return (
+            <div className={editorClassName}>
+                <textarea ref="textarea" name={this.props.path} defaultValue={this.props.value} autoComplete="off" />
+            </div>
+        );
+    }
+}
