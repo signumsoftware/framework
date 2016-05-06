434c03fa3c7bb25bb7c32a9f86c1522545ffc1ba
Olmo del Corral <o.corral@controlexpert.com>
2016-05-06 13:21:23 +0200

    remove Typeahead focus

--- Signum.React/Scripts/Lines/Typeahead.tsx
+++ Signum.React/Scripts/Lines/Typeahead.tsx
@@ -189,7 +189,7 @@ export default class Typeahead extends React.Component<TypeaheadProps, Typeahead
     handleMenuClick = (e: React.MouseEvent) => {
         e.preventDefault();
         this.select(e);
-        this.input.focus();
+        //this.input.focus();
     }
 
     mouseover = true;
