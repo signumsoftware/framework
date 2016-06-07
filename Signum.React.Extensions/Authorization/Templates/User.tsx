cbcf826a1b53b3db0d093134dca892c156bae2bd
Olmo del Corral <olmo@signumsoftware.com>
2016-06-07 23:07:16 +0200

    add DoublePassword control

--- Signum.React.Extensions/Authorization/Templates/User.tsx
+++ Signum.React.Extensions/Authorization/Templates/User.tsx
@@ -7,13 +7,45 @@ import { ValueLine, ValueLineType, EntityLine, EntityCombo, FormGroup, TypeConte
 
 
 
-export default class User extends React.Component<{ ctx: TypeContext<UserEntity> }, void> {
+export default class User extends React.Component<{ ctx: TypeContext<UserEntity> }, { withPassword: boolean }> {
 
+    constructor(props) {
+        super(props);
+        this.state = { withPassword: false };
+    }
+
+    render() {
+        const ctx = this.props.ctx;
+        var entity = this.props.ctx.value;
+
+        return (
+            <div>
+                <ValueLine ctx={ctx.subCtx(e => e.state, { readOnly: true }) } />
+                <ValueLine ctx={ctx.subCtx(e => e.userName) } />
+                { entity.isNew || this.state.withPassword ?
+                    <DoublePassword ctx={ctx.subCtx(a => a.passwordHash, { labelColumns: { sm: 4 } }) }/> :
+                    !ctx.readOnly  &&
+                    <a className="btn btn-default btn-sm" onClick={() => this.setState({ withPassword: true }) }>
+                        <i className="fa fa-key"></i> {AuthMessage.ChangePassword.niceToString() }
+                    </a>
+                }
+                <EntityCombo ctx={ctx.subCtx(e => e.role) } />
+                <ValueLine ctx={ctx.subCtx(e => e.email) } />
+                <EntityCombo ctx={ctx.subCtx(e => e.cultureInfo) }/>
+                <ValueLine ctx={ctx.subCtx(e => e.passwordNeverExpires, { labelColumns: { sm: 4 } }) } />
+                <ValueLine ctx={ctx.subCtx(e => e.passwordSetDate, { labelColumns: { sm: 4 } }) } />
+            </div>
+        );
+    }
+}
+
+class DoublePassword extends React.Component<{ ctx: TypeContext<string> }, void>{
+    
     handlePasswordChange = (event: React.SyntheticEvent) => {
 
-        var areDifferent = (this.refs["newPass"] as HTMLInputElement).value != (this.refs["newPass2"] as HTMLInputElement).value;
+        var areDifferent = this.newPass.value != this.newPass2.value;
 
-        if (areDifferent) {
+        if (this.newPass.value && areDifferent) {
             this.props.ctx.frame.setError({ "passwordHash": AuthMessage.PasswordsAreDifferent.niceToString() });
         }
         else {
@@ -23,30 +55,21 @@ export default class User extends React.Component<{ ctx: TypeContext<UserEntity>
 
     }
 
-    render() {
-        const ctx = this.props.ctx;
-        const ph = this.props.ctx.subCtx(a => a.passwordHash, { labelColumns: { sm: 4 } });
-        var entity = this.props.ctx.value;
+    newPass: HTMLInputElement;
+    newPass2: HTMLInputElement;
 
+    render() {
         return (
             <div>
-                <ValueLine ctx={ctx.subCtx(e => e.state, { readOnly: true }) } />
-                <ValueLine ctx={ctx.subCtx(e => e.userName) } />
-                { entity.isNew && <div>
-                    <FormGroup ctx={ ph } labelText={AuthMessage.ChangePasswordAspx_NewPassword.niceToString() }>
-                        <input type="password" ref="newPass" className="form-control" onChange={this.handlePasswordChange}/>
-                    </FormGroup>
-                    <FormGroup ctx={ ph } labelText={AuthMessage.ChangePasswordAspx_ConfirmNewPassword.niceToString() }>
-                        <input type="password" ref="newPass2" className="form-control" onChange={this.handlePasswordChange}/>
-                    </FormGroup>
-                </div>}
-                <EntityCombo ctx={ctx.subCtx(e => e.role) } />
-                <ValueLine ctx={ctx.subCtx(e => e.email) } />
-                <EntityCombo ctx={ctx.subCtx(e => e.cultureInfo) }/>
-                <ValueLine ctx={ctx.subCtx(e => e.passwordNeverExpires, { labelColumns: { sm: 4 } }) } />
-                <ValueLine ctx={ctx.subCtx(e => e.passwordSetDate, { labelColumns: { sm: 4 } }) } />
+                <FormGroup ctx={ this.props.ctx } labelText={AuthMessage.ChangePasswordAspx_NewPassword.niceToString() }>
+                    <input type="password" ref="newPass" className="form-control" onChange={this.handlePasswordChange}/>
+                </FormGroup>
+                <FormGroup ctx={ this.props.ctx } labelText={AuthMessage.ChangePasswordAspx_ConfirmNewPassword.niceToString() }>
+                    <input type="password" ref="newPass2" className="form-control" onChange={this.handlePasswordChange}/>
+                </FormGroup>
             </div>
         );
     }
+
 }
 
