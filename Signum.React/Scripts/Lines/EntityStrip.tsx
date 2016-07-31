750fd2a94c14fb79b3189ba4c351f7006ce039ec
mehdykarimpour <mkarimpour@gmail.com>
2016-07-31 18:13:52 +0430

    add newMListElement

--- Signum.React/Scripts/Lines/EntityStrip.tsx
+++ Signum.React/Scripts/Lines/EntityStrip.tsx
@@ -8,7 +8,7 @@ import { FindOptions } from '../FindOptions'
 import { TypeContext, StyleContext, StyleOptions, FormGroupStyle, mlistItemContext, EntityFrame } from '../TypeContext'
 import { PropertyRoute, PropertyRouteType, MemberInfo, getTypeInfo, getTypeInfos, TypeInfo, IsByAll, ReadonlyBinding, LambdaMemberType } from '../Reflection'
 import { LineBase, LineBaseProps, FormGroup, FormControlStatic, runTasks, } from '../Lines/LineBase'
-import { ModifiableEntity, Lite, Entity, MList, MListElement, EntityControlMessage, JavascriptMessage, toLite, is, liteKey, getToString } from '../Signum.Entities'
+import { ModifiableEntity, Lite, Entity, MList, MListElement, EntityControlMessage, JavascriptMessage, toLite, is, liteKey, getToString, newMListElement } from '../Signum.Entities'
 import Typeahead from '../Lines/Typeahead'
 import { EntityListBase, EntityListBaseProps } from './EntityListBase'
 
@@ -72,7 +72,7 @@ export class EntityStrip extends EntityListBase<EntityStripProps, EntityStripPro
         this.convert(lite)
             .then(e => {
                 const list = this.props.ctx.value;
-                list.push({ rowId: null, element: e });
+                list.push(newMListElement(e));
                 this.setValue(list);
             }).done();
         return "";
