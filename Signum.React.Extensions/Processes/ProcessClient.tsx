efbd490c566e0cb03998986fda6ed7f0824f0a76
Olmo del Corral <o.corral@controlexpert.com>
2016-08-30 10:00:31 +0200

    add ProcessClient

--- Signum.React.Extensions/Processes/ProcessClient.tsx
+++ Signum.React.Extensions/Processes/ProcessClient.tsx
@@ -88,7 +88,7 @@ function monkeyPatchCreateContextualMenuItem(){
         const disabled = !!coc.canExecute;
 
         const onClick = coc.settings && coc.settings.onClick ?
-            (me: React.MouseEvent) => coc.settings.onClick(coc, me) :
+            (me: React.MouseEvent) => coc.settings.onClick!(coc, me) :
             (me: React.MouseEvent) => defaultClick(coc, me);
 
         const menuItem = <MenuItem
