f1f9443f5e6c62fe8b9209af269a0eef89a6d1cb
Olmo del Corral <o.corral@controlexpert.com>
2016-09-16 09:28:06 +0200

    add generateChartExcel

--- Signum.React.Extensions/Excel/ExcelClient.tsx
+++ Signum.React.Extensions/Excel/ExcelClient.tsx
@@ -12,10 +12,13 @@ import { EntityOperationSettings } from '../../../Framework/Signum.React/Scripts
 import { PseudoType, QueryKey, GraphExplorer, OperationType, Type, getTypeName  } from '../../../Framework/Signum.React/Scripts/Reflection'
 import * as Operations from '../../../Framework/Signum.React/Scripts/Operations'
 import * as ContextualOperations from '../../../Framework/Signum.React/Scripts/Operations/ContextualOperations'
-import { ExcelReportEntity } from './Signum.Entities.Excel'
+import { ExcelReportEntity, ExcelMessage } from './Signum.Entities.Excel'
 import * as OmniboxClient from '../Omnibox/OmniboxClient'
 import * as AuthClient from '../Authorization/AuthClient'
+import * as ChartClient from '../Chart/ChartClient'
+import { ChartPermission } from '../Chart/Signum.Entities.Chart'
 import * as QuickLinks from '../../../Framework/Signum.React/Scripts/QuickLinks'
+import { ChartRequest  } from '../../../Extensions/Signum.React.Extensions/Chart/Signum.Entities.Chart'
 import ExcelMenu from './ExcelMenu'
 
 export function start(options: { routes: JSX.Element[], plainExcel: boolean, excelReport: boolean }) {
@@ -31,6 +34,21 @@ export function start(options: { routes: JSX.Element[], plainExcel: boolean, exc
 
         return <ExcelMenu searchControl={ctx.searchControl} plainExcel={options.plainExcel} excelReport={options.excelReport} />;
     }); 
+
+    if (options.plainExcel) {
+        ChartClient.ButtonBarChart.onButtonBarElements.push(ctx => {
+            if (!AuthClient.isPermissionAuthorized(ChartPermission.ViewCharting))
+                return undefined;
+
+            return (
+                <button
+                    className="sf-query-button sf-chart-script-edit btn btn-default"
+                    onClick={() => { API.generateChartExcel(ChartClient.API.cleanedChartRequest(ctx.chartRequest)); } }>
+                    {ExcelMessage.ExcelReport.niceToString()}
+                </button>
+            );
+        });
+    }
 }
 
 export namespace API {
@@ -48,6 +66,12 @@ export namespace API {
 
     export function generateExcelReport(queryRequest: QueryRequest, excelReport: Lite<ExcelReportEntity>): void{
         ajaxPostRaw({ url: "~/api/excel/excelReport" }, { queryRequest, excelReport })
+            .then(response => saveFile(response))
+            .done();
+    }
+
+    export function generateChartExcel(request: ChartRequest): void {
+        ajaxPostRaw({ url: "~/api/excel/plainChart" }, request)
             .then(response => saveFile(response))
             .done();
     }
