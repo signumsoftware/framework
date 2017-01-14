fdb22bce50933f1ba79401df96e79f8dce5b9a35
Olmo del Corral <olmo@signumsoftware.com>
2017-01-14 22:23:38 +0100

    added new machine learning extension.

---
+++ Signum.React.Extensions/MachineLearning/ExcelController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Signum.Engine.Authorization;
+using Signum.Entities;
+using Signum.Entities.Authorization;
+using Signum.Services;
+using Signum.Utilities;
+using Signum.React.Facades;
+using Signum.React.Authorization;
+using Signum.Engine.Cache;
+using Signum.Engine;
+using Signum.Entities.Cache;
+using Signum.Utilities.ExpressionTrees;
+using Signum.Entities.Processes;
+using Signum.Engine.Processes;
+using System.Threading;
+using Signum.React.ApiControllers;
+using Signum.Engine.Basics;
+using System.Web;
+using Signum.React.Files;
+using System.IO;
+using Signum.Engine.DynamicQuery;
+using Signum.Entities.DynamicQuery;
+using Signum.Entities.Chart;
+using Signum.Engine.Chart;
+
+namespace Signum.React.MachineLearning
+{
+    public class MachineLearningController : ApiController
+    {
+
+        //[Route("api/excel/reportsFor/{queryKey}"), HttpGet]
+        //public IEnumerable<Lite<ExcelReportEntity>> GetExcelReports(string queryKey)
+        //{
+        //    return ExcelLogic.GetExcelReports(QueryLogic.ToQueryName(queryKey));
+        //}
+
+    }
+}
---
+++ Signum.React.Extensions/MachineLearning/PredictorClient.tsx
@@ -0,0 +1,34 @@
+﻿import * as React from 'react'
+import { Route } from 'react-router'
+import { Dic, classes } from '../../../Framework/Signum.React/Scripts/Globals';
+import { Button, OverlayTrigger, Tooltip, MenuItem } from "react-bootstrap"
+import { ajaxPost, ajaxPostRaw, ajaxGet, saveFile } from '../../../Framework/Signum.React/Scripts/Services';
+import { EntitySettings, ViewPromise } from '../../../Framework/Signum.React/Scripts/Navigator'
+import * as Navigator from '../../../Framework/Signum.React/Scripts/Navigator'
+import * as Finder from '../../../Framework/Signum.React/Scripts/Finder'
+import { QueryRequest } from '../../../Framework/Signum.React/Scripts/FindOptions'
+import { Lite, Entity, EntityPack, ExecuteSymbol, DeleteSymbol, ConstructSymbol_From, registerToString, JavascriptMessage, toLite } from '../../../Framework/Signum.React/Scripts/Signum.Entities'
+import { EntityOperationSettings } from '../../../Framework/Signum.React/Scripts/Operations'
+import { PseudoType, QueryKey, GraphExplorer, OperationType, Type, getTypeName  } from '../../../Framework/Signum.React/Scripts/Reflection'
+import * as Operations from '../../../Framework/Signum.React/Scripts/Operations'
+import * as ContextualOperations from '../../../Framework/Signum.React/Scripts/Operations/ContextualOperations'
+import { PredictorEntity } from './Signum.Entities.MachineLearning'
+import * as OmniboxClient from '../Omnibox/OmniboxClient'
+import * as AuthClient from '../Authorization/AuthClient'
+import * as ChartClient from '../Chart/ChartClient'
+import { ChartPermission } from '../Chart/Signum.Entities.Chart'
+import * as QuickLinks from '../../../Framework/Signum.React/Scripts/QuickLinks'
+import { ChartRequest  } from '../../../Extensions/Signum.React.Extensions/Chart/Signum.Entities.Chart'
+
+export function start(options: { routes: JSX.Element[] }) {
+
+    Navigator.addSettings(new EntitySettings(PredictorEntity, e => new ViewPromise(resolve => require(['./Templates/Predictor'], resolve))));
+}
+
+export namespace API {
+    
+    //export function forQuery(queryKey: string): Promise<Lite<ExcelReportEntity>[]> {
+    //    return ajaxGet<Lite<ExcelReportEntity>[]>({ url: "~/api/excel/reportsFor/" + queryKey });
+    //}
+    
+}
