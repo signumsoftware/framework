7305c7a7435b69585467ed05faa486dc82318354
Mehdy Karimpour <mkarimpour@gmail.com>
2017-01-29 20:00:25 +0330

    add HtmlEditor

---
+++ Signum.React.Extensions/HtmlEditor/HtmlEditor.tsx
@@ -0,0 +1,38 @@
+﻿/// <reference path="react-rte.d.ts" />
+import * as React from 'react'
+import RichTextEditor, { EditorValue } from 'react-rte';
+import { IBinding } from '../../../Framework/Signum.React/Scripts/Reflection';
+
+export interface HtmlEditorProps {
+    binding: IBinding<string | null | undefined>;
+    readonly?: boolean;
+}
+
+export default class HtmlEditor extends React.Component<HtmlEditorProps, { editorValue: EditorValue }>{
+    constructor(props: HtmlEditorProps) {
+        super(props);
+
+        this.state = { editorValue: RichTextEditor.createValueFromString(props.binding.getValue() || "", "html") };
+    }
+
+    componentWillUnmount() {
+        this.saveHtml();
+    }
+
+    saveHtml() {
+        if (!this.props.readonly)
+            this.props.binding.setValue(this.state.editorValue.toString("html") || "");
+    }
+
+    render() {
+        return (
+            <RichTextEditor
+                value={this.state.editorValue}
+                readOnly={this.props.readonly}
+                onChange={ev => this.setState({ editorValue: ev })}
+                onBlur={() => this.saveHtml()}
+                />
+        );
+    }
+}
+
---
+++ Signum.React.Extensions/HtmlEditor/react-rte.d.ts
@@ -0,0 +1,24 @@
+﻿
+
+declare module 'react-rte' {
+
+    export type EditorFormat = "html" | "markdown";
+    export interface EditorValue {
+        toString(format: EditorFormat): string;
+    }
+
+    export interface RichTextEditorProps {
+        value: EditorValue;
+        onChange: (value: EditorValue) => void;
+        onBlur?: () => void;
+        autoFocus?: boolean;
+        placeholder?: string;
+        readOnly?: boolean;
+    }
+
+
+    export default class RichTextEditor extends __React.Component<RichTextEditorProps, {}>{
+        static createEmptyValue(): EditorValue;
+        static createValueFromString(markup: string, format: EditorFormat): EditorValue;
+    }
+}
