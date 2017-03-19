cd0ecfd68e4731238147b46a848ccd393d14b225
Olmo del Corral <olmo@signumsoftware.com>
2017-03-19 13:51:17 +0100

    add SnippetTS

--- Signum.React/Scripts/Exceptions/Exception.tsx
+++ Signum.React/Scripts/Exceptions/Exception.tsx
@@ -1,6 +1,6 @@
 ﻿import * as React from 'react'
 import * as moment from 'moment'
-import { Tabs, Tab} from 'react-bootstrap'
+import { Tabs, Tab } from 'react-bootstrap'
 import { ExceptionEntity } from '../Signum.Entities.Basics'
 import { ValueLine, ValueLineType, EntityLine, TypeContext } from '../Lines'
 
@@ -12,32 +12,32 @@ export default class Exception extends React.Component<{ ctx: TypeContext<Except
             <div>
                 <div className="row">
                     <div className="col-sm-6">
-                        <ValueLine ctx={sc.subCtx(f => f.environment) } />
-                        <ValueLine ctx={sc.subCtx(f => f.creationDate) } unitText={moment(sc.value.creationDate!).toUserInterface().fromNow() } />
-                        <EntityLine ctx={sc.subCtx(f => f.user) } />
-                        <ValueLine ctx={sc.subCtx(f => f.version) } />
-                        <ValueLine ctx={sc.subCtx(f => f.threadId) } />
-                        <ValueLine ctx={sc.subCtx(f => f.machineName) } />
-                        <ValueLine ctx={sc.subCtx(f => f.applicationName) } />
+                        <ValueLine ctx={sc.subCtx(f => f.environment)} />
+                        <ValueLine ctx={sc.subCtx(f => f.creationDate)} unitText={moment(sc.value.creationDate!).toUserInterface().fromNow()} />
+                        <EntityLine ctx={sc.subCtx(f => f.user)} />
+                        <ValueLine ctx={sc.subCtx(f => f.version)} />
+                        <ValueLine ctx={sc.subCtx(f => f.threadId)} />
+                        <ValueLine ctx={sc.subCtx(f => f.machineName)} />
+                        <ValueLine ctx={sc.subCtx(f => f.applicationName)} />
                     </div>
                     <div className="col-sm-6">
-                        <ValueLine ctx={sc.subCtx(f => f.actionName) } />
-                        <ValueLine ctx={sc.subCtx(f => f.controllerName) } />
-                        <ValueLine ctx={sc.subCtx(f => f.userHostAddress) } />
+                        <ValueLine ctx={sc.subCtx(f => f.actionName)} />
+                        <ValueLine ctx={sc.subCtx(f => f.controllerName)} />
+                        <ValueLine ctx={sc.subCtx(f => f.userHostAddress)} />
                         <ValueLine ctx={sc.subCtx(f => f.userHostName)} />
                         <ValueLine ctx={sc.subCtx(f => f.userAgent)} valueLineType="TextArea" />
                     </div>
                 </div>
-                <ValueLine ctx={ctx.subCtx(f => f.requestUrl) } />
-                <ValueLine ctx={ctx.subCtx(f => f.urlReferer) } />
-                <h3 style={ { color: "rgb(139, 0, 0)" } }>{ctx.value.exceptionType}</h3>
+                <ValueLine ctx={ctx.subCtx(f => f.requestUrl)} />
+                <ValueLine ctx={ctx.subCtx(f => f.urlReferer)} />
+                <h3 style={{ color: "rgb(139, 0, 0)" }}>{ctx.value.exceptionType}</h3>
                 <pre><code>{ctx.value.exceptionMessage}</code></pre>
                 <Tabs id="exceptionTabs">
-                    { this.codeTab(0, a => a.stackTrace) }
-                    { this.codeTab(1, a => a.data) }
-                    { this.codeTab(2, a => a.queryString) }
-                    { this.codeTab(3, a => a.form) }
-                    { this.codeTab(4, a => a.session) }
+                    {this.codeTab(0, a => a.stackTrace)}
+                    {this.codeTab(1, a => a.data)}
+                    {this.codeTab(2, a => a.queryString)}
+                    {this.codeTab(3, a => a.form)}
+                    {this.codeTab(4, a => a.session)}
                 </Tabs>
             </div>
         );
@@ -56,4 +56,3 @@ export default class Exception extends React.Component<{ ctx: TypeContext<Except
         </Tab>;
     }
 }
-
