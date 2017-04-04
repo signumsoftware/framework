1706426ec26df120214f24be1d3f4432cf41776a
Olmo del Corral <o.corral@controlexpert.com>
2017-04-04 09:19:38 +0200

    rename RunProcessEntity -> ExecutedLoadProcessEntity and create MigrationLogic.ExecutedLoadProcess

---
+++ Signum.Entities.Extensions/Migrations/ExecutedLoadProcess.cs
@@ -0,0 +1,49 @@
+﻿using Signum.Entities;
+using Signum.Entities.Basics;
+using Signum.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signum.Entities.Migrations
+{
+    [Serializable, EntityKind(EntityKind.System, EntityData.Transactional), TicksColumn(false)]
+    public class ExecutedLoadProcessEntity : Entity
+    {
+        [SqlDbType(Size = 400)]
+        [StringLengthValidator(AllowNulls = true, Min = 3, Max = 400)]
+        public string MethodName { get; set; }
+
+        [SqlDbType(Size = 400)]
+        [StringLengthValidator(AllowNulls = true, Min = 3, Max = 400)]
+        public string ClassName { get; set; }
+
+        [SqlDbType(Size = 400)]
+        [StringLengthValidator(AllowNulls = true, Min = 3, Max = 400)]
+        public string Description { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime? End { get; set; }
+
+        static Expression<Func<ExecutedLoadProcessEntity, double?>> DurationExpression =
+            log => (double?)(log.End - log.Start).Value.TotalMilliseconds;
+        [ExpressionField("DurationExpression"), Unit("ms")]
+        public double? Duration
+        {
+            get { return End == null ? null : DurationExpression.Evaluate(this); }
+        }
+
+        public Lite<ExceptionEntity> Exception{ get; set; }
+
+        static Expression<Func<ExecutedLoadProcessEntity, string>> ToStringExpression = e => e.MethodName;
+        [ExpressionField]
+        public override string ToString()
+        {
+            return ToStringExpression.Evaluate(this);
+        }
+    }
+}
