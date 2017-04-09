507f2b56eb0fb7f66cca5ac8ae5221dda20d4c23
Mehdy Karimpour <mkarimpour@gmail.com>
2017-04-09 12:22:30 +0430

    fix decimal attributes in DynamicType

--- Signum.Entities.Extensions/Dynamic/DynamicType.cs
+++ Signum.Entities.Extensions/Dynamic/DynamicType.cs
@@ -350,6 +350,9 @@ namespace Signum.Entities.Dynamic
 
         string Value(object obj)
         {
+            if (obj is decimal)
+                obj = (double)(decimal)obj;
+
             return CSharpRenderer.Value(obj, obj.GetType(), null);
         }
 
