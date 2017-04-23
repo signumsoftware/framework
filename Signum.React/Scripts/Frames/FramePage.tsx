3bf7a45a3cf3cb6a9fbd9bdaba487c28b2547d7d
Mehdy Karimpour <mkarimpour@gmail.com>
2017-04-23 14:11:43 +0430

    remove passing frame

--- Signum.React/Scripts/Frames/FramePage.tsx
+++ Signum.React/Scripts/Frames/FramePage.tsx
@@ -167,7 +167,7 @@ export default class FramePage extends React.Component<FramePageProps, FramePage
                 <ValidationErrors entity={this.state.pack.entity} ref={ve => this.validationErrors = ve}/>
                 { embeddedWidgets.top }
                 <div className="sf-main-control form-horizontal" data-test-ticks={new Date().valueOf() } data-main-entity={entityInfo(ctx.value) }>
-                    {this.state.getComponent && React.cloneElement(this.state.getComponent(ctx),  { ref: (c: React.Component<any, any>) => this.setComponent(c), frame }) }
+                    {this.state.getComponent && React.cloneElement(this.state.getComponent(ctx),  { ref: (c: React.Component<any, any>) => this.setComponent(c) }) }
                 </div>
                 { embeddedWidgets.bottom }
             </div>
