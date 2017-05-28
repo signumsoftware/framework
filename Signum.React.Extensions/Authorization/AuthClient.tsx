1b063ba88c8730ed0ec71fa13fa0e954da80b8e2
karimpour <mkarimpour@gmail.com>
2017-05-28 12:35:37 +0430

    better exception message in isPermissionAuthorized

--- Signum.React.Extensions/Authorization/AuthClient.tsx
+++ Signum.React.Extensions/Authorization/AuthClient.tsx
@@ -286,7 +286,14 @@ export namespace Options {
 
 export function isPermissionAuthorized(permission: PermissionSymbol | string) {
     var key = (permission as PermissionSymbol).key || permission as string;
-    const member = getTypeInfo(key.before(".")).members[key.after(".")];
+    const type = getTypeInfo(key.before("."));
+    if (!type)
+        throw new Error(`Type '${key.before(".")}' not found. Consider adding PermissionAuthLogic.RegisterPermissions(${key}) and Synchronize`);
+
+    const member = type.members[key.after(".")];
+    if (!member)
+        throw new Error(`Member '${key.after(".")}' not found. Consider adding PermissionAuthLogic.RegisterPermissions(${key}) and Synchronize`);
+
     return member.permissionAllowed;
 }
 
