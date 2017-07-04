463201fa1f726bc189444062e5e99a08ac18351c
Olmo del Corral <o.corral@controlexpert.com>
2017-07-04 12:46:25 +0200

    remove BingTranslator and add AzureTranslator

---
+++ Signum.Engine.Extensions/Translation/AzureTranslator.cs
@@ -0,0 +1,243 @@
+﻿using Signum.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Signum.Engine.Translation
+{
+    //https://msdn.microsoft.com/en-us/library/ff512422.aspx
+    public class AzureTranslator : ITranslator
+    {
+        public string AzureKey;
+        public AzureTranslator(string azureKey)
+        {
+            this.AzureKey = azureKey;
+        }
+
+        public async Task<List<string>> TranslateBatchAsync(List<string> list, string from, string to)
+        {
+            var authTokenSource = new AzureAuthToken(AzureKey);
+            string authToken = authTokenSource.GetAccessToken();           
+
+
+            var uri = "https://api.microsofttranslator.com/v2/Http.svc/TranslateArray";
+            var body = "<TranslateArrayRequest>" +
+                           "<AppId />" +
+                           "<From>{0}</From>" +
+                           "<Options>" +
+                           " <Category xmlns=\"http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2\" />" +
+                               "<ContentType xmlns=\"http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2\">{1}</ContentType>" +
+                               "<ReservedFlags xmlns=\"http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2\" />" +
+                               "<State xmlns=\"http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2\" />" +
+                               "<Uri xmlns=\"http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2\" />" +
+                               "<User xmlns=\"http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2\" />" +
+                           "</Options>" +
+                           "<Texts>{2}</Texts>" +
+                           "<To>{3}</To>" +
+                       "</TranslateArrayRequest>";
+
+            var translatedArrayElements = list
+                .Select(s => "<string xmlns=\"http://schemas.microsoft.com/2003/10/Serialization/Arrays\">{0}</string>".FormatWith(EncodeHtml(s)))
+                .ToString(s => s, "");
+
+            string requestBody = string.Format(body, from, "text/html", translatedArrayElements, to);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "text/xml");
+                request.Headers.Add("Authorization", authToken);
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                        Console.WriteLine("Request status is OK. Result of translate array method is:");
+                        var doc = XDocument.Parse(responseBody);
+                        var ns = XNamespace.Get("http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2");
+                        var sourceTextCounter = 0;
+                        var result = new List<string>();
+                        foreach (XElement xe in doc.Descendants(ns + "TranslateArrayResponse"))
+                        {
+                            foreach (var node in xe.Elements(ns + "TranslatedText"))
+                            {
+                                //Console.WriteLine("\n\nSource text: {0}\nTranslated Text: {1}", translateArraySourceTexts[sourceTextCounter], node.Value);
+                                result.Add(node.Value);
+                            }
+                            sourceTextCounter++;
+                        }
+                        return result;
+                    default:
+                        Console.WriteLine("Request status code is: {0}.", response.StatusCode);
+                        Console.WriteLine("Request error message: {0}.", responseBody);
+                        return null;
+                }
+            }
+        }
+
+        private string EncodeHtml(string origin)
+        {
+            if (origin == null)
+                return null;
+            else
+                return origin.Replace("&", "&amp;")
+                    .Replace("\"", "&quot;")
+                    .Replace("'", "&apos;")
+                    .Replace("<", "&lt;")
+                    .Replace(">", "&gt;");
+        }
+
+        public List<string> TranslateBatch(List<string> list, string from, string to)
+        {
+            List<string> result = null;
+            var task = Task.Run(async () =>
+            {
+                result = await this.TranslateBatchAsync(list, from, to);
+            });
+
+            while (!task.IsCompleted)
+            {
+                System.Threading.Thread.Yield();
+            }
+            if (task.IsFaulted)
+            {
+                throw task.Exception;
+            }
+            if (task.IsCanceled)
+            {
+                throw new Exception("Timeout obtaining access token.");
+            }
+            return result;
+        }
+
+        public bool AutoSelect()
+        {
+            return true;
+        }
+    } // BingTranslator now as Azure suscription
+
+    public class AzureAuthToken
+    {
+        /// URL of the token service
+        private static readonly Uri ServiceUrl = new Uri("https://api.cognitive.microsoft.com/sts/v1.0/issueToken");
+        /// Name of header used to pass the subscription key to the token service
+        private const string OcpApimSubscriptionKeyHeader = "Ocp-Apim-Subscription-Key";
+        /// After obtaining a valid token, this class will cache it for this duration.
+        /// Use a duration of 5 minutes, which is less than the actual token lifetime of 10 minutes.
+        private static readonly TimeSpan TokenCacheDuration = new TimeSpan(0, 5, 0);
+
+        /// Cache the value of the last valid token obtained from the token service.
+        private string storedTokenValue = string.Empty;
+        /// When the last valid token was obtained.
+        private DateTime storedTokenTime = DateTime.MinValue;
+
+        /// Gets the subscription key.
+        public string SubscriptionKey { get; private set; } = string.Empty;
+
+        /// Gets the HTTP status code for the most recent request to the token service.
+        public HttpStatusCode RequestStatusCode { get; private set; }
+
+        /// <summary>
+        /// Creates a client to obtain an access token.
+        /// </summary>
+        /// <param name="key">Subscription key to use to get an authentication token.</param>
+        public AzureAuthToken(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException("key", "A subscription key is required");
+            }
+
+            this.SubscriptionKey = key;
+            this.RequestStatusCode = HttpStatusCode.InternalServerError;
+        }
+
+        /// <summary>
+        /// Gets a token for the specified subscription.
+        /// </summary>
+        /// <returns>The encoded JWT token prefixed with the string "Bearer ".</returns>
+        /// <remarks>
+        /// This method uses a cache to limit the number of request to the token service.
+        /// A fresh token can be re-used during its lifetime of 10 minutes. After a successful
+        /// request to the token service, this method caches the access token. Subsequent 
+        /// invocations of the method return the cached token for the next 5 minutes. After
+        /// 5 minutes, a new token is fetched from the token service and the cache is updated.
+        /// </remarks>
+        public async Task<string> GetAccessTokenAsync()
+        {
+            if (SubscriptionKey == string.Empty) return string.Empty;
+
+            // Re-use the cached token if there is one.
+            if ((DateTime.Now - storedTokenTime) < TokenCacheDuration)
+            {
+                return storedTokenValue;
+            }
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method =  HttpMethod.Post;
+                request.RequestUri = ServiceUrl;
+                request.Content = new StringContent(string.Empty);
+                request.Headers.TryAddWithoutValidation(OcpApimSubscriptionKeyHeader, this.SubscriptionKey);
+                client.Timeout = TimeSpan.FromSeconds(2);
+                var response = await client.SendAsync(request);
+                this.RequestStatusCode = response.StatusCode;
+                response.EnsureSuccessStatusCode();
+                var token = await response.Content.ReadAsStringAsync();
+                storedTokenTime = DateTime.Now;
+                storedTokenValue = "Bearer " + token;
+                return storedTokenValue;
+            }
+        }
+
+        /// <summary>
+        /// Gets a token for the specified subscription. Synchronous version.
+        /// Use of async version preferred
+        /// </summary>
+        /// <returns>The encoded JWT token prefixed with the string "Bearer ".</returns>
+        /// <remarks>
+        /// This method uses a cache to limit the number of request to the token service.
+        /// A fresh token can be re-used during its lifetime of 10 minutes. After a successful
+        /// request to the token service, this method caches the access token. Subsequent 
+        /// invocations of the method return the cached token for the next 5 minutes. After
+        /// 5 minutes, a new token is fetched from the token service and the cache is updated.
+        /// </remarks>
+        public string GetAccessToken()
+        {
+            // Re-use the cached token if there is one.
+            if ((DateTime.Now - storedTokenTime) < TokenCacheDuration)
+            {
+                return storedTokenValue;
+            }
+
+            string accessToken = null;
+            var task = Task.Run(async () =>
+            {
+                accessToken = await GetAccessTokenAsync();
+            });
+
+            while (!task.IsCompleted)
+            {
+                System.Threading.Thread.Yield();
+            }
+            if (task.IsFaulted)
+            {
+                throw task.Exception;
+            }
+            else if (task.IsCanceled)
+            {
+                throw new Exception("Timeout obtaining access token.");
+            }
+            return accessToken;
+        }
+
+    }
+}
