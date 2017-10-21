f707e73d94cfd14ef51e79e5a25c14852926c466
Olmo del Corral <olmo@signumsoftware.com>
2017-10-21 08:56:32 +0200

    partial change Login.tsx

--- Signum.React.Extensions/Authorization/Login/Login.tsx
+++ Signum.React.Extensions/Authorization/Login/Login.tsx
@@ -50,66 +50,75 @@ export default class Login extends React.Component<{}, { modelState?: ModelState
     render() {
 
         return (
-            <form onSubmit={(e) => this.handleSubmit(e)}>
-
-                <div>
-
-                    <div className="form-group">
-                        <div className="col-sm-offset-4 col-sm-6">
+            <div className="container">
+                <form className="form-horizontal" onSubmit={(e) => this.handleSubmit(e)}>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6">
                             <h2>Login</h2>
                             <p>{AuthMessage.EnterYourUserNameAndPassword.niceToString()}</p>
+                            <hr />
                         </div>
                     </div>
-
-                    <div className={classes("form-group", this.error("userName") ? "has-error" : undefined)}>
-                        <label htmlFor="userName" className="col-sm-offset-2 col-sm-2 control-label">{AuthMessage.Username.niceToString()}</label>
-                        <div className="col-sm-4">
-                            <div className="input-group">
-                                <span className="input-group-addon"><i className="fa fa-user"></i></span>
-                                <input type="text" className="form-control" id="userName" ref={r => this.userName = r!} placeholder={AuthMessage.Username.niceToString()} />
-                            </div>
-                            {this.error("userName") && <span className="help-block">{this.error("userName")}</span>}
-                        </div>
-                    </div>
-
-                    <div className={classes("form-group", this.error("password") ? "has-error" : undefined)}>
-                        <label htmlFor="password" className="col-sm-offset-2 col-sm-2 control-label">{AuthMessage.Password.niceToString()}</label>
-                        <div className="col-sm-4">
-                            <div className="input-group">
-                                <span className="input-group-addon"><i className="fa fa-lock"></i></span>
-                                <input type="password" className="form-control" id="password" ref={r => this.password = r!} placeholder={AuthMessage.Password.niceToString()} />
-                            </div>
-                            {this.error("password") && <span className="help-block">{this.error("password")}</span>}
-                        </div>
-                    </div>
-
-                    {AuthClient.userTicket &&
-                        <div className="form-group">
-                            <div className="col-sm-offset-4 col-sm-6">
-                                <div className="checkbox">
-                                    <label> <input type="checkbox" ref={r => this.rememberMe = r!} />{AuthMessage.RememberMe.niceToString()}</label>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6">
+                            <div className={classes("form-group", this.error("userName") && "has-danger")}>
+                                <label className="sr-only" htmlFor="userName">{AuthMessage.Username.niceToString()}</label>
+                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                                    <div className="input-group-addon" style={{ width: "2.6rem" }}><i className="fa fa-user"></i></div>
+                                    <input type="text" className="form-control" id="userName" ref={r => this.userName = r!} placeholder={AuthMessage.Username.niceToString()} />
                                 </div>
                             </div>
                         </div>
-                    }
-
-
-                    <div className="form-group">
-                        <div className="col-sm-offset-4 col-sm-6">
-                            <button className="btn btn-primary" ref="login" id="login" type="submit">{AuthMessage.Login.niceToString()}</button>
-                        </div>
-                    </div>
-
-                    {AuthClient.resetPassword &&
-                        <div className="form-group">
-                            <div className="col-sm-offset-4 col-sm-6">
-                                <Link to="~/auth/resetPassword">{AuthMessage.IHaveForgottenMyPassword.niceToString()}</Link>
+                        <div className="col-md-3">
+                            <div className="form-control-feedback">
+                                <span className="text-danger align-middle">
+                                    <i className="fa fa-close"></i> Example error message
+                        </span>
                             </div>
                         </div>
-                    }
-                </div>
-            </form>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label className="sr-only" htmlFor="password">Password</label>
+                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                                    <div className="input-group-addon" style={{ width: "2.6rem" }}><i className="fa fa-key"></i></div>
+                                    <input type="password" name="password" className="form-control" id="password"
+                                        placeholder="Password" required />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="form-control-feedback">
+                                <span className="text-danger align-middle">
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6" style={{ paddingTop: ".35rem" }}>
+                            <div className="form-check mb-2 mr-sm-2 mb-sm-0">
+                                <label className="form-check-label">
+                                    <input className="form-check-input" name="remember"
+                                        type="checkbox" />
+                                    <span style={{ paddingBottom: ".15rem" }}>Remember me</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row" style={{ paddingTop: "1rem" }}>
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6">
+                            <button type="submit" className="btn btn-success"><i className="fa fa-sign-in"></i> Login</button>
+                            <a className="btn btn-link" href="/password/reset">Forgot Your Password?</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
         );
     }
-
 }
