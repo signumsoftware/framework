4efabca607fa40f7708cd62093090b5b2b1bafec
jbu <j.burchartz@controlexpert.com>
2018-02-05 12:47:19 +0100

    Added VersionFilterAttribute

---
+++ Signum.React/Filters/VersionFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Signum.React.Filters
+{
+    public class VersionFilterAttribute : ActionFilterAttribute
+    {
+        public static Assembly CustomAssembly;
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionContext)
+        {
+            base.OnActionExecuted(actionContext);
+
+            var version = CustomAssembly != null ? CustomAssembly.GetName().Version : Assembly.GetExecutingAssembly().GetName().Version;
+
+            actionContext.Response.Headers.Add("X-App-Version", version.ToString());
+        }
+    }
+}
