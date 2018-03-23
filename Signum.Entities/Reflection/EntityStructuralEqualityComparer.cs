afbf7c29c73819a27c62ca59b9d90afbe26adc07
Olmo del Corral <o.corral@controlexpert.com>
2018-03-23 21:13:38 +0100

    finish ScturalEqualityComparer useful

--- Signum.Entities/Reflection/EntityStructuralEqualityComparer.cs
+++ Signum.Entities/Reflection/EntityStructuralEqualityComparer.cs
@@ -16,23 +16,32 @@ namespace Signum.Entities.Reflection
         IEqualityComparer GetEqualityComparer(Type type, PropertyInfo pi);
     }
 
-    public class EntityStructuralEqualityComparer<T> : EqualityComparer<T> 
+    public interface ICompletableComparer
+    {
+        void Complete(IEqualityComparerResolver resolver, PropertyInfo pi);
+    }  
+
+    public class EntityStructuralEqualityComparer<T> : EqualityComparer<T>, ICompletableComparer
         where T : ModifiableEntity
     {
-        public Dictionary<string, PropertyComparer> Properties;
-        public Dictionary<Type, IEqualityComparer> Mixins; 
-        
-        public EntityStructuralEqualityComparer(IEqualityComparerResolver resolver)
+        public Dictionary<string, PropertyComparer<T>> Properties;
+        public Dictionary<Type, IEqualityComparer> Mixins;
+
+        public EntityStructuralEqualityComparer()
         {
+        }
+
+        public void Complete(IEqualityComparerResolver resolver, PropertyInfo pi)
+        { 
             Properties = MemberEntryFactory.GenerateList<T>(MemberOptions.Properties | MemberOptions.Typed | MemberOptions.Getter)
                 .Where(p => !((PropertyInfo)p.MemberInfo).HasAttribute<HiddenPropertyAttribute>())
-               .ToDictionary(a => a.Name, a => new PropertyComparer((PropertyInfo)a.MemberInfo, a.Getter)
+               .ToDictionary(a => a.Name, a => new PropertyComparer<T>((PropertyInfo)a.MemberInfo, a.Getter)
                {
                    Comparer = resolver.GetEqualityComparer(((PropertyInfo)a.MemberInfo).PropertyType, ((PropertyInfo)a.MemberInfo))
                });
 
             Mixins = !typeof(Entity).IsAssignableFrom(typeof(T)) ? null :
-                MixinDeclarations.GetMixinDeclarations(typeof(T)).ToDictionary(t => t, t => resolver.GetEqualityComparer(t, null)); 
+                MixinDeclarations.GetMixinDeclarations(typeof(T)).ToDictionary(t => t, t => resolver.GetEqualityComparer(t, null));
         }
 
         public override bool Equals(T x, T y)
@@ -49,9 +58,9 @@ namespace Signum.Entities.Reflection
                     return false;
             }
 
-            if(Mixins != null)
+            if (Mixins != null)
             {
-                var ex = (Entity)(ModifiableEntity)x; 
+                var ex = (Entity)(ModifiableEntity)x;
                 var ey = (Entity)(ModifiableEntity)y;
 
                 foreach (var kvp in Mixins)
@@ -61,10 +70,10 @@ namespace Signum.Entities.Reflection
                 }
             }
 
-            return true; 
-          
+            return true;
+
         }
-   
+
         public override int GetHashCode(T obj)
         {
             if (obj == null)
@@ -87,31 +96,89 @@ namespace Signum.Entities.Reflection
 
             return result;
         }
-
-        public class PropertyComparer
-        {
-            public PropertyInfo MemberInfo { get; set; }
-            public Func<T, object> Getter { get; set; }
-            public IEqualityComparer Comparer { get; set; }
-
-            public PropertyComparer(PropertyInfo memberInfo, Func<T, object> getter)
-            {
-                this.Getter = getter;
-                this.MemberInfo = memberInfo;
-            }
-        }
     }
 
-    public class SortedMListEqualityComparer<T> : EqualityComparer<MList<T>>
+    public class ClassStructuralEqualityComparer<T> : EqualityComparer<T>, ICompletableComparer
+    {
+        public Dictionary<string, PropertyComparer<T>> Properties;
+
+        public ClassStructuralEqualityComparer()
+        {
+        }
+
+        public void Complete(IEqualityComparerResolver resolver, PropertyInfo pi)
+        {
+            Properties = MemberEntryFactory.GenerateList<T>(MemberOptions.Properties | MemberOptions.Typed | MemberOptions.Getter)
+              .ToDictionary(a => a.Name, a => new PropertyComparer<T>((PropertyInfo)a.MemberInfo, a.Getter)
+              {
+                  Comparer = resolver.GetEqualityComparer(((PropertyInfo)a.MemberInfo).PropertyType, ((PropertyInfo)a.MemberInfo))
+              }); ;
+        }
+
+        public override bool Equals(T x, T y)
+        {
+            if ((x == null) != (y == null))
+                return false;
+
+            if (x.GetType() != y.GetType())
+                return false;
+
+            foreach (var p in Properties.Values)
+            {
+                if (!p.Comparer.Equals(p.Getter(x), p.Getter(y)))
+                    return false;
+            }
+
+            return true;
+
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            if (obj == null)
+                return 0;
+
+            int result = obj.GetType().GetHashCode();
+            foreach (var p in Properties.Values)
+            {
+                result = result * 31 + p.Comparer.GetHashCode(p.Getter(obj));
+            }
+
+            return result;
+        }
+
+
+    }
+
+    public class PropertyComparer<T>
+    {
+        public PropertyInfo MemberInfo { get; set; }
+        public Func<T, object> Getter { get; set; }
+        public IEqualityComparer Comparer { get; set; }
+
+        public PropertyComparer(PropertyInfo memberInfo, Func<T, object> getter)
+        {
+            this.Getter = getter;
+            this.MemberInfo = memberInfo;
+        }
+
+        public override string ToString() => $"{MemberInfo} -> {Comparer}";
+    }
+
+    public class SortedListEqualityComparer<T> : EqualityComparer<IList<T>>, ICompletableComparer
     {
         public IEqualityComparer<T> ElementComparer { get; set; }
+
+        public SortedListEqualityComparer()
+        {
+        }
         
-        public SortedMListEqualityComparer(IEqualityComparerResolver resolver, PropertyInfo pi)
+        public void Complete(IEqualityComparerResolver resolver, PropertyInfo pi)
         {
             ElementComparer = (IEqualityComparer<T>)resolver.GetEqualityComparer(typeof(T), pi);
         }
 
-        public override bool Equals(MList<T> x, MList<T> y)
+        public override bool Equals(IList<T> x, IList<T> y)
         {
             if ((x == null) != (y == null))
                 return false;
@@ -128,7 +195,7 @@ namespace Signum.Entities.Reflection
             return true;
         }
 
-        public override int GetHashCode(MList<T> obj)
+        public override int GetHashCode(IList<T> obj)
         {
             if (obj == null)
                 return 0;
@@ -143,16 +210,20 @@ namespace Signum.Entities.Reflection
         }
     }
 
-    public class UnsortedMListEqualityComparer<T> : EqualityComparer<MList<T>>
+    public class UnsortedListEqualityComparer<T> : EqualityComparer<IList<T>>, ICompletableComparer
     {
         public IEqualityComparer<T> ElementComparer { get; set; }
 
-        public UnsortedMListEqualityComparer(IEqualityComparerResolver resolver, PropertyInfo pi)
+        public UnsortedListEqualityComparer()
+        {
+        }
+
+        public void Complete(IEqualityComparerResolver resolver, PropertyInfo pi)
         {
             ElementComparer = (IEqualityComparer<T>)resolver.GetEqualityComparer(typeof(T), pi);
         }
 
-        public override bool Equals(MList<T> mx, MList<T> my)
+        public override bool Equals(IList<T> mx, IList<T> my)
         {
             if ((mx == null) != (my == null))
                 return false;
@@ -178,7 +249,7 @@ namespace Signum.Entities.Reflection
             return true;
         }
 
-        public override int GetHashCode(MList<T> obj)
+        public override int GetHashCode(IList<T> obj)
         {
             if (obj == null)
                 return 0;
@@ -193,26 +264,52 @@ namespace Signum.Entities.Reflection
         }
     }
 
-    public class DefaultEqualityComparerResolver : IEqualityComparerResolver
+    public abstract class EqualityComparerResolverWithCache : IEqualityComparerResolver
     {
-        public IEqualityComparer GetEqualityComparer(Type type, PropertyInfo pi)
+        Dictionary<(Type, PropertyInfo), IEqualityComparer> cache = new Dictionary<(Type, PropertyInfo), IEqualityComparer>();
+        public virtual IEqualityComparer GetEqualityComparer(Type type, PropertyInfo pi)
+        {
+            if (cache.TryGetValue((type, pi), out var comparer))
+                return comparer;
+            else
+            {
+                var comp = GetEqualityComparerInternal(type, pi);
+                cache.Add((type, pi), comp);
+
+                if (comp is ICompletableComparer cc)
+                    cc.Complete(this, pi);
+
+                return comp;
+            }
+        }
+
+        protected abstract IEqualityComparer GetEqualityComparerInternal(Type type, PropertyInfo pi);
+    }
+
+    public class DefaultEqualityComparerResolver : EqualityComparerResolverWithCache
+    {
+        protected override IEqualityComparer GetEqualityComparerInternal(Type type, PropertyInfo pi)
         {
             if (typeof(EmbeddedEntity).IsAssignableFrom(type) ||
                 typeof(MixinEntity).IsAssignableFrom(type) ||
                 typeof(ModelEntity).IsAssignableFrom(type))
             {
-                return (IEqualityComparer)Activator.CreateInstance(typeof(EntityStructuralEqualityComparer<>).MakeGenericType(type), this);
+                return (IEqualityComparer)Activator.CreateInstance(typeof(EntityStructuralEqualityComparer<>).MakeGenericType(type));
             }
-
-            if(typeof(IMListPrivate).IsAssignableFrom(type))
+            
+            if (typeof(IList).IsAssignableFrom(type))
             {
-                if(pi?.HasAttribute<PreserveOrderAttribute>() == true)
-                    return (IEqualityComparer)Activator.CreateInstance(typeof(SortedMListEqualityComparer<>).MakeGenericType(type), this, pi);
+                if (pi?.HasAttribute<PreserveOrderAttribute>() == true)
+                    return (IEqualityComparer)Activator.CreateInstance(typeof(SortedListEqualityComparer<>).MakeGenericType(type));
                 else
-                    return (IEqualityComparer)Activator.CreateInstance(typeof(UnsortedMListEqualityComparer<>).MakeGenericType(type), this, pi);
+                    return (IEqualityComparer)Activator.CreateInstance(typeof(UnsortedListEqualityComparer<>).MakeGenericType(type));
             }
 
-            return EqualityComparer<object>.Default;
+            if (type.IsClass && type.GetMethod("Equals", BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly) == null)
+                return (IEqualityComparer)Activator.CreateInstance(typeof(ClassStructuralEqualityComparer<>).MakeGenericType(type)); ;
+            
+            return (IEqualityComparer)typeof(EqualityComparer<>).MakeGenericType(type).GetProperty("Default").GetValue(null);
         }
     }
+
 }
