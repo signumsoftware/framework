9fa0017c6122d3f4db3f57c975651b0f323af3a2
Olmo del Corral <olmo@signumsoftware.com>
2018-03-29 20:05:42 +0200

    remove Signum.Web

--- Signum.Web/Signum/Scripts/globals.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../Headers/jquery/jquery.d.ts"/>
-/// <reference path="SF.ts"/>
-/// <reference path="../Headers/bootstrap/bootstrap.d.ts"/>
-/// <reference path="../Headers/bootstrap/bootstrap.datepicker.d.ts"/>
-/// <reference path="../Headers/bootstrap/bootstrap.timepicker.d.ts"/>
-
-/// <reference path="SF.UI.ts"/>
