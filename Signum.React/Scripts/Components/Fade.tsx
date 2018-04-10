833b69ac203c4cb00e23dc24161a15448ccc746d
Konstantin Lukaschenko <k.lukaschenko@googlemail.com>
2018-04-10 17:18:16 +0200

    introduced Fade and FadeModal.

--- Signum.React/Scripts/Components/Fade.tsx
+++ Signum.React/Scripts/Components/Fade.tsx
@@ -1,48 +1,36 @@
-﻿import * as React from 'react';
-import Transition, { TransitionState, TransitionProps, EnterHandler, EndHandler, ExitHandler } from 'react-transition-group/Transition';
-import { classes } from '../Globals';
+﻿import { Transition } from 'react-transition-group';
+import * as React from 'react';
 
-export interface FadeProps {
-    in?: boolean;
-    mountOnEnter?: boolean;
-    unmountOnExit?: boolean;
-    timeout: number | { enter?: number, exit?: number };
-    addEndListener?: EndHandler;
-    onEnter?: EnterHandler;
-    onEntering?: EnterHandler;
-    onEntered?: EnterHandler;
-    onExit?: ExitHandler;
-    onExiting?: ExitHandler;
-    onExited?: ExitHandler;
-    children: React.ReactElement<any>
+const duration = 300;
+
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+}
+
+const transitionStyles: { [state: string]: React.CSSProperties } = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 },
 };
 
 
-
-const fadeClass = {
-    ["entering" as TransitionState]: 'show',
-    ["entered" as TransitionState]: 'show',
-};
+interface FadeProps {
+    in: boolean;
+}
 
 export class Fade extends React.Component<FadeProps> {
-
-    static defaultProps = {
-        in: false,
-        timeout: 300,
-        mountOnEnter: false,
-        unmountOnExit: false,
-        appear: false
-    };
-
     render() {
-        const { children, ...props } = this.props;
-        
         return (
-            <Transition {...props} >
-                {
-                    (state) => React.cloneElement(children, { className: classes(children.props.className, 'fade', fadeClass[state]) })
-                }
+            <Transition in={this.props.in} timeout={duration}>
+                {(state: string) => (
+                    <div style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                    }}>
+                        {this.props.children}
+                    </div>
+                )}
             </Transition>
         );
-  }
+    }
 }
