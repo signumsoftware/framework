5315c553d6894c27c75a2cbd30dbc6ed9f9f9cb1
Olmo del Corral <o.corral@controlexpert.com>
2018-04-23 10:34:45 +0200

    fix ValueLineType

--- Signum.React/Scripts/Lines/ValueLine.tsx
+++ Signum.React/Scripts/Lines/ValueLine.tsx
@@ -58,7 +58,7 @@ export class ValueLine extends LineBase<ValueLineProps, ValueLineProps> {
         state.valueLineType = ValueLine.getValueLineType(state.type!);
 
         if (state.valueLineType == undefined)
-            throw new Error(`No ValueLine found for '${state.type!.name}' (property route = ${state.ctx.propertyRoute ? state.ctx.propertyRoute.propertyPath() : "??"})`);
+            throw new Error(`No ValueLineType found for type '${state.type!.name}' (property route = ${state.ctx.propertyRoute ? state.ctx.propertyRoute.propertyPath() : "??"})`);
     }
 
     inputElement?: HTMLElement | null;
