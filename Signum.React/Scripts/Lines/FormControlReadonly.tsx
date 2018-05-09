c14a8f1ed8d86379bf6b978e604de618250f8698
sschoensee <sschoensee@controlexpert.com>
2018-05-09 14:57:45 +0200

    Revert to last Version and set ccs styleoption for deactivate pointer events.

--- Signum.React/Scripts/Lines/FormControlReadonly.tsx
+++ Signum.React/Scripts/Lines/FormControlReadonly.tsx
@@ -3,6 +3,7 @@ import { StyleContext, TypeContext } from '../Lines';
 import { classes, addClass } from '../Globals';
 
 import "./Lines.css"
+import { navigatorIsReadOnly } from '../../../../Extensions/Signum.React.Extensions/Authorization/AuthClient';
 
 export interface FormControlReadonlyProps extends React.Props<FormControlReadonly> {
     ctx: StyleContext;
@@ -16,14 +17,19 @@ export class FormControlReadonly extends React.Component<FormControlReadonlyProp
         const ctx = this.props.ctx;
 
         var attrs = this.props.htmlAttributes;
+        
+        if (ctx.readonlyAsPlainText) {
+            return (
+                <p {...attrs} className={classes(ctx.formControlPlainTextClass, attrs && attrs.className, this.props.className)}>
+                    {this.props.children}
+                </p>
+            );
 
-
-        var formControlClasses = ctx.readonlyAsPlainText ? ctx.formControlPlainTextClass : classes(ctx.formControlClass, "readonly");
-
-        return (
-            <div {...attrs} className={classes(formControlClasses, attrs && attrs.className, this.props.className)}>
-                {this.props.children || "\u00A0" /*To get min height*/}
-            </div>
-        );
+        } else {
+            return (
+                <input type="text" style={{ pointerEvents: "none" }} readOnly {...attrs} className={classes(ctx.formControlClass, attrs && attrs.className, this.props.className)}
+                    value={React.Children.toArray(this.props.children)[0] as string || ""} />
+            );
+        }
     }
 }
