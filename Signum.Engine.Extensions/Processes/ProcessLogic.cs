882f0c0982ded05039f6a5926d89e608f9f6c3ba
jbu <j.burchartz@controlexpert.com>
2018-07-16 15:06:04 +0200

    removed "this"

--- Signum.Engine.Extensions/Processes/ProcessLogic.cs
+++ Signum.Engine.Extensions/Processes/ProcessLogic.cs
@@ -584,7 +584,7 @@ namespace Signum.Engine.Processes
 
         public ActionProcessAlgorithm(Action<ExecutingProcess> action)
         {
-            this.Action = action ?? throw new ArgumentNullException(nameof(action));
+            Action = action ?? throw new ArgumentNullException(nameof(action));
         }
         
         public void Execute(ExecutingProcess executingProcess) => Action(executingProcess);
