69154245404c273eb8a681489e5b3eb7aee96676
karimpour <mkarimpour@gmail.com>
2018-08-27 18:16:43 +0430

    make sense DynamicValidations for DisableMixin

--- Signum.Engine.Extensions/Dynamic/DynamicValidationLogic.cs
+++ Signum.Engine.Extensions/Dynamic/DynamicValidationLogic.cs
@@ -74,8 +74,12 @@ namespace Signum.Engine.Dynamic
             if (!initialized)
                 return null;
 
-            var candidates = DynamicValidations.Value.TryGetC(mod.GetType());
-            if (candidates == null)
+            var candidates = DynamicValidations.Value.TryGetC(mod.GetType()).EmptyIfNull()
+                .Where(pair => !MixinDeclarations.IsDeclared(typeof(DynamicValidationEntity), typeof(DisabledMixin)) ||
+                                pair.Validation.Mixin<DisabledMixin>().IsDisabled == false)
+                .ToList();
+
+            if (candidates.IsEmpty())
                 return null;
 
             foreach (var pair in candidates)
