32b43cccce15dfcef2fe06d84ef9ebccd90220eb
Olmo del Corral <o.corral@controlexpert.com>
2018-11-12 09:53:04 +0100

    fix formatting

--- Signum.React.Extensions/Dynamic/Type/DynamicMixinConnection.tsx
+++ Signum.React.Extensions/Dynamic/Type/DynamicMixinConnection.tsx
@@ -1,4 +1,4 @@
-﻿import * as React from 'react'
+import * as React from 'react'
 import { Dic, classes } from '@framework/Globals'
 import * as Constructor from '@framework/Constructor'
 import * as Finder from '@framework/Finder'
@@ -11,70 +11,70 @@ import * as DynamicTypeClient from '../DynamicTypeClient'
 import { Typeahead } from '@framework/Components';
 
 interface DynamicMixinConnectionComponentProps {
-    ctx: TypeContext<DynamicMixinConnectionEntity>;
+  ctx: TypeContext<DynamicMixinConnectionEntity>;
 }
 
 export default class DynamicMixinConnectionComponent extends React.Component<DynamicMixinConnectionComponentProps> {
 
-    render() {
-        const ctx = this.props.ctx;
+  render() {
+    const ctx = this.props.ctx;
 
-        return (
-            <div>
-                <MixinCombo
-                    binding={Binding.create(ctx.value, a => a.mixinName)}
-                    labelText={ctx.niceName(a => a.mixinName)}
-                    labelColumns={2}
-                    onChange={() => this.forceUpdate()} />
-                <EntityLine ctx={ctx.subCtx(dt => dt.entityType)} />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <MixinCombo
+          binding={Binding.create(ctx.value, a => a.mixinName)}
+          labelText={ctx.niceName(a => a.mixinName)}
+          labelColumns={2}
+          onChange={() => this.forceUpdate()} />
+        <EntityLine ctx={ctx.subCtx(dt => dt.entityType)} />
+      </div>
+    );
+  }
 }
 
 export interface MixinComboProps {
-    binding: Binding<string | null | undefined>;
-    labelText: string;
-    labelColumns: number;
-    onChange?: () => void;
+  binding: Binding<string | null | undefined>;
+  labelText: string;
+  labelColumns: number;
+  onChange?: () => void;
 }
 
 export class MixinCombo extends React.Component<MixinComboProps>{
 
-    handleGetItems = (query: string) => {
-        return Finder.fetchEntitiesWithFilters(
-            DynamicTypeEntity,
-            [
-                { token: "Entity.BaseType", operation: "EqualTo", value: "MixinEntity" },
-                { token: "Entity.TypeName", operation: "StartsWith", value: query },
-            ], [], 5)
-            .then(lites => lites && lites.map(a => a.toStr));
-    }
+  handleGetItems = (query: string) => {
+    return Finder.fetchEntitiesWithFilters(
+      DynamicTypeEntity,
+      [
+        { token: "Entity.BaseType", operation: "EqualTo", value: "MixinEntity" },
+        { token: "Entity.TypeName", operation: "StartsWith", value: query },
+      ], [], 5)
+      .then(lites => lites && lites.map(a => a.toStr));
+  }
 
-    handleOnChange = (newValue: string) => {
-        this.props.binding.setValue(newValue);
-        this.forceUpdate();
+  handleOnChange = (newValue: string) => {
+    this.props.binding.setValue(newValue);
+    this.forceUpdate();
 
-        if (this.props.onChange)
-            this.props.onChange();
-    }
+    if (this.props.onChange)
+      this.props.onChange();
+  }
 
-    render() {
-        let lc = this.props.labelColumns;
-        return (
-            <div className="form-group form-group-sm row" >
-                <label className={classes("col-form-label col-form-label-sm", "col-sm-" + (lc == null ? 2 : lc))}>
-                    {this.props.labelText}
-                </label>
-                <div className={"col-sm-" + (lc == null ? 10 : 12 - lc)}>
-                    <div style={{ position: "relative" }}>
-                        <Typeahead
-                            inputAttrs={{ className: "form-control form-control-sm sf-entity-autocomplete" }}
-                            getItems={this.handleGetItems}
-                            value={this.props.binding.getValue() || ""}
-                            onChange={this.handleOnChange} />
-                    </div>
-                </div>
-            </div>);
-    }
+  render() {
+    let lc = this.props.labelColumns;
+    return (
+      <div className="form-group form-group-sm row" >
+        <label className={classes("col-form-label col-form-label-sm", "col-sm-" + (lc == null ? 2 : lc))}>
+          {this.props.labelText}
+        </label>
+        <div className={"col-sm-" + (lc == null ? 10 : 12 - lc)}>
+          <div style={{ position: "relative" }}>
+            <Typeahead
+              inputAttrs={{ className: "form-control form-control-sm sf-entity-autocomplete" }}
+              getItems={this.handleGetItems}
+              value={this.props.binding.getValue() || ""}
+              onChange={this.handleOnChange} />
+          </div>
+        </div>
+      </div>);
+  }
 }
