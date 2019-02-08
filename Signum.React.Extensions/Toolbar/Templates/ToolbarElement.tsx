ce42dfeb7d435acce567f6c5bc693ebce42357dd
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2019-02-08 15:50:31 +0100

    fix ToolbarElement url and fixedWidth

--- Signum.React.Extensions/Toolbar/Templates/ToolbarElement.tsx
+++ Signum.React.Extensions/Toolbar/Templates/ToolbarElement.tsx
@@ -6,8 +6,10 @@ import { ColorTypeaheadLine } from '../../Basics/Templates/ColorTypeahead'
 import { IconTypeaheadLine } from '../../Basics/Templates/IconTypeahead'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as Dashboard from '../../Dashboard/Admin/Dashboard'
+import { PermissionSymbol } from '../../Authorization/Signum.Entities.Authorization';
 
 export default class ToolbarElement extends React.Component<{ ctx: TypeContext<ToolbarElementEmbedded> }> {
+
   handleTypeChanges = () => {
     var a = this.props.ctx.value;
     if (a.type == "Divider") {
@@ -15,6 +17,14 @@ export default class ToolbarElement extends React.Component<{ ctx: TypeContext<T
       a.content == null;
       a.label == null;
       a.modified = true;
+    }
+    this.forceUpdate();
+  }
+
+  handleContentChange = () => {
+    const tbe = this.props.ctx.value;
+    if (tbe.content && !PermissionSymbol.isLite(tbe.content)) {
+      tbe.url = null;
     }
     this.forceUpdate();
   }
@@ -38,7 +48,7 @@ export default class ToolbarElement extends React.Component<{ ctx: TypeContext<T
             <ValueLine ctx={ctx4.subCtx(t => t.type)} onChange={this.handleTypeChanges} />
           </div>
           <div className="col-sm-5 offset-sm-1">
-            {ctx2.value.type != "Divider" && <EntityLine ctx={ctx2.subCtx(t => t.content)} onChange={() => this.forceUpdate()} />}
+            {ctx2.value.type != "Divider" && <EntityLine ctx={ctx2.subCtx(t => t.content)} onChange={this.handleContentChange} />}
           </div>
         </div>
 
