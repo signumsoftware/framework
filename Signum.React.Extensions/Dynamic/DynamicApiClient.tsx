2a72e1d4b799385a46efbcd7b2de69cd55f72690
karimpour <mkarimpour@gmail.com>
2019-03-04 11:53:16 +0330

    add Constructor for DynamicApi Eval

--- Signum.React.Extensions/Dynamic/DynamicApiClient.tsx
+++ Signum.React.Extensions/Dynamic/DynamicApiClient.tsx
@@ -1,7 +1,9 @@
 import { EntitySettings } from '@framework/Navigator'
 import * as Navigator from '@framework/Navigator'
-import { DynamicApiEntity } from './Signum.Entities.Dynamic'
+import { DynamicApiEntity, DynamicApiEval } from './Signum.Entities.Dynamic'
+import * as Constructor from '@framework/Constructor'
 
 export function start(options: { routes: JSX.Element[] }) {
   Navigator.addSettings(new EntitySettings(DynamicApiEntity, w => import('./Api/DynamicApi')));
+  Constructor.registerConstructor(DynamicApiEntity, () => DynamicApiEntity.New({ eval: DynamicApiEval.New() }));
 }
