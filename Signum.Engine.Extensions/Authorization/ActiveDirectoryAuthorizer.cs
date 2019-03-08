6e43edee7767a9ddc10867df176358d9ac992aa4
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2019-03-08 12:30:16 +0100

    Revert "Revert "AllowOtherDomains""
    This reverts commit f74618e186721cff782a42ff7ca934182ee3f025.

--- Signum.Engine.Extensions/Authorization/ActiveDirectoryAuthorizer.cs
+++ Signum.Engine.Extensions/Authorization/ActiveDirectoryAuthorizer.cs
@@ -45,18 +45,18 @@ namespace Signum.Engine.Authorization
             using (AuthLogic.Disable())
             {
                 var config = this.GetConfig();
-                var domainName = userName.TryAfterLast('@');
-                var localName = userName.TryBeforeLast('@') ?? userName;
-
+                var domainName = userName.TryAfterLast('@') ?? userName.TryBefore('\\') ?? config.DefaultDomainName;
+                var localName = userName.TryBeforeLast('@') ?? userName.TryAfter('\\') ?? userName;
+                
                 UserEntity user;
 
-                if (config.DomainName.HasText() && (domainName == null || config.DomainName.ToLower() == domainName?.ToLower()))
+                if (domainName != null && (config.AllowOtherDomains || config.DefaultDomainName?.ToLower() == domainName.ToLower()))
                 {
                     try
                     {
-                        using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, config.DomainName))
+                        using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName))
                         {
-                            if (pc.ValidateCredentials(localName + "@" + config.DomainName, password))
+                            if (pc.ValidateCredentials(localName + "@" + domainName, password))
                             {
                                 user = AuthLogic.RetrieveUser(userName);
 
