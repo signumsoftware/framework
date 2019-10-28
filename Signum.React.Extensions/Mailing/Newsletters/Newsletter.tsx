c66c983b1d02d5d4be6176411b1a2f6e72295217
Olmo del Corral <olmo@signumsoftware.com>
2019-10-28 08:12:24 +0100

    update Mailing to hooks

--- Signum.React.Extensions/Mailing/Newsletters/Newsletter.tsx
+++ Signum.React.Extensions/Mailing/Newsletters/Newsletter.tsx
@@ -3,21 +3,19 @@ import { ValueLine, EntityLine } from '@framework/Lines'
 import { TypeContext } from '@framework/TypeContext'
 import { NewsletterEntity } from '../Signum.Entities.Mailing'
 
-export default class Newsletter extends React.Component<{ ctx: TypeContext<NewsletterEntity> }> {
-  render() {
-    const nc = this.props.ctx;
+export default function Newsletter(p : { ctx: TypeContext<NewsletterEntity> }){
+  const nc = p.ctx;
 
-    return (
-      <div>
-        <ValueLine ctx={nc.subCtx(n => n.name)} />
-        <ValueLine ctx={nc.subCtx(n => n.state)} readOnly={true} />
-        <ValueLine ctx={nc.subCtx(n => n.from)} />
-        <ValueLine ctx={nc.subCtx(n => n.displayFrom)} />
-        <EntityLine ctx={nc.subCtx(e => e.query)} />
-        <ValueLine ctx={nc.subCtx(n => n.subject)} />
-        <ValueLine ctx={nc.subCtx(n => n.text)} valueLineType="TextArea" valueHtmlAttributes={{ style: { width: "100%", height: "180px" } }} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <ValueLine ctx={nc.subCtx(n => n.name)} />
+      <ValueLine ctx={nc.subCtx(n => n.state)} readOnly={true} />
+      <ValueLine ctx={nc.subCtx(n => n.from)} />
+      <ValueLine ctx={nc.subCtx(n => n.displayFrom)} />
+      <EntityLine ctx={nc.subCtx(e => e.query)} />
+      <ValueLine ctx={nc.subCtx(n => n.subject)} />
+      <ValueLine ctx={nc.subCtx(n => n.text)} valueLineType="TextArea" valueHtmlAttributes={{ style: { width: "100%", height: "180px" } }} />
+    </div>
+  );
 }
 
