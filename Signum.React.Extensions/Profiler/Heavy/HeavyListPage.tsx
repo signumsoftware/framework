7389f5bf4a6c753030ec91d3d821cd56c16014f7
Olmo del Corral <olmo@signumsoftware.com>
2019-10-30 22:39:52 +0100

    update Times and HeavyProfiles too Hooks

--- Signum.React.Extensions/Profiler/Heavy/HeavyListPage.tsx
+++ Signum.React.Extensions/Profiler/Heavy/HeavyListPage.tsx
@@ -5,55 +5,38 @@ import * as d3 from 'd3'
 import * as Navigator from '@framework/Navigator'
 import { API, HeavyProfilerEntry } from '../ProfilerClient'
 import "./Profiler.css"
+import { useTitle, useAPI, useAPIWithReload, useSize } from '../../../../Framework/Signum.React/Scripts/Hooks'
 
 interface HeavyListProps extends RouteComponentProps<{}> {
 
 }
 
-export default function HeavyList(p : HeavyListProps, { enabled?: boolean; entries?: HeavyProfilerEntry[], fileToUpload?: File, fileVer: number }){
-  function constructor(props: HeavyListProps) {
-    super(props);
-    state = { fileVer: 0 };
-  }
+export default function HeavyList(p: HeavyListProps) {
 
-  function componentWillMount() {
-    loadIsEnabled().done()
-    loadEntries().done();
-    Navigator.setTitle("Heavy Profiler");
-  }
+  const [enabled, reloadEnabled] = useAPIWithReload(() => API.Heavy.isEnabled(), []);
+  const [entries, reloadEntries] = useAPIWithReload(() => API.Heavy.entries(), []);
 
-  function componentWillUnmount() {
-    Navigator.setTitle();
-  }
+  const [fileToUpload, setFileToUpload] = React.useState<File | undefined>(undefined);
+  const [fileVer, setFileVer] = React.useState<number>(0)
 
- function loadEntries() {
-    const entries = await API.Heavy.entries();
-    return setState({ entries });
-  }
+  useTitle("Heavy Profiler");
 
   function handleClear(e: React.MouseEvent<any>) {
     API.Heavy.clear()
-      .then(() => loadEntries())
+      .then(() => reloadEntries())
       .done();
   }
 
   function handleUpdate(e: React.MouseEvent<any>) {
-    loadEntries().done();
-    loadIsEnabled().done();
-  }
-
- function loadIsEnabled() {
-    const enabled = await API.Heavy.isEnabled();
-    return setState({ enabled });
+    reloadEntries();
+    reloadEnabled();
   }
 
   function handleSetEnabled(value: boolean) {
     API.Heavy.setEnabled(value)
-      .then(() => loadIsEnabled())
-      .then(() => loadEntries())
+      .then(() => { reloadEntries(); reloadEnabled(); })
       .done();
   }
-
 
   function handleDownload() {
     API.Heavy.download(undefined);
@@ -61,7 +44,7 @@ export default function HeavyList(p : HeavyListProps, { enabled?: boolean; entri
 
   function handleInputChange(e: React.FormEvent<any>) {
     let f = (e.currentTarget as HTMLInputElement).files![0];
-    setState({ fileToUpload: f });
+    setFileToUpload(f);
   }
 
   function handleUpload() {
@@ -72,106 +55,18 @@ export default function HeavyList(p : HeavyListProps, { enabled?: boolean; entri
       let fileName = fileToUpload!.name;
 
       API.Heavy.upload({ fileName, content })
-        .then(() => setState({ fileToUpload: undefined, fileVer: fileVer + 1 }))
-        .then(() => loadEntries())
+        .then(() => {
+          setFileToUpload(undefined);
+          setFileVer(fileVer + 1);
+          reloadEntries();
+        })
         .done();
     };
     fileReader.readAsDataURL(fileToUpload!);
   }
 
-
-  function componentDidUpdate() {
-    mountChart();
-  }
-
-  chartContainer?: HTMLDivElement | null;
-
-  function mountChart() {
-    if (chartContainer == undefined)
-      return;
-
-    let data = entries!;
-
-    let fontSize = 12;
-    let fontPadding = 4;
-    let characterWidth = 7;
-    let labelWidth = 60 * characterWidth; //Max characters: 100
-    let rightMargin = 10 * characterWidth; //Aproximate elapsed time length: 10
-
-    let width = chartContainer.getBoundingClientRect().width;
-    let height = (fontSize + (2 * fontPadding)) * (data.length);
-    chartContainer.style.height = height + "px";
-
-    let minStart = data.map(a => a.beforeStart).min()!;
-    let maxEnd = data.map(a => a.end).max()!;
-
-    let x = d3.scaleLinear()
-      .domain([minStart, maxEnd])
-      .range([labelWidth + 3, width - rightMargin]);
-
-    let y = d3.scaleLinear()
-      .domain([0, data.length])
-      .range([0, height - 1]);
-
-    let entryHeight = y(1);
-
-    d3.select(chartContainer).selectAll("svg").remove();
-
-    let chart = d3.select(chartContainer)
-      .append('svg:svg').attr('width', width).attr('height', height);
-
-    let groups = chart.selectAll("g.entry").data(data).enter()
-      .append('svg:g').attr('class', 'entry')
-      .attr('data-full-index', function (v) { return v.fullIndex; });
-
-    groups.append('svg:rect').attr('class', 'left-background')
-      .attr('x', 0)
-      .attr('y', function (v, i) { return y(i); })
-      .attr('width', labelWidth)
-      .attr('height', entryHeight)
-      .attr('fill', '#ddd')
-      .attr('stroke', '#fff');
-
-    let labelsLeft = groups.append('svg:text').attr('class', 'label label-left')
-      .attr('dy', function (v, i) { return y(i); })
-      .attr('y', fontPadding + fontSize)
-      .attr('fill', '#000')
-      .text(function (v) { return v.role + " " + v.additionalData; });
-
-    groups.append('svg:rect').attr('class', 'right-background')
-      .attr('x', labelWidth)
-      .attr('y', function (v, i) { return y(i); })
-      .attr('width', width - labelWidth)
-      .attr('height', entryHeight)
-      .attr('fill', '#fff')
-      .attr('stroke', '#ddd');
-
-    let rectangles = groups.append('svg:rect').attr('class', 'shape')
-      .attr('x', function (v) { return x(v.start); })
-      .attr('y', function (v, i) { return y(i); })
-      .attr('width', function (v) { return x(v.end) - x(v.start); })
-      .attr('height', entryHeight)
-      .attr('fill', function (v) { return v.color; });
-
-    let labelsRight = groups.append('svg:text').attr('class', 'label label-right')
-      .attr('dx', function (v) { return x(v.end) + 3; })
-      .attr('dy', function (v, i) { return y(i); })
-      .attr('y', fontPadding + fontSize)
-      .attr('fill', '#000')
-      .text(function (v) { return v.elapsed; });
-
-    groups.append('svg:title').text(function (v) { return v.elapsed + " - " + v.additionalData; });
-
-    groups.on("click", e => {
-      let url = "~/profiler/heavy/entry/" + e.fullIndex;
-
-      if (d3.event.ctrlKey) {
-        window.open(Navigator.toAbsoluteUrl(url));
-      } else {
-        Navigator.history.push(url);
-      }
-    });
-  }
+  const { size, setContainer } = useSize();
+  
   if (entries == undefined)
     return <h3 className="display-6">Heavy Profiler (loading...) </h3>;
 
@@ -197,9 +92,60 @@ export default function HeavyList(p : HeavyListProps, { enabled?: boolean; entri
 
       <br />
       <h3>Entries</h3>
-      <div className="sf-profiler-chart" ref={d => chartContainer = d}>
+      <div className="sf-profiler-chart" ref={setContainer}>
+        {size && <EntrieListPath entries={entries} width={size.width} />}
       </div>
     </div>
+  );
+}
+
+
+function EntrieListPath({ width, entries }: { width: number, entries: HeavyProfilerEntry[] }) {
+
+  let data = entries!;
+
+  let fontSize = 12;
+  let fontPadding = 4;
+  let characterWidth = 7;
+  let labelWidth = 60 * characterWidth; //Max characters: 100
+  let rightMargin = 10 * characterWidth; //Aproximate elapsed time length: 10
+
+  let height = (fontSize + (2 * fontPadding)) * (data.length);
+
+  let minStart = data.map(a => a.beforeStart).min()!;
+  let maxEnd = data.map(a => a.end).max()!;
+
+  let x = d3.scaleLinear()
+    .domain([minStart, maxEnd])
+    .range([labelWidth + 3, width - rightMargin]);
+
+  let y = d3.scaleLinear()
+    .domain([0, data.length])
+    .range([0, height - 1]);
+
+  let entryHeight = y(1);
+
+  function handleOnClick(e: React.MouseEvent, v: HeavyProfilerEntry) {
+    let url = "~/profiler/heavy/entry/" + v.fullIndex;
+
+    if (d3.event.ctrlKey) {
+      window.open(Navigator.toAbsoluteUrl(url));
+    } else {
+      Navigator.history.push(url);
+    }
+  }
+
+  return (
+    <svg width={width + "px"} height={height + "px"}>
+      {data.map((v, i) => <g className="entry" data-full-key={v.fullIndex} onClick={e => handleOnClick(e, v)}>
+        <rect className="left-background" x={0} y={y(i)} width={labelWidth} height={entryHeight} fill="#ddd" stroke="#fff" />
+        <text className="label label-left" y={y(i)} dy={fontPadding + fontSize} fill="#000">{v.role + " " + v.additionalData}</text>
+        <rect className="right-background" x={labelWidth} y={y(i)} width={width - labelWidth} height={entryHeight} fill="#fff" stroke="#ddd" />
+        <rect className="shape" x={x(v.start)} y={y(i)} width={x(v.end) - x(v.start)} height={entryHeight} fill={v.color} />
+        <text className="label label-right" x={x(v.end) + 3} y={y(i)} width={x(v.end) - x(v.start)} dy={fontPadding + fontSize} height={entryHeight} fill='#000'>{v.elapsed}</text>
+        <title>{v.elapsed + " - " + v.additionalData}</title>
+      </g>)}
+    </svg>
   );
 }
 
