b1ea7d29dba4aeb9af74133ea3ccbe8912053586
Saeid Rezaei <s.rezaei@controlexpert.com>
2020-01-27 09:49:28 +0100

    added support for localised boolean rendering in word templates. cleanup RenderNode method.

--- Signum.Engine.Extensions/Word/TemplateNodes.cs
+++ Signum.Engine.Extensions/Word/TemplateNodes.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml;
+using Signum.Entities;
 
 namespace Signum.Engine.Word
 {
@@ -347,22 +348,38 @@ namespace Signum.Engine.Word
             ValueProvider.FillQueryTokens(tokens);
         }
 
-        internal protected override void RenderNode(WordTemplateParameters p)
+        protected internal override void RenderNode(WordTemplateParameters p)
         {
-            object obj = ValueProvider.GetValue(p);
-            string text = obj is Enum ? ((Enum)obj).NiceToString() :
-                obj is IFormattable ? ((IFormattable)obj).ToString(Format ?? ValueProvider.Format, p.Culture) :
-                obj?.ToString();
+            var obj = ValueProvider.GetValue(p);
 
+            string text;
+            
+            switch (obj)
+            {
+                case Enum e:
+                    text = e.NiceToString();
+                    break;
+                case bool b:
+                    text = b ? BooleanEnum.True.NiceToString() : BooleanEnum.False.NiceToString();
+                    break;
+                case IFormattable f:
+                    text = f.ToString(Format ?? ValueProvider.Format, p.Culture);
+                    break;
+                default:
+                    text = obj?.ToString();
+                    break;
+            }
+            
             if (text != null && text.Contains('\n'))
             {
-                var replacements = text.Lines().Select((line, i) => this.NodeProvider.NewRun((OpenXmlCompositeElement)this.RunProperties?.CloneNode(true), line, initialBr: i > 0));
+                var replacements = text.Lines()
+                    .Select((line, i) => NodeProvider.NewRun((OpenXmlCompositeElement)RunProperties?.CloneNode(true), line, initialBr: i > 0));
 
                 this.ReplaceBy(replacements);
             }
             else
             {
-                this.ReplaceBy(this.NodeProvider.NewRun((OpenXmlCompositeElement)this.RunProperties?.CloneNode(true), text));
+                this.ReplaceBy(NodeProvider.NewRun((OpenXmlCompositeElement)RunProperties?.CloneNode(true), text));
             }
         }
 
