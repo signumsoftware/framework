690e9662ece262d58b3e71a23f6a21af1d4e4a39
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2020-02-05 13:15:22 +0100

    fix AlterColumn in Postgres

--- Signum.Engine/Engine/SqlBuilder.cs
+++ Signum.Engine/Engine/SqlBuilder.cs
@@ -152,9 +152,19 @@ FOR EACH ROW EXECUTE PROCEDURE versioning(
             return new SqlPreCommandSimple("ALTER TABLE {0} ADD {1};".FormatWith(table.Name, CreateOldColumn(column)));
         }
 
-        public SqlPreCommand AlterTableAlterColumn(ITable table, IColumn column, string? defaultConstraintName = null, ObjectName? forceTableName = null)
+        public SqlPreCommand AlterTableAlterColumn(ITable table, IColumn column, DiffColumn diffColumn, ObjectName? forceTableName = null)
         {
-            var alterColumn = new SqlPreCommandSimple("ALTER TABLE {0} ALTER COLUMN {1};".FormatWith(forceTableName ?? table.Name, this.ColumnLine(column, null, isChange: true)));
+            var tableName = forceTableName ?? table.Name;
+
+            var alterColumn = !IsPostgres ?
+                 new SqlPreCommandSimple("ALTER TABLE {0} ALTER COLUMN {1};".FormatWith(tableName, this.ColumnLine(column, null, isChange: true))) :
+                 new[] 
+                 {
+                     !diffColumn.DbType.Equals(column.DbType) || diffColumn.Collation != column.Collation || !diffColumn.ScaleEquals(column) || !diffColumn.SizeEquals(column) ? 
+                        new SqlPreCommandSimple("ALTER TABLE {0} ALTER COLUMN {1} TYPE {2};".FormatWith(tableName, column.Name.SqlEscape(isPostgres),  GetColumnType(column) + (column.Collation != null ? " COLLATE " + column.Collation : null))) : null, 
+                     diffColumn.Nullable &&  !column.Nullable.ToBool()? new SqlPreCommandSimple("ALTER TABLE {0} ALTER COLUMN {1} SET NOT NULL;".FormatWith(tableName, column.Name.SqlEscape(isPostgres))) : null,
+                     !diffColumn.Nullable && column.Nullable.ToBool()? new SqlPreCommandSimple("ALTER TABLE {0} ALTER COLUMN {1} DROP NOT NULL;".FormatWith(tableName, column.Name.SqlEscape(isPostgres))) : null,
+                 }.Combine(Spacing.Simple) ?? new SqlPreCommandSimple("ALTER TABLE {0} ALTER COLUMN {1} -- UNEXPECTED COLUMN CHANGE!!".FormatWith(tableName, column.Name.SqlEscape(isPostgres)));
 
             if (column.Default == null)
                 return alterColumn;
@@ -162,7 +172,7 @@ FOR EACH ROW EXECUTE PROCEDURE versioning(
             var defCons = GetDefaultConstaint(table, column)!;
 
             return SqlPreCommand.Combine(Spacing.Simple,
-                AlterTableDropConstraint(table.Name, defaultConstraintName ?? defCons.Name),
+                AlterTableDropConstraint(table.Name, diffColumn.DefaultConstraint?.Name ?? defCons.Name),
                 alterColumn,
                 AlterTableAddDefaultConstraint(table.Name, defCons)
             )!;
