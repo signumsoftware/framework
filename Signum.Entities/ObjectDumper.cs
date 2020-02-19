b8f1bfecd721bdf94e7ce221bbd93471ca437d07
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2020-02-19 16:46:45 +0100

    fix ObjectDumper

--- Signum.Entities/ObjectDumper.cs
+++ Signum.Entities/ObjectDumper.cs
@@ -272,7 +272,7 @@ namespace Signum.Entities
 
             private bool IsMixinField(FieldInfo field)
             {
-                return field.Name == "mixin" && field.DeclaringType == typeof(Entity) ||
+                return  field.Name == "mixin" && field.DeclaringType == typeof(ModifiableEntity) ||
                     field.Name == "next" && field.DeclaringType == typeof(MixinEntity);
             }
 
