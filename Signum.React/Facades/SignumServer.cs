5ebd55579c6f8d51213367d99ace824a38626ec5
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2020-03-03 13:26:44 +0100

    make RegisterLike for "Signum.Entities" and "Signum.Entities.DynamicQuery"

--- Signum.React/Facades/SignumServer.cs
+++ Signum.React/Facades/SignumServer.cs
@@ -12,6 +12,7 @@ using Signum.Engine.Maps;
 using Signum.Engine.Operations;
 using Signum.Entities;
 using Signum.Entities.Basics;
+using Signum.Entities.DynamicQuery;
 using Signum.React.ApiControllers;
 using Signum.React.Filters;
 using Signum.React.Json;
@@ -87,6 +88,8 @@ namespace Signum.React.Facades
             SignumControllerFactory.RegisterArea(MethodInfo.GetCurrentMethod()!);
 
             ReflectionServer.Start();
+            ReflectionServer.RegisterLike(typeof(SearchMessage), () => UserHolder.Current != null);
+            ReflectionServer.RegisterLike(typeof(PaginationMode), () => UserHolder.Current != null);
             ReflectionServer.OverrideIsNamespaceAllowed.Add(typeof(DayOfWeek).Namespace!, () => UserHolder.Current != null);
             ReflectionServer.OverrideIsNamespaceAllowed.Add(typeof(CollectionMessage).Namespace!, () => UserHolder.Current != null);
         }
