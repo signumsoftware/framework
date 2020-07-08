62036650d04ef6a87739a98e444ab37355b982d2
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2020-07-08 13:42:34 +0200

    remove Newsletter

--- Signum.React.Extensions/Mailing/MailingClient.tsx
+++ Signum.React.Extensions/Mailing/MailingClient.tsx
@@ -10,7 +10,7 @@ import { PseudoType, Type, getTypeName } from '@framework/Reflection'
 import * as Operations from '@framework/Operations'
 import { EmailMessageEntity, EmailTemplateMessageEmbedded, EmailMasterTemplateEntity, EmailMasterTemplateMessageEmbedded, EmailMessageOperation, EmailPackageEntity, EmailRecipientEmbedded, EmailConfigurationEmbedded, EmailTemplateEntity, AsyncEmailSenderPermission, EmailModelEntity, IEmailOwnerEntity } from './Signum.Entities.Mailing'
 import { EmailSenderConfigurationEntity, Pop3ConfigurationEntity, Pop3ReceptionEntity, EmailAddressEmbedded } from './Signum.Entities.Mailing'
-import { NewsletterEntity, NewsletterDeliveryEntity, SendEmailTaskEntity, EmailTemplateVisibleOn } from './Signum.Entities.Mailing'
+import { SendEmailTaskEntity, EmailTemplateVisibleOn } from './Signum.Entities.Mailing'
 import * as OmniboxClient from '../Omnibox/OmniboxClient'
 import * as AuthClient from '../Authorization/AuthClient'
 import * as QuickLinks from '@framework/QuickLinks'
@@ -31,7 +31,6 @@ export var allTypes: string[] = [];
 
 export function start(options: {
   routes: JSX.Element[],
-  newsletter: boolean,
   pop3Config: boolean,
   sendEmailTask: boolean,
   contextual: boolean,
@@ -80,11 +79,6 @@ export function start(options: {
   }));
 
   Navigator.addSettings(new EntitySettings(EmailSenderConfigurationEntity, e => import('./Templates/EmailSenderConfiguration')));
-
-  if (options.newsletter) {
-    Navigator.addSettings(new EntitySettings(NewsletterEntity, e => import('./Newsletters/Newsletter')));
-    Navigator.addSettings(new EntitySettings(NewsletterDeliveryEntity, e => import('./Newsletters/NewsletterDelivery')));
-  }
 
   if (options.sendEmailTask) {
     Navigator.addSettings(new EntitySettings(SendEmailTaskEntity, e => import('./Templates/SendEmailTask')));
