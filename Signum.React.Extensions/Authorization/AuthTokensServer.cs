5a4f85166d21ab836723f106294ca0a76e6251cd
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2020-07-08 22:21:36 +0200

    revert OnAuthToken (use custom Authenticators instead)

--- Signum.React.Extensions/Authorization/AuthTokensServer.cs
+++ Signum.React.Extensions/Authorization/AuthTokensServer.cs
@@ -21,8 +21,6 @@ namespace Signum.React.Authorization
     public static class AuthTokenServer
     {
         static Func<AuthTokenConfigurationEmbedded> Configuration;
-        public static Action<UserEntity, AuthToken?, AuthToken?> OnAuthToken;
-
 
         public static void Start(Func<AuthTokenConfigurationEmbedded> tokenConfig, string hashableEncryptionKey)
         {
@@ -78,11 +76,6 @@ namespace Signum.React.Authorization
                 ctx.HttpContext.Response.Headers["New_Token"] = RefreshToken(token, out var newUser);
                 return new SignumAuthenticationResult { User = token.User };
             }
-            else
-            {
-                OnAuthToken?.Invoke(token.User, token, null);
-
-            }
 
             return new SignumAuthenticationResult { User = token.User };
         }
@@ -108,9 +101,6 @@ namespace Signum.React.Authorization
                 User = user,
                 CreationDate = TimeZoneManager.Now,
             };
-
-
-            OnAuthToken?.Invoke(user,oldToken, newToken);
 
             var result = SerializeToken(newToken);
             newUser = user;
@@ -146,8 +136,6 @@ namespace Signum.React.Authorization
                 User = user,
                 CreationDate = TimeZoneManager.Now,
             };
-
-            OnAuthToken?.Invoke(user,null, newToken);
 
             return SerializeToken(newToken);
         }
