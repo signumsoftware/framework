5f1cc3099d5091a35af0dbf4f54379d297155ce5
alejandro <alejandro.cano@signumsoftware.com>
2020-07-10 20:58:56 +0200

    OnAuthToken

--- Signum.React.Extensions/Authorization/AuthTokensServer.cs
+++ Signum.React.Extensions/Authorization/AuthTokensServer.cs
@@ -21,6 +21,7 @@ namespace Signum.React.Authorization
     public static class AuthTokenServer
     {
         static Func<AuthTokenConfigurationEmbedded> Configuration;
+        public static Action<UserEntity, AuthToken?, AuthToken?> OnAuthToken;
 
         public static void Start(Func<AuthTokenConfigurationEmbedded> tokenConfig, string hashableEncryptionKey)
         {
@@ -76,6 +77,9 @@ namespace Signum.React.Authorization
                 ctx.HttpContext.Response.Headers["New_Token"] = RefreshToken(token, out var newUser);
                 return new SignumAuthenticationResult { User = token.User };
             }
+            else {
+                OnAuthToken?.Invoke(token.User, token, null);
+            }
 
             return new SignumAuthenticationResult { User = token.User };
         }
@@ -101,6 +105,8 @@ namespace Signum.React.Authorization
                 User = user,
                 CreationDate = TimeZoneManager.Now,
             };
+
+            OnAuthToken?.Invoke(user, oldToken, newToken);
 
             var result = SerializeToken(newToken);
             newUser = user;
@@ -136,6 +142,8 @@ namespace Signum.React.Authorization
                 User = user,
                 CreationDate = TimeZoneManager.Now,
             };
+
+            OnAuthToken?.Invoke(user, null, newToken);
 
             return SerializeToken(newToken);
         }
