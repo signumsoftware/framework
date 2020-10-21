9e58a5597d7a8855bfd383278e4de619da2779d4
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2020-10-21 15:36:00 +0200

    add CultureDropdown fullNames option (by default false)

--- Signum.React.Extensions/Translation/CultureDropdown.tsx
+++ Signum.React.Extensions/Translation/CultureDropdown.tsx
@@ -7,27 +7,34 @@ import { NavDropdown } from 'react-bootstrap';
 import { useAPI } from '@framework/Hooks';
 
 
-export default function CultureDropdown(props: {}) {
+export default function CultureDropdown(p: { fullName?: boolean }) {
 
   var cultures = useAPI(signal => CultureClient.getCultures(false), []);
-  function handleSelect(c: Lite<CultureInfoEntity>) {
-    CultureClient.changeCurrentCulture(c);
-  }
 
   if (!cultures)
     return null;
 
   const current = CultureClient.currentCulture;
 
-  const pair = Dic.map(cultures, (name, c) => ({ name, c })).filter(p => is(p.c, current)).singleOrNull();
+  const pair = Dic.map(cultures, (name, c) => ({ name, c })).singleOrNull(p => is(p.c, current));
+
+  function handleSelect(c: Lite<CultureInfoEntity>) {
+    CultureClient.changeCurrentCulture(c);
+  }
+
+
 
   return (
-    <NavDropdown id="cultureDropdown" data-culture={pair?.name} title={current.nativeName} className="sf-culture-dropdown">
+    <NavDropdown id="cultureDropdown" data-culture={current.name} title={p.fullName ? current.nativeName : simplifyName(current.nativeName)} className="sf-culture-dropdown">
       {Dic.map(cultures, (name, c, i) =>
         <NavDropdown.Item key={i} data-culture={name} disabled={is(c, current)} onClick={() => handleSelect(c)}>
-          {c.toStr}
+          {p.fullName ? c.toStr : simplifyName(c.toStr!)}
         </NavDropdown.Item>
       )}
     </NavDropdown >
   );
+}
+
+function simplifyName(name: string) {
+  return name.tryBefore("(")?.trim() ?? name;
 }
