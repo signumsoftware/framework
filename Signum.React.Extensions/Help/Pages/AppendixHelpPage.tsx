09b1182a15c2b42fa872f1163d94b57106ca527d
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2020-10-22 10:55:05 +0200

    fix translations and messages

--- Signum.React.Extensions/Help/Pages/AppendixHelpPage.tsx
+++ Signum.React.Extensions/Help/Pages/AppendixHelpPage.tsx
@@ -71,7 +71,7 @@ function DeleteButton({ ctx }: { ctx: TypeContext<AppendixHelpEntity> }) {
   function onClick() {
     MessageModal.show({
       title: OperationMessage.Confirm.niceToString(),
-      message: OperationMessage.PleaseConfirmYouWantLikeToDelete0FromTheSystem.niceToString().formatHtml(<strong>{getToString(ctx.value)}</strong>),
+      message: OperationMessage.PleaseConfirmYouWouldLikeToDelete0FromTheSystem.niceToString().formatHtml(<strong>{getToString(ctx.value)}</strong>),
       buttons: "yes_no",
       icon: "warning",
       style: "warning",
