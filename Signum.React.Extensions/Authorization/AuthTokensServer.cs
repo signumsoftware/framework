af8bb8309c224c39e510dc543674129b9e8d40bc
Olmo del Corral <olmo@signumsoftware.com>
2021-01-18 22:04:21 +0100

    more on fix AuthClient

--- Signum.React.Extensions/Authorization/AuthTokensServer.cs
+++ Signum.React.Extensions/Authorization/AuthTokensServer.cs
@@ -23,7 +23,6 @@ namespace Signum.React.Authorization
     public static class AuthTokenServer
     {
         public static Func<AuthTokenConfigurationEmbedded> Configuration;
-        static Func<AuthTokenConfigurationEmbedded> Configuration;
         public static Action<UserEntity, AuthToken?, AuthToken?> OnAuthToken;
 
         public static void Start(Func<AuthTokenConfigurationEmbedded> tokenConfig, string hashableEncryptionKey)
@@ -80,7 +79,8 @@ namespace Signum.React.Authorization
                 ctx.HttpContext.Response.Headers["New_Token"] = RefreshToken(token, out var newUser);
                 return new SignumAuthenticationResult { User = token.User };
             }
-            else {
+            else
+            {
                 OnAuthToken?.Invoke(token.User, token, null);
             }
 
