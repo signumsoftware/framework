7e27ad8941e84e95266a624fbc1efae2adb4d9eb
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2021-02-12 13:05:57 +0100

    fix Warning level

--- Signum.Upgrade/Upgrades/Upgrade_20210210_UpgradeNugets.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20210210_UpgradeNugets.cs
@@ -18,6 +18,7 @@ namespace Signum.Upgrade.Upgrades
             uctx.ChangeCodeFile(@"Southwind.React\Southwind.React.csproj", file =>
             {
                 file.UpdateNugetReference("Microsoft.TypeScript.MSBuild", "4.1.4");
+                file.WarningLevel = WarningLevel.Warning;
                 file.UpdateNugetReference("Swashbuckle.AspNetCore", "6.0.3");
             });
 
