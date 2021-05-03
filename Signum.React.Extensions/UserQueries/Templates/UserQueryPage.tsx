928d0548245251aa5fc8165845b1328be8a6b9b9
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2021-05-03 10:14:13 +0200

    fix vertical scroll, title and maximize button in UserQueryPage

--- Signum.React.Extensions/UserQueries/Templates/UserQueryPage.tsx
+++ Signum.React.Extensions/UserQueries/Templates/UserQueryPage.tsx
@@ -12,6 +12,8 @@ import * as UserQueryClient from '../UserQueryClient'
 import { RouteComponentProps } from "react-router";
 import { useAPI } from '@framework/Hooks'
 import { useState } from 'react'
+import { translated } from '../../Translation/TranslatedInstanceTools'
+import SearchPage from '../../../../Framework/Signum.React/Scripts/SearchControl/SearchPage'
 
 interface UserQueryPageProps extends RouteComponentProps<{ userQueryId: string; entity?: string }> {
 
@@ -33,27 +35,51 @@ export default function UserQueryPage(p: UserQueryPageProps) {
       })
   }, [userQueryId, entity]);
 
+  function onResize() {
+    const sc = searchControl.current;
+    const scl = sc?.searchControlLoaded;
+    const containerDiv = scl?.containerDiv;
+    if (containerDiv) {
+
+      const marginTop = containerDiv.offsetTop;
+      const maxHeight = (window.innerHeight - (marginTop + SearchPage.marginDown));
+      containerDiv.style.maxHeight = Math.max(maxHeight, SearchPage.minHeight) + "px";
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
   const searchControl = React.useRef<SearchControlHandler>(null);
 
-  if (fo == undefined)
+  if (fo == undefined || currentUserQuery == null)
     return null;
 
   return (
-    <div id="divSearchPage">
+    <div id="divSearchPage" className="sf-search-page">
       <h2>
         <span className="sf-entity-title">{getQueryNiceName(fo.queryName)}</span>&nbsp;
         <a className="sf-popup-fullscreen" href="#" onClick={(e) => searchControl.current!.searchControlLoaded!.handleFullScreenClick(e)}>
           <FontAwesomeIcon icon="external-link-alt" />
         </a>
+        <p className="lead">
+          ({translated(currentUserQuery, a => a.displayName)})
+      </p>
       </h2>
-      {currentUserQuery && < SearchControl ref={searchControl}
+
+      {currentUserQuery && <SearchControl ref={searchControl}
         hideFullScreenButton={true}
         largeToolbarButtons={true}
+        defaultIncludeDefaultFilters={true}
         extraOptions={{ userQuery: newLite(UserQueryEntity, userQueryId) }}
         defaultRefreshMode={currentUserQuery.refreshMode}
         searchOnLoad={currentUserQuery.refreshMode == "Auto"}
+        onHeighChanged={onResize}
         showBarExtension={true}
-        findOptions={fo} />
+        findOptions={fo}
+      />
       }
     </div>
   );
