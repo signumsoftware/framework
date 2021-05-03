a200057aa3a1685271e8797696b96c2af9675614
Thomas Münzer <thomas.muenzer@bechtle.com>
2021-05-03 13:41:14 +0200

    Changes UI

--- Signum.React.Extensions/Workflow/Case/CaseFrameModal.tsx
+++ Signum.React.Extensions/Workflow/Case/CaseFrameModal.tsx
@@ -248,8 +248,10 @@ export default class CaseFrameModal extends React.Component<CaseFrameModalProps,
 
     return (
       <div className="sf-main-entity case-main-entity" data-main-entity={entityInfo(mainEntity)}>
-        {renderWidgets(wc)}
-        {this.entityComponent && !mainEntity.isNew && !pack.activity.doneBy ? <ButtonBar ref={bb => this.buttonBar = bb} frame={mainFrame} pack={mainFrame.pack} /> : <br />}
+        <div className="sf-button-widget-container">
+          {this.entityComponent && !mainEntity.isNew && !pack.activity.doneBy ? <ButtonBar ref={bb => this.buttonBar = bb} frame={mainFrame} pack={mainFrame.pack} /> : <br />}
+          {renderWidgets(wc)}
+        </div>
         <ValidationErrors entity={mainEntity} ref={ve => this.validationErrorsTop = ve} prefix={this.prefix} />
         <ErrorBoundary>
           {this.state.getComponent && <AutoFocus>{FunctionalAdapter.withRef(this.state.getComponent(ctx), c => this.setComponent(c))}</AutoFocus>}
