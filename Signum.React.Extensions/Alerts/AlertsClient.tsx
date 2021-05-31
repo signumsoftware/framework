c2fba0e5e272d97904a19bcf816133d06871b645
Thomas Münzer <thomas.muenzer@bechtle.com>
2021-05-31 15:28:25 +0200

    allow arbitrary links in Alerts

--- Signum.React.Extensions/Alerts/AlertsClient.tsx
+++ Signum.React.Extensions/Alerts/AlertsClient.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 import { DateTime } from 'luxon'
 import { EntitySettings } from '@framework/Navigator'
 import * as Navigator from '@framework/Navigator'
@@ -12,8 +13,9 @@ import { andClose } from '@framework/Operations/EntityOperations';
 import * as AuthClient from '../Authorization/AuthClient'
 import { ajaxGet } from '@framework/Services'
 import * as Finder from '@framework/Finder'
-import { Entity, Lite } from '@framework/Signum.Entities'
+import { Entity, isEntity, isLite, Lite, toLite } from '@framework/Signum.Entities'
 import { EntityLink } from '@framework/Search'
+import Alert from './Templates/Alert'
 
 export function start(options: { routes: JSX.Element[], showAlerts?: (typeName: string, when: "CreateAlert" | "QuickLink") => boolean }) {
   Navigator.addSettings(new EntitySettings(AlertEntity, e => import('./Templates/Alert')));
@@ -50,7 +52,12 @@ export function start(options: { routes: JSX.Element[], showAlerts?: (typeName: 
   }));
 
   Finder.registerPropertyFormatter(AlertEntity.tryPropertyRoute(a => a.text), new Finder.CellFormatter((cell, ctx) => {
-    return formatText(cell, ctx.row.columns[ctx.columns.indexOf("Target")]);
+
+    var alert: Partial<AlertEntity> = {
+      target: ctx.row.columns[ctx.columns.indexOf("Target")]
+    };
+
+    return formatText(cell, alert);
   }));
 
   Finder.addSettings({
@@ -92,38 +99,65 @@ function chooseDate(): Promise<DateTime | undefined> {
   });
 }
 
+let LinkPlaceholder = /\[(?<prop>(\w|\d|\.)+)(\:(?<text>.+))?\](\((?<url>.+)\))?/g;
 
-export function formatText(text: string, target: Lite<Entity> | null, onNavigated?: () => void) {
-  if (!target)
-    return text;
+export function formatText(text: string, alert: Partial<AlertEntity>, onNavigated?: () => void): React.ReactElement{
 
-  if (text.contains("[Target]"))
-    return (
-      <>
-        {text.before("[Target]")}
-        <EntityLink lite={target} onNavigated={onNavigated} />
-        {text.after("[Target]")}
-      </>
-    );
+  debugger;
+  var nodes: (string | React.ReactElement)[] = [];
+  var pos = 0;
+  for (const match of Array.from(text.matchAll(LinkPlaceholder))) {
+    nodes.push(text.substring(pos, match.index))
 
-  if (text.contains("[Target:"))
-    return (
-      <>
-        {text.before("[Target:")}
-        <EntityLink lite={target} onNavigated={onNavigated}>{text.after("[Target:").beforeLast("]")}</EntityLink>
-        {text.afterLast("]")}
-      </>
-    );
+    var groups = (match as any).groups as { prop: string, text?: string, url?: string };
 
-  return (
-    <>
-      {text}
-      <br />
-      <EntityLink lite={target} onNavigated={onNavigated}/>
-    </>
-  );
+    var prop = getPropertyValue(groups.prop, alert);
+
+    var lite = isEntity(prop) ? toLite(prop) :
+      isLite(prop) ? prop : null;
+
+    if (groups.url)
+      nodes.push(<Link to={replacePlaceHolders(groups.url, alert)!}>{replacePlaceHolders(groups.text, alert) ?? lite?.toStr}</Link>);
+    else if (lite != null) {
+      if (groups.text)
+        nodes.push(<EntityLink lite={lite}>{replacePlaceHolders(groups.text, alert)}</EntityLink>);
+      else
+        nodes.push(<EntityLink lite={lite}/>);
+    }
+
+    pos = match.index! + match[0].length;
+  }
+
+  nodes.push(text.substring(pos));
+
+  if (nodes.length == 1 && alert.target != null) {
+    nodes.push(<br />);
+    nodes.push(<EntityLink lite={alert.target} />)
+  }
+
+  return React.createElement(React.Fragment, {}, ...nodes);
 }
 
+const TextPlaceholder = /{(?<prop>(\w|\d|\.)+)}/
+
+function replacePlaceHolders(value: string | null | undefined, alert: Partial<AlertEntity>) {
+  if (value == null)
+    return null;
+
+  return value.replace(TextPlaceholder, args => {
+    return getPropertyValue(args[1], alert)?.ToString()!;
+  });
+}
+
+function getPropertyValue(str: string, object: any) : any{
+  if (str.contains(".")) {
+    var obj2 = getPropertyValue(str.before("."), object);
+
+    return obj2 == null ? obj2 : obj2[str.after(".").firstLower()];
+  }
+
+  return object[str.firstLower()]
+}
 
 export module API {
 
