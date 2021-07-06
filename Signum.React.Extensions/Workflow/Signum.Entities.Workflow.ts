1aae1824183a7393103bf558fd4c5c38a6f0f219
Thomas Münzer <thomas.muenzer@bechtle.com>
2021-07-06 14:03:22 +0200

    BudgetSystem

--- Signum.React.Extensions/Workflow/Signum.Entities.Workflow.ts
+++ Signum.React.Extensions/Workflow/Signum.Entities.Workflow.ts
@@ -436,6 +436,7 @@ export interface WorkflowConfigurationEmbedded extends Entities.EmbeddedEntity {
   scriptRunnerPeriod: number;
   avoidExecutingScriptsOlderThan: number | null;
   chunkSizeRunningScripts: number;
+  isBudgetActive: boolean;
 }
 
 export const WorkflowConnectionEntity = new Type<WorkflowConnectionEntity>("WorkflowConnection");
