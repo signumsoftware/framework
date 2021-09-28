c43f30638c472f3bea1633e60e8a002b0f1165ed
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2021-09-28 08:26:43 +0200

    remove new CSharpParseOptions(LanguageVersion.CSharp8) in EvalEmbedded to default to latest version

--- Signum.Entities.Extensions/Dynamic/EvalEmbedded.cs
+++ Signum.Entities.Extensions/Dynamic/EvalEmbedded.cs
@@ -74,7 +74,7 @@ namespace Signum.Entities.Dynamic
                 {
                     try
                     {
-                        var tree = SyntaxFactory.ParseSyntaxTree(code, options: new CSharpParseOptions(LanguageVersion.CSharp8));
+                        var tree = SyntaxFactory.ParseSyntaxTree(code);
                         
                         var compilation = CSharpCompilation.Create($"{Guid.NewGuid()}.dll")
                          .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, nullableContextOptions: NullableContextOptions.Enable))
