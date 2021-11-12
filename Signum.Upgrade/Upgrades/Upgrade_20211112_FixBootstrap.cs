54c71c2739c81a59735ce10e866a6ac83e3531ee
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2021-11-12 15:37:06 +0100

    fixes bootstrap

---
+++ Signum.Upgrade/Upgrades/Upgrade_20211112_FixBootstrap.cs
@@ -0,0 +1,34 @@
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20211112_FixBootstrap : CodeUpgradeBase
+{
+    public override string Description => "Set Production mode";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+        uctx.ChangeCodeFile($@"Southwind.React\Dockerfile", file =>
+        {
+            file.Replace("build-development", "build-production");
+        });
+
+        uctx.ChangeCodeFile($@"Southwind.React\App\Layout.tsx", file =>
+        {
+            file.Replace("supportIE = true", "supportIE = false");
+        });
+
+        uctx.ChangeCodeFile($@"Southwind.React\App\SCSS\custom.scss", file =>
+        {
+            file.InsertAfterFirstLine(a => a.Contains(@"@import ""./_bootswatch"";"), @".btn.input-group-text{
+    background: $input-group-addon-bg;
+    border: $input-border-width solid $input-group-addon-border-color
+}");
+        });
+
+        uctx.ChangeCodeFile(@$"Southwind.React\package.json", file =>
+        {
+            file.Replace(
+                @"webpack --config webpack.config.polyfills.js webpack --config webpack.config.dll.js --mode=production",
+                @"webpack --config webpack.config.polyfills.js --mode=production && webpack --config webpack.config.dll.js --mode=production")
+        });
+    }
+}
