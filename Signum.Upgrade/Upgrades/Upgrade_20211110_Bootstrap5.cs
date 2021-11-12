1f4c904831ecbd6ec9b0021aab340290da4260a9
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2021-11-12 15:36:43 +0100

    fix merge

--- Signum.Upgrade/Upgrades/Upgrade_20211110_Bootstrap5.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20211110_Bootstrap5.cs
@@ -1,30 +1,22 @@
-using Signum.Utilities;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace Signum.Upgrade.Upgrades
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20211110_Bootstrap5 : CodeUpgradeBase
 {
-    class Upgrade_20211110_Bootstrap5 : CodeUpgradeBase
+    public override string Description => "Upgrade to Bootstrap 5 and many other NPM packages";
+
+    public override void Execute(UpgradeContext uctx)
     {
-        public override string Description => "Upgrade to Bootstrap 5 and many other NPM packages";
-
-        public override void Execute(UpgradeContext uctx)
+        uctx.ChangeCodeFile(@"Southwind.React/package.json", file =>
         {
-            uctx.ChangeCodeFile(@"Southwind.React/package.json", file =>
-            {
-                if (!file.Content.Contains(@"""immutable"""))
-                    file.InsertAfterFirstLine(a => a.Contains("draft-js"), @"""immutable"": ""3.8.2"",");
+            if (!file.Content.Contains(@"""immutable"""))
+                file.InsertAfterFirstLine(a => a.Contains("draft-js"), @"""immutable"": ""3.8.2"",");
 
-                file.RemoveAllLines(a => a.Contains("jquery")); //Rome https://trends.google.com/trends/explore?date=all&geo=DE&q=jquery,react
+            file.RemoveAllLines(a => a.Contains("jquery")); //Rome https://trends.google.com/trends/explore?date=all&geo=DE&q=jquery,react
                 file.ReplaceLine(a => a.Contains("popper.js"), @"""@popperjs/core"": ""2.10.2"",");
 
-                file.UpdateNpmPackages(@"
+            file.UpdateNpmPackages(@"
 ""@fortawesome/fontawesome-svg-core"": ""1.2.36"",
     ""@fortawesome/free-regular-svg-icons"": ""5.15.4"",
     ""@fortawesome/free-solid-svg-icons"": ""5.15.4"",
@@ -67,24 +59,23 @@ namespace Signum.Upgrade.Upgrades
     ""webpack-bundle-analyzer"": ""4.5.0"",
     ""webpack-cli"": ""4.9.1"",
     ""webpack-notifier"": ""1.14.1""");
-            });
+        });
 
-            var allDirectories = new[] { uctx.RootFolder, Path.Combine(uctx.RootFolder, "Framework") };
+        var allDirectories = new[] { uctx.RootFolder, Path.Combine(uctx.RootFolder, "Framework") };
 
-            uctx.ForeachCodeFile(@"*.ts, *.tsx", allDirectories, file =>
-            {
-                file.Replace(new Regex(@"\bml-([1234]|auto)\b"), "ms-$1");
-                file.Replace(new Regex(@"\bmr-([1234]|auto)\b"), "me-$1");
-                file.Replace(new Regex(@"\bpl-([1234]|auto)\b"), "ps-$1");
-                file.Replace(new Regex(@"\bpr-([1234]|auto)\b"), "pe-$1");
-                file.Replace(new Regex(@"\bpr-([1234]|auto)\b"), "pe-$1");
-                file.Replace(new Regex(@"\bfloat-left\b"), "float-start");
-                file.Replace(new Regex(@"\bfloat-right\b"), "float-end");
-                file.Replace(new Regex(@"\btext-left\b"), "text-start");
-                file.Replace(new Regex(@"\btext-right\b"), "text-end");
-                file.Replace(new Regex(@"\btext-right\b"), "text-end");
+        uctx.ForeachCodeFile(@"*.ts, *.tsx", allDirectories, file =>
+        {
+            file.Replace(new Regex(@"\bml-([1234]|auto)\b"), "ms-$1");
+            file.Replace(new Regex(@"\bmr-([1234]|auto)\b"), "me-$1");
+            file.Replace(new Regex(@"\bpl-([1234]|auto)\b"), "ps-$1");
+            file.Replace(new Regex(@"\bpr-([1234]|auto)\b"), "pe-$1");
+            file.Replace(new Regex(@"\bpr-([1234]|auto)\b"), "pe-$1");
+            file.Replace(new Regex(@"\bfloat-left\b"), "float-start");
+            file.Replace(new Regex(@"\bfloat-right\b"), "float-end");
+            file.Replace(new Regex(@"\btext-left\b"), "text-start");
+            file.Replace(new Regex(@"\btext-right\b"), "text-end");
+            file.Replace(new Regex(@"\btext-right\b"), "text-end");
 
-            });
-        }
+        });
     }
 }
--- Signum.Upgrade/Upgrades/Upgrade_20211111_ReplaceDefaultExecute.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20211111_ReplaceDefaultExecute.cs
@@ -1,25 +1,14 @@
-using Signum.Utilities;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+namespace Signum.Upgrade.Upgrades;
 
-namespace Signum.Upgrade.Upgrades
+class Upgrade_20211111_ReplaceDefaultExecute : CodeUpgradeBase
 {
-    class Upgrade_20211006_ReplaceDefaultExecute : CodeUpgradeBase
-    {
-        public override string Description => "Replace eoc.defaultClick( with return eoc.defaultClick(";
+    public override string Description => "Replace eoc.defaultClick( with return eoc.defaultClick(";
 
-        public override void Execute(UpgradeContext uctx)
+    public override void Execute(UpgradeContext uctx)
+    {
+        uctx.ForeachCodeFile($@"*.tsx", uctx.ReactDirectory, file =>
         {
-            uctx.ForeachCodeFile($@"*.tsx", uctx.ReactDirectory, file =>
-            {
-                file.Replace("eoc.defaultClick(", "return eoc.defaultClick(");
-            });
-        }
+            file.Replace("eoc.defaultClick(", "return eoc.defaultClick(");
+        });
     }
 }
