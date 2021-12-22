18d765b6670020d13cea46110ba9ae8cec25e0c0
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2021-12-22 20:57:48 +0100

    remove CacheConfigurationEmbedded

--- Signum.React.Extensions/Cache/Signum.Entities.Cache.ts
+++ Signum.React.Extensions/Cache/Signum.Entities.Cache.ts
@@ -7,22 +7,9 @@ import * as Entities from '../../Signum.React/Scripts/Signum.Entities'
 import * as Authorization from '../Authorization/Signum.Entities.Authorization'
 
 
-export const CacheConfigurationEmbedded = new Type<CacheConfigurationEmbedded>("CacheConfigurationEmbedded");
-export interface CacheConfigurationEmbedded extends Entities.EmbeddedEntity {
-  Type: "CacheConfigurationEmbedded";
-  commonSecret: string;
-  serverInstances: Entities.MList<ServereInstanceEmbedded>;
-}
-
 export module CachePermission {
   export const ViewCache : Authorization.PermissionSymbol = registerSymbol("Permission", "CachePermission.ViewCache");
   export const InvalidateCache : Authorization.PermissionSymbol = registerSymbol("Permission", "CachePermission.InvalidateCache");
-}
-
-export const ServereInstanceEmbedded = new Type<ServereInstanceEmbedded>("ServereInstanceEmbedded");
-export interface ServereInstanceEmbedded extends Entities.EmbeddedEntity {
-  Type: "ServereInstanceEmbedded";
-  url: string;
 }
 
 
