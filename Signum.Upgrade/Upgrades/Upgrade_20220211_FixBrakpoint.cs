0ade34b93694f748455b6d1edc2186210d177bdd
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2022-02-11 12:39:24 +0100

    add Upgrade_20220211_FixBrakpoint

---
+++ Signum.Upgrade/Upgrades/Upgrade_20220211_FixBrakpoint.cs
@@ -0,0 +1,16 @@
+using Signum.Utilities;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20220211_FixBrakpoint : CodeUpgradeBase
+{
+    public override string Description => "Change Breakpoint to sm because breakpoints round down";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+        uctx.ChangeCodeFile(@"Southwind.React\App\Layout.tsx", file =>
+        {
+            file.Replace("Breakpoints.sm", "Breakpoints.md"); 
+        });
+    }
+}
