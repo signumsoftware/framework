6e9e7d916ac5daa22f3cc643fa153431c771c96c
mehdy-karimpour <mehdy.karimpour@signumsoftware.com>
2022-04-13 17:46:41 +0430

    support query strings

--- Signum.Entities/ValidationAttributes.cs
+++ Signum.Entities/ValidationAttributes.cs
@@ -331,6 +331,7 @@ public class URLValidatorAttribute : RegexValidatorAttribute
         + @"([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]" // second level domain
         + @"(\.[a-z]{2,6})?)" // first level domain- .com or .museum
         + "(:[0-9]{1,4})?" // port number- :80
+        + @"(((\?|\&)([^=]+)\=([^&]+))*)?"
         + "((/?)|" // a slash isn't required if there is no file name
         + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$", RegexOptions.IgnoreCase);
 
