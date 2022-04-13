0b1b1d673bd1e6f84eb4bbcc029a5abef50643bb
mehdy-karimpour <mehdy.karimpour@signumsoftware.com>
2022-04-13 17:46:54 +0430

    add url variables

--- Signum.React.Extensions/Dashboard/View/LinkListPart.tsx
+++ Signum.React.Extensions/Dashboard/View/LinkListPart.tsx
@@ -3,21 +3,32 @@ import * as React from 'react'
 import * as AppContext from '@framework/AppContext'
 import { LinkListPartEntity, LinkElementEmbedded } from '../Signum.Entities.Dashboard'
 import { PanelPartContentProps } from '../DashboardClient';
+import { urlVariables } from '../UrlVariables';
+import { Dic } from '@framework/Globals';
 
 export default function LinkListPart(p: PanelPartContentProps<LinkListPartEntity >){
   return (
     <ul className="sf-cp-link-list">
       {
         p.content.links.map(mle => mle.element)
-          .map((le, i) =>
-            <li key={i} >
-              <a href={AppContext.toAbsoluteUrl(le.link!)}
-                target={le.opensInNewTab ? "_blank" : undefined}
-                onClick={le.link!.startsWith("~") ? (e => { e.preventDefault(); AppContext.history.push(le.link!) }) : undefined}
-                title={le.label!}>
-                {le.label}
-              </a>
-            </li>)
+          .map((le, i) => {
+
+            var link = le.link;
+            Dic.getKeys(urlVariables).forEach(v => {
+              link = link.replaceAll(v, urlVariables[v]());
+            });
+
+            return (
+              <li key={i} >
+                <a href={AppContext.toAbsoluteUrl(link)}
+                  target={le.opensInNewTab ? "_blank" : undefined}
+                  onClick={le.link!.startsWith("~") ? (e => { e.preventDefault(); AppContext.history.push(link) }) : undefined}
+                  title={le.label!}>
+                  {le.label}
+                </a>
+              </li>
+            );
+          })
       }
     </ul>
   );
