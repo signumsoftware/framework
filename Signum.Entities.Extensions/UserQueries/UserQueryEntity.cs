97d0bde47c6bdb7f7cccd939007ddf079f831034
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2022-08-10 16:24:42 +0200

    fix CanToArray in UserQueryEntity

--- Signum.Entities.Extensions/UserQueries/UserQueryEntity.cs
+++ Signum.Entities.Extensions/UserQueries/UserQueryEntity.cs
@@ -89,13 +89,12 @@ public class UserQueryEntity : Entity, IUserAssetEntity, IHasEntityType
     internal void ParseData(QueryDescription description)
     {
         var canAggregate = this.GroupResults ? SubTokensOptions.CanAggregate : 0;
-        var canAggregateXorOperation = canAggregate != 0 ? canAggregate : SubTokensOptions.CanOperation;
 
         foreach (var f in Filters)
             f.ParseData(this, description, SubTokensOptions.CanAnyAll | SubTokensOptions.CanElement | canAggregate);
 
         foreach (var c in Columns)
-            c.ParseData(this, description, SubTokensOptions.CanElement | canAggregateXorOperation);
+            c.ParseData(this, description, SubTokensOptions.CanElement | SubTokensOptions.CanToArray | (canAggregate != 0 ? canAggregate : SubTokensOptions.CanOperation));
 
         foreach (var o in Orders)
             o.ParseData(this, description, SubTokensOptions.CanElement | canAggregate);
