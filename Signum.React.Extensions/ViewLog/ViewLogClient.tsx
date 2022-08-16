4d4382d455443f733e22da0b4b07e7b87b88f3e5
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2022-08-16 16:59:42 +0200

    fix ViewLog QuickLink

--- Signum.React.Extensions/ViewLog/ViewLogClient.tsx
+++ Signum.React.Extensions/ViewLog/ViewLogClient.tsx
@@ -1,13 +1,16 @@
 import * as QuickLinks from '@framework/QuickLinks'
+import * as Navigator from '@framework/Navigator'
 import { ViewLogEntity } from './Signum.Entities.ViewLog'
 
-export function start(options: { routes: JSX.Element[], showAlerts?: (typeName: string, when: "CreateAlert" | "QuickLink") => boolean }) {
+export function start(options: { routes: JSX.Element[], showQuickLink?: (typeName: string) => boolean }) {
+
   QuickLinks.registerGlobalQuickLink(ctx => new QuickLinks.QuickLinkExplore({
     queryName: ViewLogEntity,
     filterOptions: [{ token: ViewLogEntity.token(e => e.target), value: ctx.lite}]
   }, {
-    icon: "search",
-    iconColor: "green",
+    isVisible: Navigator.isFindable(ViewLogEntity) && (options.showQuickLink == null || options.showQuickLink(ctx.lite.EntityType)),
+    icon: "eye",
+    iconColor: "#2E86C1",
   }));
 }
 
