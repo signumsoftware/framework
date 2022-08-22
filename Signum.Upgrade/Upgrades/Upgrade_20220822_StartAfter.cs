2eb1f9e0943589c5c07687398e66a3cec96e0ed5
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2022-08-22 17:00:01 +0200

    add Upgrade_20220822_StartAfter

---
+++ Signum.Upgrade/Upgrades/Upgrade_20220822_StartAfter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20220822_StartAfter : CodeUpgradeBase
+{
+    public override string Description => "Rename StartXXAfter methods";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+        uctx.ChangeCodeFile($@"Southwind.React\Startup.cs", file =>
+        {
+            file.Replace(
+                "ProcessRunnerLogic.StartRunningProcesses(",
+                "ProcessRunner.StartRunningProcessesAfter(");
+
+            file.Replace(
+                "SchedulerLogic.StartScheduledTasks(",
+                "ScheduleTaskRunner.StartScheduledTaskAfter(");
+
+            file.Replace(
+                "AsyncEmailSenderLogic.StartRunningEmailSenderAsync(",
+                "AsyncEmailSender.StartAsyncEmailSenderAfter(");
+
+            file.Replace(
+                "ProcessRunnerLogic.StartRunningProcesses(",
+                "ProcessRunner.StartRunningProcessesAfter(");
+        });
+    }
+}
