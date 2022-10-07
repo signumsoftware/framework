2e237d336beed7b1942dcd97af7b85a739356794
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2022-10-07 08:44:56 +0200

    fix Upgrade_20221006_DraftJS

--- Signum.Upgrade/Upgrades/Upgrade_20221006_DraftJS.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20221006_DraftJS.cs
@@ -11,10 +11,10 @@ class Upgrade_20221006_DraftJS : CodeUpgradeBase
     {
         uctx.ForeachCodeFile("package.json", file =>
         {
-            file.InsertAfterLastLine(a => a.Contains("@fortawesome"), "\"@types/draft-js\": \"0.11.9\"");
-            file.InsertBeforeFirstLine(a => a.Contains("\"history\""), "\"draft-js\": \"0.11.7\"");
-            file.InsertBeforeFirstLine(a => a.Contains("\"history\""), "\"draftjs-to-html\": \"0.9.1\"");
-            file.InsertAfterFirstLine(a => a.Contains("\"history\""), "\"html-to-draftjs\": \"1.5.0\"");
+            file.InsertAfterLastLine(a => a.Contains("@fortawesome"), "\"@types/draft-js\": \"0.11.9\",");
+            file.InsertBeforeFirstLine(a => a.Contains("\"history\""), "\"draft-js\": \"0.11.7\",");
+            file.InsertBeforeFirstLine(a => a.Contains("\"history\""), "\"draftjs-to-html\": \"0.9.1\",");
+            file.InsertAfterFirstLine(a => a.Contains("\"history\""), "\"html-to-draftjs\": \"1.5.0\",");
         });     
     }
 }
