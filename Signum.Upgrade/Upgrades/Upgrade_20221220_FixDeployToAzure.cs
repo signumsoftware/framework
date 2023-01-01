97a2fcff678df18745fc6b7726b57d1996568634
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-01-01 18:25:35 +0100

    fix Upgrade_20221220_FixDeployToAzure

--- Signum.Upgrade/Upgrades/Upgrade_20221220_FixDeployToAzure.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20221220_FixDeployToAzure.cs
@@ -20,8 +20,8 @@ class Upgrade_20221220_FixDeployToAzure : CodeUpgradeBase
             file.Replace("Start-Process ", @"$p = (Start-Process ");
             file.Replace("-NoNewWindow -Wait", @"-NoNewWindow -Wait -PassThru)");
             file.Replace(
-                """if(-Not $?){ Write-Host '"SQL Migration" failed' -ForegroundColor DarkRed; exit; }""",
-                """if($p.ExitCode -eq -1){ Write-Host '"SQL Migration" failed' -ForegroundColor DarkRed; exit; }""");
-        });
+                """if(-Not $?){ Write-Host '"SQL Migrations" failed' -ForegroundColor DarkRed; exit; }""",
+                """if($p.ExitCode -eq -1){ Write-Host '"SQL Migrations" failed' -ForegroundColor DarkRed; exit; }""");
+        }, WarningLevel.Warning);
     }
 }
