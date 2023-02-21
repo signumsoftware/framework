258c506d06f614813a9e3ef4ea6dcef0af49276b
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-02-22 00:00:07 +0100

    Translations

--- Signum.Engine/Basics/QueryLogic.cs
+++ Signum.Engine/Basics/QueryLogic.cs
@@ -21,7 +21,7 @@ public static class QueryLogic
     {
         QueryToken.StaticEntityExtensions = parent => Expressions.GetExtensionsTokens(parent);
         QueryToken.DynamicEntityExtensions = parent => Expressions.GetExtensionsWithParameterTokens(parent);
-        EntityPropertyToken.DateTimeKindField = ept => Schema.Current.Field(ept.PropertyRoute) is FieldValue fv ? fv.DateTimeKind : DateTimeKind.Unspecified;
+        EntityPropertyToken.DateTimeKindField = ept => Schema.Current.TryField(ept.PropertyRoute) is FieldValue fv ? fv.DateTimeKind : DateTimeKind.Unspecified;
         ExtensionToken.BuildExtension = (parentType, key, parentExpression) => Expressions.BuildExtension(parentType, key, parentExpression);
         QueryToken.ImplementedByAllSubTokens = GetImplementedByAllSubTokens;
         QueryToken.IsSystemVersioned = IsSystemVersioned;
