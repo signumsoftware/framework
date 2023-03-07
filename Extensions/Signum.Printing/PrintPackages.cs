6ae72f88c833ccb8954927094e1d8595faa9a278
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-03-07 21:54:03 +0100

    Extract Signum.Printing

---
+++ Extensions/Signum.Printing/PrintPackages.cs
@@ -0,0 +1,13 @@
+using Signum.Entities.Processes;
+
+namespace Signum.Entities.Printing;
+
+[EntityKind(EntityKind.System, EntityData.Transactional)]
+public class PrintPackageEntity : Entity, IProcessDataEntity
+{
+    [StringLengthValidator(Max = 200)]
+    public string? Name { get; set; }
+
+    [AutoExpressionField]
+    public override string ToString() => As.Expression(() => Name ?? "- No Name -");
+}
---
+++ Extensions/Signum.Printing/PrintServer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Signum.React.Printing;
+
+public static class PrintServer
+{
+    public static void Start(IApplicationBuilder app)
+    {
+        SignumControllerFactory.RegisterArea(MethodInfo.GetCurrentMethod());
+
+    }
+}
