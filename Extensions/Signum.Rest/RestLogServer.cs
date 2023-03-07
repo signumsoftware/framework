d9b1324c1c5d054b74080a8c1d2c3edf7ae6f729
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-03-07 21:54:06 +0100

    Extract Signum.Rest

---
+++ Extensions/Signum.Rest/RestLogServer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Signum.React.RestLog;
+
+public static class RestLogServer
+{
+    public static void Start(IApplicationBuilder app)
+    {
+        SignumControllerFactory.RegisterArea(MethodInfo.GetCurrentMethod());
+
+
+    }
+}
