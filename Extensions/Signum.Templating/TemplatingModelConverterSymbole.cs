f6c51c8bd0cb4bcbfb26c70bbb04af7b96ab7b9c
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-03-07 21:54:10 +0100

    Extract Signum.Templating

---
+++ Extensions/Signum.Templating/TemplatingModelConverterSymbole.cs
@@ -0,0 +1,13 @@
+
+namespace Signum.Entities.Templating;
+
+[EntityKind(EntityKind.SystemString, EntityData.Master, IsLowPopulation = true)]
+public class ModelConverterSymbol : Symbol
+{
+    private ModelConverterSymbol() { }
+
+    public ModelConverterSymbol(Type declaringType, string fieldName) :
+        base(declaringType, fieldName)
+    {
+    }
+}
