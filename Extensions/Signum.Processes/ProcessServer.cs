f5c15a59e27dc0bb757cfca318415624589871ec
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-03-07 21:54:04 +0100

    Extract Signum.Processes

---
+++ Extensions/Signum.Processes/ProcessServer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Signum.React.Processes;
+
+public static class ProcessServer
+{
+    public static void Start(IApplicationBuilder app)
+    {
+        SignumControllerFactory.RegisterArea(MethodInfo.GetCurrentMethod());
+
+    }
+}
