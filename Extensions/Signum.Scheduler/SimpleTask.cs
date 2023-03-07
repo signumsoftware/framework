5aee9a654d6fcc96145ed36f819a825e3de0f404
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-03-07 21:54:08 +0100

    Extract Signum.Scheduler

---
+++ Extensions/Signum.Scheduler/SimpleTask.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Signum.Entities.Scheduler;
+
+[EntityKind(EntityKind.SystemString, EntityData.Master)]
+public class SimpleTaskSymbol : Symbol, ITaskEntity
+{
+    private SimpleTaskSymbol() { }
+
+    public SimpleTaskSymbol(Type declaringType, string fieldName) :
+        base(declaringType, fieldName)
+    {
+    }
+}
