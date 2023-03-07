f23061746e3ff2f8d119dcedf42a0aedfd24d3f2
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-03-07 21:54:12 +0100

    Extract Signum.Toolbar

---
+++ Extensions/Signum.Toolbar/ToolbarServer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Signum.React.Toolbar;
+
+public static class ToolbarServer
+{
+    public static void Start(IApplicationBuilder app)
+    {
+        SignumControllerFactory.RegisterArea(MethodInfo.GetCurrentMethod());
+    }
+}
