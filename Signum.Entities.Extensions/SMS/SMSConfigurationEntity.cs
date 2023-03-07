6dbdda1c7bf48a4da62ca369ab52ebe059d92d04
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-03-07 21:54:09 +0100

    Extract Signum.SMS

--- Signum.Entities.Extensions/SMS/SMSConfigurationEntity.cs
+++
@@ -1,8 +0,0 @@
-using Signum.Entities.Basics;
-
-namespace Signum.Entities.SMS;
-
-public class SMSConfigurationEmbedded : EmbeddedEntity
-{
-    public CultureInfoEntity DefaultCulture { get; set; }
-}
