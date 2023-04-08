6f62b6bb1319b351d58d4d749a3b23943a9f4398
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-04-08 20:32:07 +0200

    fix Signum.Templating

--- Extensions/Signum.Templating/TemplateTokenMessage.cs
+++ Extensions/Signum.Templating/TemplateTokenMessage.cs
@@ -18,32 +18,3 @@ public enum TemplateTokenMessage
     [Description("Impossible to access {0} because the template has no {1}")]
     ImpossibleToAccess0BecauseTheTemplateHAsNo1,
 }
-
-
-public class MultiEntityModel : ModelEntity
-{
-    [ImplementedByAll]
-    [NoRepeatValidator]
-    public MList<Lite<Entity>> Entities { get; set; } = new MList<Lite<Entity>>();
-}
-
-public class QueryModel : ModelEntity
-{
-    [InTypeScript(false)]
-    public object QueryName { get; set; }
-
-    [InTypeScript(false)]
-    public List<Filter> Filters { get; set; } = new List<Filter>();
-
-    [InTypeScript(false)]
-    public List<Order> Orders { get; set; } = new List<Order>();
-
-    [InTypeScript(false)]
-    public Pagination Pagination { get; set; }
-}
-
-public enum QueryModelMessage
-{
-    [Description("Configure your query and press [Search] before [Ok]")]
-    ConfigureYourQueryAndPressSearchBeforeOk
-}
