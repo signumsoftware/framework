258f7eee28f2b9b158cefb644ea81be47aabf0a5
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-04-09 00:57:29 +0200

    Move TensorFlow into MachineLearning

---
+++ Extensions/Signum.MachineLearning/TensorFlow/NetworkBuilder.cs
@@ -0,0 +1,119 @@
+using Signum.Entities.MachineLearning;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Tensorflow;
+using Tensorflow.Keras.Optimizers;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+
+namespace Signum.Engine.MachineLearning.TensorFlow;
+
+public static class NetworkBuilder
+{
+    public static Tensor DenseLayer(Tensor input, int outputDim, NeuralNetworkActivation activation, NeuralNetworkInitializer initializer, int seed, string name)
+    {
+        //if (input.shape.Rank != 1)
+        //{
+        //    int newDim = input.Shape.Dimensions.Aggregate((d1, d2) => d1 * d2);
+        //    input = CNTKLib.Reshape(input, new int[] { newDim });
+        //}
+
+        return tf_with(tf.variable_scope(name), delegate
+        {
+            Tensor fullyConnected = FullyConnectedLinearLayer(input, outputDim, initializer, seed);
+            switch (activation)
+            {
+                case NeuralNetworkActivation.None: return fullyConnected;
+                case NeuralNetworkActivation.ReLU: return tf.nn.relu(fullyConnected, "ReLU");
+                case NeuralNetworkActivation.Sigmoid: return tf.nn.sigmoid(fullyConnected, "Sigmoid");
+                case NeuralNetworkActivation.Tanh: return tf.nn.tanh(fullyConnected, "Tanh");
+                default: throw new InvalidOperationException("Unexpected activation " + activation);
+            }
+        });
+    }
+
+    public static Tensor FullyConnectedLinearLayer(Tensor input, int outputDim, NeuralNetworkInitializer initializer, int seed)
+    {
+        int inputDim = (int)input.shape[1];
+        
+        IInitializer? init = GetInitializer(initializer, seed);
+        IVariableV1 W = tf.compat.v1.get_variable("W", new int[] { inputDim, outputDim }, tf.float32, init);
+
+        IVariableV1 b = tf.compat.v1.get_variable("b", new int[] { outputDim }, tf.float32, init);
+        return tf.matmul(input, W.AsTensor()) + b.AsTensor();
+    }
+
+    private static IInitializer GetInitializer(NeuralNetworkInitializer initializer, int seed)
+    {
+        switch (initializer)
+        {
+            case NeuralNetworkInitializer.glorot_uniform_initializer: return tf.glorot_uniform_initializer;
+            case NeuralNetworkInitializer.ones_initializer: return tf.ones_initializer;
+            case NeuralNetworkInitializer.zeros_initializer: return tf.zeros_initializer;
+            case NeuralNetworkInitializer.random_uniform_initializer: return tf.random_uniform_initializer;
+            case NeuralNetworkInitializer.orthogonal_initializer: return tf.orthogonal_initializer;
+            case NeuralNetworkInitializer.random_normal_initializer: return tf.random_normal_initializer(seed: seed);
+            case NeuralNetworkInitializer.truncated_normal_initializer: return tf.truncated_normal_initializer(seed: seed);
+            case NeuralNetworkInitializer.variance_scaling_initializer: return tf.variance_scaling_initializer(seed: seed);
+
+            default:
+                throw new InvalidOperationException("");
+        }
+    }
+
+
+
+    public static Tensor GetEvalFunction(NeuralNetworkEvalFunction lossFunction, Tensor labels, Tensor calculatedOutputs)
+    {
+        switch (lossFunction)
+        {
+            case NeuralNetworkEvalFunction.sigmoid_cross_entropy_with_logits: return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels, calculatedOutputs));
+            case NeuralNetworkEvalFunction.softmax_cross_entropy_with_logits: return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, calculatedOutputs));
+            case NeuralNetworkEvalFunction.softmax_cross_entropy_with_logits_v2: return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels, calculatedOutputs));
+            case NeuralNetworkEvalFunction.ClassificationError: return NetworkBuilder.ClassificationError(calculatedOutputs, labels);
+            case NeuralNetworkEvalFunction.MeanSquaredError: return MeanSquaredError(labels, calculatedOutputs);
+            case NeuralNetworkEvalFunction.MeanAbsoluteError: return NetworkBuilder.MeanAbsoluteError(calculatedOutputs, labels);
+            case NeuralNetworkEvalFunction.MeanAbsolutePercentageError: return NetworkBuilder.MeanAbsolutePercentageError(calculatedOutputs, labels);
+            default:
+                throw new InvalidOperationException("Unexpected " + lossFunction);
+        }
+    }
+
+    private static Tensor ClassificationError(Tensor calculatedOutputs, Tensor labels)
+    {
+        var correct_pred = tf.equal(tf.arg_max(calculatedOutputs, 1), tf.arg_max(labels, 1));
+        return tf.reduce_mean(tf.cast(correct_pred, tf.float32));
+    }
+
+    private static Tensor MeanSquaredError(Tensor labels, Tensor calculatedOutputs)
+    {
+        return tf.reduce_mean(tf.square(calculatedOutputs - labels));
+    }
+
+    public static Tensor MeanAbsoluteError(Tensor labels, Tensor calculatedOutputs)
+    {
+        return tf.reduce_mean(tf.abs(calculatedOutputs - labels));
+    }
+
+    public static Tensor MeanAbsolutePercentageError(Tensor labels, Tensor calculatedOutputs)
+    {
+        return tf.reduce_mean(tf.abs((calculatedOutputs - labels) / labels));
+    }
+
+    internal static Optimizer GetOptimizer(NeuralNetworkSettingsEntity s)
+    {
+        switch (s.Optimizer)
+        {
+            case TensorFlowOptimizer.Adam:
+                return tf.train.AdamOptimizer((float)s.LearningRate, (float)s.LearningEpsilon);
+
+            case TensorFlowOptimizer.GradientDescentOptimizer:
+                return tf.train.GradientDescentOptimizer((float)s.LearningRate);
+
+            default:
+                throw new InvalidOperationException("Unexpected Learner");
+        }
+    }
+}
---
+++ Extensions/Signum.MachineLearning/TensorFlow/TensorFlowEncoding.cs
@@ -0,0 +1,271 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Signum.Entities.DynamicQuery;
+using Signum.Entities.MachineLearning;
+using Signum.Entities.UserAssets;
+using Signum.Utilities;
+using Signum.Utilities.Reflection;
+
+namespace Signum.Engine.MachineLearning.TensorFlow;
+
+public interface ITensorFlowEncoding
+{
+    string? ValidateEncodingProperty(PredictorEntity predictor, PredictorSubQueryEntity? subQuery, PredictorColumnEncodingSymbol encoding, PredictorColumnUsage usage, QueryTokenEmbedded token);
+    List<PredictorCodification> GenerateCodifications(ResultColumn rc, PredictorColumnBase column);
+    object? DecodeValue(PredictorColumnBase column, List<PredictorCodification> codifications, float[] outputValues, PredictionOptions options);
+    void EncodeValue(object? value, PredictorColumnBase column, List<PredictorCodification> codifications, float[] inputValues, int offset); //Span<T>
+}
+
+public class NoneTFEncoding : ITensorFlowEncoding
+{
+    public string? ValidateEncodingProperty(PredictorEntity predictor, PredictorSubQueryEntity? subQuery, PredictorColumnEncodingSymbol encoding, PredictorColumnUsage usage, QueryTokenEmbedded token)
+    {
+        var nn = (NeuralNetworkSettingsEntity)predictor.AlgorithmSettings;
+        if (!ReflectionTools.IsNumber(token.Token.Type) && token.Token.Type.UnNullify() != typeof(bool))
+            return PredictorMessage._0IsRequiredFor1.NiceToString(DefaultColumnEncodings.OneHot.NiceToString(), token.Token.NiceTypeName);
+
+        if (usage == PredictorColumnUsage.Output && (nn.PredictionType == PredictionType.Classification || nn.PredictionType == PredictionType.MultiClassification))
+            return PredictorMessage._0NotSuportedFor1.NiceToString(encoding.NiceToString(), nn.PredictionType.NiceToString());
+
+        return null;
+    }
+
+    public List<PredictorCodification> GenerateCodifications(ResultColumn rc, PredictorColumnBase column)
+    {
+        return new List<PredictorCodification> { new PredictorCodification(column) };
+    }
+
+    public void EncodeValue(object? value, PredictorColumnBase column, List<PredictorCodification> codifications, float[] inputValues, int offset)
+    {
+        var c = codifications.SingleEx();
+        inputValues[offset + c.Index]  = Convert.ToSingle(value);
+    }
+
+    public object? DecodeValue(PredictorColumnBase column, List<PredictorCodification> codifications, float[] outputValues, PredictionOptions options)
+    {
+        var c = codifications.SingleEx();
+        return ReflectionTools.ChangeType(outputValues[c.Index], column.Token.Type);
+    }
+}
+
+public class OneHotTFEncoding : ITensorFlowEncoding
+{
+    public string? ValidateEncodingProperty(PredictorEntity predictor, PredictorSubQueryEntity? subQuery, PredictorColumnEncodingSymbol encoding, PredictorColumnUsage usage, QueryTokenEmbedded token)
+    {
+        var nn = (NeuralNetworkSettingsEntity)predictor.AlgorithmSettings;
+        if (ReflectionTools.IsDecimalNumber(token.Token.Type))
+            return PredictorMessage._0NotSuportedFor1.NiceToString(encoding.NiceToString(), predictor.Algorithm.NiceToString());
+
+        if (usage == PredictorColumnUsage.Output && (nn.PredictionType == PredictionType.Regression || nn.PredictionType == PredictionType.MultiRegression))
+            return PredictorMessage._0NotSuportedFor1.NiceToString(encoding.NiceToString(), nn.PredictionType.NiceToString());
+
+        return null;
+    }
+
+    public List<PredictorCodification> GenerateCodifications(ResultColumn rc, PredictorColumnBase column)
+    {
+        return rc.Values.Cast<object>().NotNull().Distinct().Select(v => new PredictorCodification(column) { IsValue = v }).ToList();
+    }
+
+    public void EncodeValue(object? value, PredictorColumnBase column, List<PredictorCodification> codifications, float[] inputValues, int offset)
+    {
+        var dic = GetCodificationDictionary(column, codifications);
+        if (value != null && dic.TryGetValue(value, out int index))
+            inputValues[offset + index] = 1;
+    }
+
+    public virtual Dictionary<object, int> GetCodificationDictionary(PredictorColumnBase column, List<PredictorCodification> codifications)
+    {
+        if (column.ColumnModel != null)
+            return (Dictionary<object, int>)column.ColumnModel;
+
+        column.ColumnModel = codifications.ToDictionary(a => a.IsValue!, a => a.Index);
+
+        return (Dictionary<object, int>)column.ColumnModel;
+    }
+
+    public object? DecodeValue(PredictorColumnBase column, List<PredictorCodification> codifications, float[] outputValues, PredictionOptions options)
+    {
+        var cods = options?.FilteredCodifications ?? codifications;
+
+        if (options?.AlternativeCount == null)
+        {
+            var max = float.MinValue;
+            PredictorCodification? best = null;
+            foreach (var c in cods)
+            {
+                if (max < outputValues[c.Index])
+                {
+                    best = c;
+                    max = outputValues[c.Index];
+                }
+            }
+            return best?.IsValue;
+        }
+        else
+        {
+            //Softmax
+            var sum = cods.Sum(cod => Math.Exp(outputValues[cod.Index]));
+
+            return cods.OrderByDescending(c => outputValues[c.Index]).Take(options.AlternativeCount.Value).Select(c => new AlternativePrediction(
+                probability: (float)(Math.Exp(outputValues[c.Index]) / sum),
+                value: c.IsValue
+            )).ToList();
+        }
+    }
+}
+
+public abstract class BaseNormalizeTFEncoding : ITensorFlowEncoding
+{
+    public string? ValidateEncodingProperty(PredictorEntity predictor, PredictorSubQueryEntity? subQuery, PredictorColumnEncodingSymbol encoding, PredictorColumnUsage usage, QueryTokenEmbedded token)
+    {
+        if (!ReflectionTools.IsDecimalNumber(token.Token.Type) && !ReflectionTools.IsNumber(token.Token.Type))
+            return PredictorMessage._0NotSuportedFor1.NiceToString(encoding.NiceToString(), predictor.Algorithm.NiceToString());
+
+        return null;
+    }
+
+    public List<PredictorCodification> GenerateCodifications(ResultColumn rc, PredictorColumnBase column)
+    {
+        var values = rc.Values.Cast<object>().NotNull().Select(a => Convert.ToSingle(a)).ToList();
+        return new List<PredictorCodification>
+        {
+            new PredictorCodification(column)
+            {
+                Average = values.Count == 0 ? 0 : values.Average(),
+                StdDev = values.Count == 0 ? 1 : values.StdDev(),
+                Min = values.Count == 0 ? 0 : values.Min(),
+                Max = values.Count == 0 ? 1 : values.Max(),
+            }
+        };
+    }
+
+    public abstract float EncodeSingleValue(object? valueDefault, PredictorCodification c);
+    public void EncodeValue(object? value, PredictorColumnBase column, List<PredictorCodification> codifications, float[] inputValues, int offset)
+    {
+        PredictorCodification c = codifications.SingleEx();
+        inputValues[offset + c.Index] = EncodeSingleValue(value, c);
+    }
+
+    public abstract object? DecodeSingleValue(float value, PredictorCodification c);
+    public object? DecodeValue(PredictorColumnBase column, List<PredictorCodification> codifications, float[] outputValues, PredictionOptions options)
+    {
+        PredictorCodification cod = codifications.SingleEx();
+        float value = outputValues[cod.Index];
+        return DecodeSingleValue(value, cod);
+    }
+}
+
+public class NormalizeZScoreTFEncoding : BaseNormalizeTFEncoding
+{
+    public override float EncodeSingleValue(object? value, PredictorCodification c)
+    {
+        return (Convert.ToSingle(value) - c.Average!.Value) / c.StdDev!.Value;
+    }
+
+    public override object? DecodeSingleValue(float value, PredictorCodification c)
+    {
+        var newValue = c.Average! + (c.StdDev! * value);
+        return ReflectionTools.ChangeType(newValue, c.Column.Token.Type);
+    }
+}
+
+public class NormalizeMinMaxTFEncoding : BaseNormalizeTFEncoding
+{
+    public override float EncodeSingleValue(object? value, PredictorCodification c)
+    {
+        return (Convert.ToSingle(value) - c.Min!.Value) / (c.Max!.Value - c.Min!.Value);
+    }
+
+    public override object? DecodeSingleValue(float value, PredictorCodification c)
+    {
+        var newValue = c.Min!.Value + ((c.Max!.Value - c.Min.Value) * value);
+        return ReflectionTools.ChangeType(newValue, c.Column.Token.Type);
+    }
+}
+
+class NormalizeLogTFEncoding : BaseNormalizeTFEncoding
+{
+    public float MinLog = -5;
+
+    public override float EncodeSingleValue(object? value, PredictorCodification c)
+    {
+        var dValue = Convert.ToDouble(value);
+        return (dValue <= 0 ? MinLog : Math.Max(MinLog, (float)Math.Log(dValue)));
+    }
+
+    public override object? DecodeSingleValue(float value, PredictorCodification c)
+    {
+        var newValue = (float)Math.Exp((double)value);
+        return ReflectionTools.ChangeType(newValue, c.Column.Token.Type);
+    }
+}
+
+public class SplitWordsTFEncoding : ITensorFlowEncoding
+{
+    public string? ValidateEncodingProperty(PredictorEntity predictor, PredictorSubQueryEntity? subQuery, PredictorColumnEncodingSymbol encoding, PredictorColumnUsage usage, QueryTokenEmbedded token)
+    {
+        var nn = (NeuralNetworkSettingsEntity)predictor.AlgorithmSettings;
+        if (token.Token.Type != typeof(string))
+            return PredictorMessage._0NotSuportedFor1.NiceToString(encoding.NiceToString(), predictor.Algorithm.NiceToString());
+
+        if (usage == PredictorColumnUsage.Output)
+            return PredictorMessage._0NotSuportedFor1.NiceToString(encoding.NiceToString(), usage.NiceToString());
+
+        return null;
+    }
+
+    public virtual List<PredictorCodification> GenerateCodifications(ResultColumn rc, PredictorColumnBase column)
+    {
+        var distinctStrings = rc.Values.Cast<string>().NotNull().Distinct();
+
+        var allWords = distinctStrings.SelectMany(str => SplitWords(str)).Distinct(StringComparer.CurrentCultureIgnoreCase).ToList();
+
+        return allWords.Select(v => new PredictorCodification(column) { IsValue = v }).ToList();
+    }
+
+    public char[] separators = new[] { '.', ',', ' ', '-', '(', ')', '/', '\\', ';', ':' };
+
+    public virtual string[] SplitWords(string str)
+    {
+        return str.SplitNoEmpty(separators);
+    }
+
+    public void EncodeValue(object? value, PredictorColumnBase column, List<PredictorCodification> codifications, float[] inputValues, int offset)
+    {
+        var words = SplitWords((string)value! ?? "");
+
+        var dic = GetCodificationDictionary(column, codifications);
+
+        foreach (var w in words)
+        {
+            if (dic.TryGetValue(w, out int index))
+                inputValues[index + offset] = 1;
+        }
+    }
+
+    public int MaxDecodedWords = 5;
+    public float MinDecodedWordValue = 0.1f;
+
+    public object? DecodeValue(PredictorColumnBase column, List<PredictorCodification> codifications, float[] outputValues, PredictionOptions options)
+    {
+        var bestCodifications = codifications
+            .Where(c => outputValues[c.Index] > MinDecodedWordValue)
+            .OrderByDescending(a => outputValues[a.Index])
+            .Take(MaxDecodedWords)
+            .ToList();
+
+        return bestCodifications.ToString(a => a.IsValue!.ToString(), ", ");
+    }
+
+    public virtual Dictionary<string, int> GetCodificationDictionary(PredictorColumnBase column, List<PredictorCodification> codifications)
+    {
+        if (column.ColumnModel != null)
+            return (Dictionary<string, int>)column.ColumnModel;
+
+        column.ColumnModel = codifications.ToDictionaryEx(a => (string)a.IsValue!, a => a.Index, StringComparer.CurrentCultureIgnoreCase);
+
+        return (Dictionary<string, int>)column.ColumnModel;
+    }
+}
