5424b09fd302b08b7aedcc9f6918086a51d5e1d6
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2023-05-25 11:12:14 +0200

    add addTab to ReactVisitor

--- Signum/React/Frames/ReactVisitor.tsx
+++ Signum/React/Frames/ReactVisitor.tsx
@@ -4,7 +4,7 @@ import { ModifiableEntity } from '../Signum.Entities'
 import { TypeContext } from '../TypeContext'
 import { PropertyRoute } from '../Reflection'
 import { ColumnOption, OrderOption, Pagination } from '../Search';
-import { Tab } from 'react-bootstrap'
+import { Tab, Tabs } from 'react-bootstrap'
 
 export class ReactVisitor {
   visitChild(child: React.ReactChild): React.ReactNode {
@@ -210,10 +210,19 @@ export class ViewReplacer<T extends ModifiableEntity> {
     return this;
   }
 
-  removeTab(tabId: string | number): this {
+  removeTab(eventKey: string): this {
     this.result = new ReplaceVisitor(
-      e => e.type == Tab && e.props.eventKey == tabId,
+      e => e.type == Tab && e.props.eventKey == eventKey,
       e => [])
+      .visit(this.result);
+
+    return this;
+  }
+
+  addTab(tabsId: string, ...newTabs: (React.ReactElement<any> | undefined | false | null)[]): this {
+    this.result = new ReplaceVisitor(
+      e => e.type == Tabs && e.props.id == tabsId,
+      e => [React.cloneElement(e, { children: [...React.Children.toArray(e.props.children), ...newTabs] })])
       .visit(this.result);
 
     return this;
