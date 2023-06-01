cdfe50983e1b59e4375f8bc53fcb5c64be6f8989
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2023-06-01 12:47:06 +0200

    Upgrade_20230601_Add_AuthTokenEncryptionKeyConfiguration

---
+++ Signum.Upgrade/Upgrades/Upgrade_20230601_Add_AuthTokenEncryptionKeyConfiguration.cs
@@ -0,0 +1,59 @@
+using LibGit2Sharp;
+using Signum.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20230601_Add_AuthTokenEncryptionKeyConfiguration : CodeUpgradeBase
+{
+    public override string Description => "Add AuthTokenEncryptionKey Configuration";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+        uctx.ChangeCodeFile($"{uctx.ApplicationName}/appsettings.json", file =>
+        {
+            file.InsertAfterFirstLine(l => l.Contains(@"""ServerName"""),
+                $"\"AuthTokenEncryptionKey\": \"<Default Encryption Key for {uctx.ApplicationName} >\","
+            );
+        });
+
+        uctx.ChangeCodeFile($"{uctx.ApplicationName}/Program.cs", program =>
+        {            
+            program.ReplaceLine(l => l.Contains(@"AuthTokenEncryptionKey ="),
+                @"AuthTokenEncryptionKey = app.Configuration.GetValue<string>(""AuthTokenEncryptionKey"")!,"
+            );
+        });
+
+        var ConfigFunctionName = $"Start{uctx.ApplicationName}Configuration";
+        var ConfigFunctionBody = "";
+        uctx.ChangeCodeFile($"{uctx.ApplicationName}/Starter.cs", starter =>
+        {
+            starter.RemoveAllLines(l => l.Contains($"{ConfigFunctionName}(sb);"));
+            
+            starter.InsertBeforeFirstLine(l => l.Contains("RegisterTypeConditions(sb);"), "GlobalsLogic.Start(sb);");
+
+            ConfigFunctionBody = starter.GetMethodBody(l => l.Contains($"{ConfigFunctionName}(SchemaBuilder sb)")).Replace("Configuration =", "\tStarter.Configuration =");
+            starter.ReplaceBetweenIncluded(l => l.Contains($"{ConfigFunctionName}(SchemaBuilder sb)"), 
+                l => l.StartsWith("    }"), "");
+        });
+
+        uctx.CreateCodeFile("Southwind/Globals/GlobalsLogic.cs", $$"""
+            namespace {{uctx.ApplicationName}}.Globals;
+            
+            public static class GlobalsLogic
+            {
+                public static void Start(SchemaBuilder sb)
+                {
+                    if (sb.NotDefined(MethodBase.GetCurrentMethod()))
+                    {
+                {{ConfigFunctionBody}}
+                    }
+                }
+            }            
+
+            """);
+    }
+}
