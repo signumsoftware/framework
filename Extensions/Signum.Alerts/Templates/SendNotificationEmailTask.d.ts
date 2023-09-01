3a0df0e443b65dade2537166d5b7ce9d4d06c959
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-09-02 00:26:32 +0200

    remove d.ts in Signum.Alerts

--- Extensions/Signum.Alerts/Templates/SendNotificationEmailTask.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { TypeContext } from '@framework/TypeContext';
-import { SendNotificationEmailTaskEntity } from '../Signum.Alerts';
-export default function SendNotificationEmailTask(p: {
-    ctx: TypeContext<SendNotificationEmailTaskEntity>;
-}): JSX.Element;
