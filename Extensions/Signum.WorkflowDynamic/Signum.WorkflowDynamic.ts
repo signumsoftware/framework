55129a54f1858a6a2c2e4b41b0afda6d7bcc89bd
BECHTLE\thomas.muenzer <thomas.muenzer@bechtle.com>
2023-10-09 16:54:13 +0200

    make Workflow independent of Synamic by creating  Signum.WorkflowDynamic

---
+++ Extensions/Signum.WorkflowDynamic/Signum.WorkflowDynamic.ts
@@ -0,0 +1,14 @@
+//////////////////////////////////
+//Auto-generated. Do NOT modify!//
+//////////////////////////////////
+
+import { MessageKey, QueryKey, Type, EnumType, registerSymbol } from '../../Signum/React/Reflection'
+import * as Entities from '../../Signum/React/Signum.Entities'
+import * as Operations from '../../Signum/React/Signum.Operations'
+import * as Types from '../Signum.Dynamic/Signum.Dynamic.Types'
+
+
+export module DynamicTypeWorkflowOperation {
+  export const FixCaseDescriptions : Operations.ExecuteSymbol<Types.DynamicTypeEntity> = registerSymbol("Operation", "DynamicTypeWorkflowOperation.FixCaseDescriptions");
+}
+
