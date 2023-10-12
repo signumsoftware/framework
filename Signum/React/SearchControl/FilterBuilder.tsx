79a56a487b2d04e5c61639db165d6a8afe15661e
Olmo del Corral Cano <olmo@signumsoftware.com>
2023-10-12 19:34:35 +0200

    fix FilterBuilder

--- Signum/React/SearchControl/FilterBuilder.tsx
+++ Signum/React/SearchControl/FilterBuilder.tsx
@@ -310,7 +310,7 @@ export function FilterGroupComponent(p: FilterGroupComponentsProps) {
   return (
     <>
       <tr className="sf-filter-group" style={{ backgroundColor: "#eee" }}>
-        <td style={{ paddingLeft: paddingLeft }} colSpan={2}>
+        <td style={{ paddingLeft: paddingLeft }} colSpan={3}>
           <div className="d-flex">
             {!readOnly &&
               <a href="#"
@@ -327,7 +327,7 @@ export function FilterGroupComponent(p: FilterGroupComponentsProps) {
                 <FontAwesomeIcon icon={fg.expanded ? ["far", "square-minus"] : ["far", "square-plus"]} className="me-2"
                   title={(fg.expanded ? EntityControlMessage.Collapse : EntityControlMessage.Expand).niceToString()} />
               </a>
-              <select className="form-select form-select-xs sf-group-selector fw-bold me-2" value={fg.groupOperation as any} disabled={readOnly} onChange={handleChangeOperation}>
+              <select className="form-select form-select-xs sf-group-selector fw-bold me-2 w-auto" value={fg.groupOperation as any} disabled={readOnly} onChange={handleChangeOperation}>
                 {FilterGroupOperation.values().map((ft, i) => <option key={i} value={ft as any}>{ft == "Or" ? SearchMessage.OrGroup.niceToString() : SearchMessage.AndGroup.niceToString()}</option>)}
               </select>
               <small style={{ whiteSpace: "nowrap" }}>
