2eb7e84eb7147b8bfd359dd494ca60f2f7967c7d
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2023-10-13 14:54:17 +0200

    Upgrade_20231013_AutoLine

--- Signum.Upgrade/Upgrades/Upgrade_20231013_AutoLine.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20231013_AutoLine.cs
@@ -14,6 +14,19 @@ class Upgrade_20231013_AutoLine : CodeUpgradeBase
             file.Replace(new Regex(@"\bValueLine\b"), m => "AutoLine");
             file.Replace(new Regex(@"<ValueLine"), m => "<AutoLine");
             file.Replace(new Regex(@"ValueLineModal"), m => "AutoLineModal");
+            file.Replace(new Regex(@"<AutoLine(?<atts>[^/]*)/>"), m =>
+            {
+                var atts = m.Groups["atts"].Value;
+
+                if (atts.Contains("inlineCheckbox"))
+                    return "<CheckboxLine" + atts + "/>";
+
+                if (atts.Contains("optionItems"))
+                    return "<EnumLine" + atts + "/>";
+
+                return m.Value;
+
+            });
         });
     }
 }
