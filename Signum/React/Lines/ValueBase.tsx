e5ec0a4f940282c746efc67ac346d4401b5c7cd5
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2023-10-16 12:12:10 +0200

    validate TypeLine

--- Signum/React/Lines/ValueBase.tsx
+++ Signum/React/Lines/ValueBase.tsx
@@ -30,7 +30,7 @@ export class ValueBaseController<T extends ValueBaseProps<any>> extends LineBase
 
   assertType(tagName: string, types: string[]) {
     if (!types.contains(this.props.type!.name))
-      throw new Error(`Invalid type ${this.props.type?.name} in ${tagName} für ${this.props.ctx.propertyPath ?? "?"}")
+      throw new Error(`Invalid type ${this.props.type?.name} in ${tagName} for ${this.props.ctx.propertyPath ?? "?"}`)
   }
 
   overrideProps(state: T, overridenProps: T) {
