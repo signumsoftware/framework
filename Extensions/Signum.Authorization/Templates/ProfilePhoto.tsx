c07b4306c9708a5ec9418b2cc955f4d7b8fbf39e
Reza Nosratabad <reza.nosratabad@signumsoftware.com>
2023-10-16 20:03:04 +0330

    fix ProfilePhoto cache

--- Extensions/Signum.Authorization/Templates/ProfilePhoto.tsx
+++ Extensions/Signum.Authorization/Templates/ProfilePhoto.tsx
@@ -2,21 +2,21 @@ import React, { useEffect, useState } from "react";
 import { UserEntity } from "../Signum.Authorization";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './ProfilePhoto.css'
-import { getToString, Lite, toLite } from "@framework/Signum.Entities";
+import { getToString, Lite, liteKey, toLite } from "@framework/Signum.Entities";
 import UserCircle from "./UserCircle";
 import * as UserCircles from "./UserCircle";
-import { classes } from "@framework/Globals";
+import { Dic, classes } from "@framework/Globals";
 
 export var urlProviders: ((u: UserEntity | Lite<UserEntity>, size: number) => Promise<string | null>)[] = [];
 
 export function clearCache() {
-  urlCache?.clear();
-  urlCache = undefined;
+  Dic.clear(urlCache);
+  urlCache = {};
 }
 
 export default function ProfilePhoto(p: { user: UserEntity, size: number }) {
   const [imageError, setImageError] = useState(false);
-  const url = useFirstCachedUrl(p.user, p.size!);
+  const url = useCachedUrl(p.user, p.size!);
 
   useEffect(() => {
     setImageError(false);
@@ -36,7 +36,8 @@ export default function ProfilePhoto(p: { user: UserEntity, size: number }) {
 export function SmallProfilePhoto(p: { user: Lite<UserEntity>, size?: number, className?: string, fallback?: React.ReactNode }) {
   const [imageError, setImageError] = useState(false);
   const size = p.size ?? 22;
-  const url = useFirstCachedUrl(p.user, size!);
+
+  const url = useCachedUrl(p.user, size!);
 
   useEffect(() => {
     setImageError(false);
@@ -44,24 +45,27 @@ export function SmallProfilePhoto(p: { user: Lite<UserEntity>, size?: number, cl
 
   return (
     <div className={classes("small-user-profile-photo", p.className)}>
-      {url && !imageError ? <img src={url} style={{ maxWidth: `${size}px`, maxHeight: `${size}px` }} onError={() => setImageError(true)} title={getToString(p.user)} /> :
+      {url && !imageError ? <img src={url} style={{ maxWidth: `${size}px`, maxHeight: `${size}px` }} onError={(e) => setImageError(true)} title={getToString(p.user)} /> :
         p.fallback ?? <UserCircle user = {p.user } />}
     </div>
   );
 }
 
-var urlCache: Promise<string | null>[] | undefined;
+var urlCache: { [userKey: string]: Promise<string | null> | null } = { };
 
-function useFirstCachedUrl(user: UserEntity | Lite<UserEntity>, size: number) {
+function useCachedUrl(user: UserEntity | Lite<UserEntity>, size: number) {
   const [url, setUrl] = useState<string | null>(null);
 
   useEffect(() => {
 
-    const firstPromise = (urlCache ??= urlProviders.map(f => f(user, size))).firstOrNull();
+    const userLite = UserEntity.isLite(user) ? user : toLite(user);
+    const userLiteKey = liteKey(userLite);
 
-    firstPromise ? firstPromise.then(u => setUrl(u)) : setUrl(null);
+    const promise = (urlCache[userLiteKey] ??= urlProviders.map(f => f(user, size)).notNull().firstOrNull());
 
-  }, [Boolean(urlCache)]);
+    promise ? promise.then(u => setUrl(u)) : setUrl(null);
+
+  }, [user.id]);
 
   return url
 }
