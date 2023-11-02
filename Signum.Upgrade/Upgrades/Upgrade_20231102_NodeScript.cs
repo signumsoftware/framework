525ab46ab46b47728119c1a2094d1dcfb3a6e31f
Daniel Constantino <daniel.constantino@bechtle.com>
2023-11-02 09:47:40 +0100

    update Upgrade_20231102_NodeScript

---
+++ Signum.Upgrade/Upgrades/Upgrade_20231102_NodeScript.cs
@@ -0,0 +1,25 @@
+using Signum.Utilities;
+using System.Collections.Generic;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20231102_NodeScript : CodeUpgradeBase
+{
+    public override string Description => "Update Node Script in Dockerfile";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+        uctx.ChangeCodeFile(@"Southwind/Dockerfile", file =>
+        {
+            file.ReplaceLine(l => l.Contains("curl -sL https://deb.nodesource.com/setup_1"), """
+                RUN curl -SLO https://deb.nodesource.com/nsolid_setup_deb.sh
+                RUN chmod 500 nsolid_setup_deb.sh
+                RUN./ nsolid_setup_deb.sh 21
+                """
+                );
+        });
+    }
+}
+
+
+
