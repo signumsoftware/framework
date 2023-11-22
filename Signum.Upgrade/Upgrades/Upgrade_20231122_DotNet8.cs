c486de7023be059246d4b61d2fa2c4e4d2ded075
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2023-11-22 07:59:08 +0100

    Migrate to .net 8.0

---
+++ Signum.Upgrade/Upgrades/Upgrade_20231122_DotNet8.cs
@@ -0,0 +1,33 @@
+using Signum.Utilities;
+using System.Collections.Generic;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20231122_DotNet8 : CodeUpgradeBase
+{
+    public override string Description => "Migrate to .Net 8";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+        uctx.ForeachCodeFile(@"*.csproj", file =>
+        {
+            file.Replace("<TargetFramework>net7.0</TargetFramework>", "<TargetFramework>net8.0</TargetFramework>");
+        });
+
+        uctx.ForeachCodeFile(@"deploy*.ps1", file =>
+        {
+            file.Replace("net7.0", "net8.0");
+        });
+
+        uctx.ChangeCodeFile(@"Southwind\Dockerfile", file =>
+        {
+            file.Replace("dotnet/aspnet:7.", "dotnet/aspnet:8.");
+            file.Replace("dotnet/sdk:7.", "dotnet/sdk:8.");
+            file.WarningLevel = WarningLevel.None;
+            file.Replace("dotnet/aspnet:7.0", "dotnet/aspnet:8.0.0");
+        });
+    }
+}
+
+
+
