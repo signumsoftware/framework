7d34e0fc4617f0502d21d53cce89f52a5f57fc15
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2023-12-18 11:58:39 +0100

    update Changelog

--- Signum/Changelog.ts
+++ Signum/Changelog.ts
@@ -1,6 +1,7 @@
 import type { ChangeLogDic } from "./React/Basics/ChangeLogClient";
 
 export default {
+  "2023-12-18": "fix SearchValue onClick",
   "2023-11-27": "Add TranslateToken to use instance translations in search control / charting",
   "2023-11-24": "fix change log viewer",
   "2023-11-22": "Migrate to .Net 8",
