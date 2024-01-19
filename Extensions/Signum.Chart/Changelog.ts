b619ea14988b6a70305c3d277c15655fab7432cb
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2024-01-19 13:11:41 +0100

    add Horizontal Margin parameter for Multi Columns Chart

--- Extensions/Signum.Chart/Changelog.ts
+++ Extensions/Signum.Chart/Changelog.ts
@@ -1,7 +1,7 @@
 import type { ChangeLogDic } from "@framework/Basics/ChangeLogClient";
 
 export default {
-  //"2023-11-13": ["sample change log",],
+  "2024-01-19": ["add Horizontal Margin parameter for Multi Columns Chart"],
 
 } satisfies ChangeLogDic;
 
