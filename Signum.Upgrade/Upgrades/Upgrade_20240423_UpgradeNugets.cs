c6cfcc02b8362b4482764b9b213ce508e6288dd5
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2024-04-24 08:42:21 +0200

    fix Upgrade_20240423_UpgradeNugets

--- Signum.Upgrade/Upgrades/Upgrade_20240423_UpgradeNugets.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20240423_UpgradeNugets.cs
@@ -15,8 +15,12 @@ class Upgrade_20240424_UpgradeNugets : CodeUpgradeBase
         {
             file.UpdateNugetReferences("""
                 <PackageReference Include="Selenium.WebDriver" Version="4.19.0" />
+                <PackageReference Include="Selenium.WebDriver.ChromeDriver" Version="124.0.6367.6000" />
                 <PackageReference Include="Microsoft.TypeScript.MSBuild" Version="5.4.4"/>
                 <PackageReference Include="SixLabors.ImageSharp" Version="2.1.8" />
+                <PackageReference Include="SkiaSharp.NativeAssets.Linux" Version="2.88.8" />
+                <PackageReference Include="xunit" Version="2.7.1" />
+                <PackageReference Include="xunit.runner.visualstudio" Version="2.5.8">
                 """);
         });
     }
