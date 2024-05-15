48c0715299b0053da15ce4830abf5395685df777
Daniel Constantino <daniel.constantino@bechtle.com>
2024-05-15 15:18:15 +0200

    remove debugger in IconTypeahead

--- Signum/React/Components/IconTypeahead.tsx
+++ Signum/React/Components/IconTypeahead.tsx
@@ -242,13 +242,11 @@ export function isIconDefined(icon: IconProp) {
   if (typeof icon == "object")
     return lib.definitions[toSortPrefix(icon.prefix) ?? icon.prefix]?.[icon.iconName];
 
-  debugger;
   if (typeof icon == "string") {
 
     if (lib.definitions["fas"]?.[icon])
       return true;
     else {
-      debugger;
       return false;
     }
   }
