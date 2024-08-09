a9834ce48474ff8a40dd51f9357e80d4ec19f3f4
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2024-08-09 08:44:40 +0200

    HealthCheckPart formatting

--- Extensions/Signum.Dashboard/View/HealthCheckPart.tsx
+++ Extensions/Signum.Dashboard/View/HealthCheckPart.tsx
@@ -7,10 +7,11 @@ import { useAPI } from '../../../Signum/React/Hooks';
 import { ajaxGet } from '../../../Signum/React/Services';
 import * as AppContext from '@framework/AppContext'
 import { softCast } from '../../../Signum/React/Globals';
+import { Color } from '@framework/Basics/Color'
 
 export default function HealthCheckPart(p: PanelPartContentProps<HealthCheckPartEntity>): React.JSX.Element {
   return (
-    <div className="d-flex mt-5">
+    <div className="d-flex flex-wrap mt-5">
       {
         p.content.items.map(mle => mle.element).map((le, i) => <HealthCheckElement key={i} element={le} />)
       }
@@ -31,13 +32,16 @@ function HealthCheckElement(p: { element: HealthCheckElementEmbedded }) {
   }).catch((e) => {
     return softCast<StatusInfo>({ status: 'Error', message: e.message });
   }), [p.element.checkURL]);
-
-  return (<div className='d-flex justify-content-center align-items-center mx-2 my-2'
+  var bgc = statusInfo === undefined ? '#00000038' : statusInfo.status == HealthCheckStatus.value('Ok') ? '#39b54a' : '#ec4205';
+  return (<div className='d-flex justify-content-center align-items-center mx-2 my-2 rounded'
     title={statusInfo?.message}
     style={{
       cursor: p.element.navigateURL ? 'pointer' : undefined,
-      minWidth: 240, minHeight: 80,
-      backgroundColor: statusInfo === undefined ? 'lightgray' : statusInfo.status == HealthCheckStatus.value('Ok') ? 'lightgreen' : 'red', borderRadius: 5
+      minWidth: 240,
+      minHeight: 80,
+      backgroundColor: bgc,
+      color: Color.parse(bgc).opositePole().toString(),
+      boxShadow: "3px 3px 12px 3px #00000038"
     }} onClick={e => {
       var path = AppContext.toAbsoluteUrl(p.element.navigateURL);
       window.open(path);
