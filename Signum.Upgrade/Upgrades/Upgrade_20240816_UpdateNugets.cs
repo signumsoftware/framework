3eacf517ad5cec17fb042f3283bd039d5b76f848
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2024-08-16 13:07:56 +0200

    fix Upgrade_20240816_UpdateNugets

--- Signum.Upgrade/Upgrades/Upgrade_20240816_UpdateNugets.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20240816_UpdateNugets.cs
@@ -19,6 +19,7 @@ class Upgrade_20240816_UpdateNugets : CodeUpgradeBase
                 <PackageReference Include="HtmlAgilityPack" Version="1.11.63" />
                 <PackageReference Include="Azure.Storage.Blobs" Version="12.21.2" />
                 <PackageReference Include="Microsoft.CodeAnalysis.CSharp" Version="4.11.0" />
+                <PackageReference Include="Selenium.WebDriver.ChromeDriver" Version="127.0.6533.11900" />
                 """);
         });
     }
