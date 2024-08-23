610c931178d8ebebc82672c385d229ef41a058d2
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2024-08-23 11:12:46 +0200

    fix Upgrade_20240823_UpdateNugets

--- Signum.Upgrade/Upgrades/Upgrade_20240823_UpdateNugets.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20240823_UpdateNugets.cs
@@ -17,6 +17,8 @@ class Upgrade_20240823_UpdateNugets : CodeUpgradeBase
             file.UpdateNugetReferences("""
                 <PackageReference Include="HtmlAgilityPack" Version="1.11.64" />
                 <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.11.0" />
+                <PackageReference Include="Selenium.WebDriver.ChromeDriver" Version="128.0.6613.8400" />
+                <PackageReference Include="Swashbuckle.AspNetCore" Version="6.7.1" />
                 """);
         });
     }
