b13550314182e91ef01f03287541dad3f2694d75
Fabian Seiffert <GOSCHEN@users.noreply.github.com>
2024-11-18 17:27:01 +0100

    current work

--- Extensions/Signum.Files/Components/FileLine.tsx
+++ Extensions/Signum.Files/Components/FileLine.tsx
@@ -10,51 +10,67 @@ import { FileDownloader, FileDownloaderConfiguration, DownloadBehaviour } from '
 import { FileUploader }  from './FileUploader'
 
 import "./Files.css"
-import { genericForwardRef, genericForwardRefWithMemo, useController } from '@framework/Lines/LineBase'
+import { genericForwardRef, useController } from '@framework/Lines/LineBase'
 
 export { FileTypeSymbol };
 
-export interface FileLineProps<V extends ModifiableEntity & IFile | Lite<IFile & Entity> | null> extends EntityBaseProps<V> {
+export interface FileLineProps<V extends ModifiableEntity /*& IFile*/ | Lite</*IFile &*/ Entity>> extends EntityBaseProps<V> {
   download?: DownloadBehaviour;
   showFileIcon?: boolean;
   dragAndDrop?: boolean;
   dragAndDropMessage?: string;
   fileType?: FileTypeSymbol;
   accept?: string;
-  configuration?: FileDownloaderConfiguration<AsEntity<V>>;
+  //configuration?: FileDownloaderConfiguration<AsEntity<V>>;
+  configuration?: FileDownloaderConfiguration<IFile>;
   maxSizeInBytes?: number;
+  getFile?: (ectx: NoInfer<V>) => ModifiableEntity & IFile | Lite<IFile & Entity>;
+  createFromFile?: (file: ModifiableEntity & IFile) => Promise<NoInfer<V> | undefined>;
 }
 
 
-export class FileLineController<V extends ModifiableEntity & IFile | Lite<IFile & Entity> | null> extends EntityBaseController<FileLineProps<V>, V>{
+export class FileLineController<V extends ModifiableEntity /*& IFile */| Lite</*IFile & */Entity>> extends EntityBaseController<FileLineProps<V>, V>{
 
-  getDefaultProps(state: FileLineProps<V>): void {
+  overrideProps(p: FileLineProps<V>, overridenProps: FileLineProps<V>): void {
+  //getDefaultProps(state: FileLineProps<V>): void {
 
-    super.getDefaultProps(state);
+    //super.getDefaultProps(state);
+    p.view = EntityBaseController.defaultIsViewable(p.type!, false) && overridenProps.getFile != null;
+    super.overrideProps(p, overridenProps);
 
-    const m = state.ctx.propertyRoute?.member;
+    let pr = p.ctx.propertyRoute;
+    //if (pr && p.getFile)
+    //  pr = pr.addMember("Indexer", "", true).addLambda(p.getFile);
+
+    const m = pr?.member;
     if (m?.defaultFileTypeInfo) {
 
-      if (state.fileType == null)
-        state.fileType = getSymbol(FileTypeSymbol, m.defaultFileTypeInfo.key)
+      if (p.fileType == null)
+        p.fileType = getSymbol(FileTypeSymbol, m.defaultFileTypeInfo.key)
 
 
-      if (state.accept == null && m.defaultFileTypeInfo.onlyImages)
-        state.accept = "image/*";
+      if (p.accept == null && m.defaultFileTypeInfo.onlyImages)
+        p.accept = "image/*";
 
-      if (state.maxSizeInBytes == null && m.defaultFileTypeInfo.maxSizeInBytes)
-        state.maxSizeInBytes = m.defaultFileTypeInfo.maxSizeInBytes;
+      if (p.maxSizeInBytes == null && m.defaultFileTypeInfo.maxSizeInBytes)
+        p.maxSizeInBytes = m.defaultFileTypeInfo.maxSizeInBytes;
     }
   }
 
   handleFileLoaded = (file: IFile & ModifiableEntity): void => {
 
-    this.convert(file as Aprox<V>)
+    if (this.props.createFromFile)
+      this.props.createFromFile(file)
+        .then(em => em && this.setValue(em));
+    else
+      this.convert(file as unknown as Aprox<V>)
       .then(f => this.setValue(f));
   }
 }
 
-export const FileLine: <V extends (ModifiableEntity & IFile) | Lite<IFile & Entity> | null>(props: FileLineProps<V> & React.RefAttributes<FileLineController<V>>) => React.ReactNode | null = genericForwardRefWithMemo(function FileLine<V extends ModifiableEntity & IFile | Lite<IFile & Entity> | null>(props: FileLineProps<V>, ref: React.Ref<FileLineController<V>>) {
+export const FileLine: <V extends (ModifiableEntity/* & IFile*/) | Lite</*IFile & */Entity>>(props: FileLineProps<V> & React.RefAttributes<FileLineController<V>>) => React.ReactNode | null = 
+  genericForwardRef(function FileLine<V extends ModifiableEntity/* & IFile */| Lite</*IFile & */Entity>>(props: FileLineProps<V>, ref: React.Ref<FileLineController<V>>) {
+
   const c = useController(FileLineController, props, ref);
   const p = c.props;
 
@@ -80,12 +96,13 @@ export const FileLine: <V extends (ModifiableEntity & IFile) | Lite<IFile & Enti
     const val = ctx.value!;
 
     const content = p.download == "None" ?
-      <span className={classes(ctx.formControlClass, "file-control")} > {getToString(val)}</span > :
+      <span className={classes(ctx.formControlClass, "file-control")}> {/*getToString(p.getFile ? p.getFile(val) : val)*/"Test"}</span > :
       <FileDownloader
-        configuration={p.configuration as FileDownloaderConfiguration<IFile>}
+        configuration={p.configuration /*as FileDownloaderConfiguration<IFile>*/}
         download={p.download}
         showFileIcon={p.showFileIcon}
-        entityOrLite={val}
+        containerEntity={p.getFile ? val as ModifiableEntity : undefined}
+        entityOrLite={p.getFile ? p.getFile(val) : val as ModifiableEntity & IFile | Lite<IFile & Entity>}
         htmlAttributes={{ className: classes(ctx.formControlClass, "file-control") }} />;
 
     const buttons =
@@ -131,7 +148,7 @@ export const FileLine: <V extends (ModifiableEntity & IFile) | Lite<IFile & Enti
       </div>
     );
   }
-}, (prev, next) => FileLineController.propEquals(prev, next));
+});
 
 (FileLine as any).defaultProps = {
   download: "ViewOrSave",
