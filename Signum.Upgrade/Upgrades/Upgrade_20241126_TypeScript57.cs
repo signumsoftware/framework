f7c30a6af70ece8c671f72789bee47f1dfd43e94
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2024-11-26 18:14:28 +0100

    add Upgrade_20241126_TypeScript57

---
+++ Signum.Upgrade/Upgrades/Upgrade_20241126_TypeScript57.cs
@@ -0,0 +1,31 @@
+using Signum.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20241126_TypeScript57 : CodeUpgradeBase
+{
+    public override string Description => "Upgrade to TypeScript 5.7";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+
+        uctx.ChangeCodeFile(@"*.csproj", file =>
+        {
+           
+            file.UpdateNugetReferences("""
+                <PackageReference Include="Microsoft.TypeScript.MSBuild" Version="5.7.1">
+                <PackageReference Include="DocumentFormat.OpenXml" Version="3.2.0" />
+                <PackageReference Include="Selenium.Support" Version="4.27.0" />
+                <PackageReference Include="Selenium.WebDriver" Version="4.27.0" />
+                """);
+                //<PackageReference Include="Signum.TSGenerator" Version="9.0.0" />
+        });
+    }
+}
+
+
+
