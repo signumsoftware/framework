81603a605dab1b92d9a1672c532eade9cf044640
alejandro <alejandro.cano@signumsoftware.com>
2025-01-10 09:53:31 +0100

    CultureScope

--- Signum/Basics/CultureInfoLogic.cs
+++ Signum/Basics/CultureInfoLogic.cs
@@ -4,6 +4,10 @@ using Signum.Engine.Maps;
 using Signum.Engine.Sync;
 using Signum.API;
 using System.Collections.Frozen;
+using System;
+using System.Globalization;
+using System.Threading;
+
 
 namespace Signum.Basics;
 
@@ -94,5 +98,28 @@ public static class CultureInfoLogic
     public static CultureInfoEntity GetCultureInfoEntity(string cultureName)
     {
         return CultureInfoToEntity.Value.GetOrThrow(cultureName);
+    }
+}
+
+
+
+
+public class CultureScope : IDisposable
+{
+    private readonly CultureInfo _originalCulture;
+    private readonly CultureInfo _originalUICulture;
+
+    public CultureScope(CultureInfo newCulture)
+    {
+        _originalCulture = Thread.CurrentThread.CurrentCulture;
+        _originalUICulture = Thread.CurrentThread.CurrentUICulture;
+        Thread.CurrentThread.CurrentCulture = newCulture;
+        Thread.CurrentThread.CurrentUICulture = newCulture;
+    }
+
+    public void Dispose()
+    {
+        Thread.CurrentThread.CurrentCulture = _originalCulture;
+        Thread.CurrentThread.CurrentUICulture = _originalUICulture;
     }
 }
