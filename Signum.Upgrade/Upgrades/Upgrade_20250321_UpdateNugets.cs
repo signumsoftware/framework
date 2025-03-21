7d2fcc8d5c666b194432f28f367dca881614c049
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2025-03-21 12:22:46 +0100

    add Upgrade_20250321_UpdateNugets

--- Signum.Upgrade/Upgrades/Upgrade_20250321_UpdateNugets.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20250321_UpdateNugets.cs
@@ -20,6 +20,7 @@ class Upgrade_20250321_UpdateNugets : CodeUpgradeBase
                 <PackageReference Include="HtmlAgilityPack" Version="1.12.0" />
                 <PackageReference Include="Microsoft.CodeAnalysis.CSharp" Version="4.13.0" />
                 <PackageReference Include="Microsoft.Extensions.Configuration" Version="9.0.3" />
+                <PackageReference Include="Microsoft.Extensions.Configuration.Binder" Version="9.0.3" />
                 <PackageReference Include="Microsoft.Extensions.Configuration.Json" Version="9.0.3" />
                 <PackageReference Include="Microsoft.Extensions.Configuration.UserSecrets" Version="9.0.3" />
                 <PackageReference Include="Microsoft.Graph" Version="5.74.0" />
@@ -34,6 +35,12 @@ class Upgrade_20250321_UpdateNugets : CodeUpgradeBase
                 <PackageReference Include="System.Drawing.Common" Version="9.0.3" />
                 <PackageReference Include="System.Text.Encoding.CodePages" Version="9.0.3" />
                 <PackageReference Include="xunit.runner.visualstudio" Version="3.0.2">
+                <PackageReference Include="OpenTelemetry" Version="1.11.2" />
+                <PackageReference Include="OpenTelemetry.Exporter.OpenTelemetryProtocol" Version="1.11.2" />
+                <PackageReference Include="OpenTelemetry.Exporter.Console" Version="1.11.2" />
+                <PackageReference Include="OpenTelemetry.Extensions.Hosting" Version="1.11.2" />
+                <PackageReference Include="OpenTelemetry.Instrumentation.AspNetCore" Version="1.11.1" />
+                <PackageReference Include="OpenTelemetry.Instrumentation.Http" Version="1.11.1" />
                 """);
 
             file.ReplaceLine(a => a.Contains(@"Include=""xunit"""), @"<PackageReference Include=""xunit.v3"" Version=""2.0.0"" />");
