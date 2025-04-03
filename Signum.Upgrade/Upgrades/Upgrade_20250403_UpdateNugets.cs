aaa8caaf44b3e7109f8d081fb215ee03d0e0e958
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2025-04-03 11:09:05 +0200

    add Upgrade_20250403_UpdateNugets

---
+++ Signum.Upgrade/Upgrades/Upgrade_20250403_UpdateNugets.cs
@@ -0,0 +1,28 @@
+using Signum.Utilities;
+using System.Collections.Generic;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20250403_UpdateNugets : CodeUpgradeBase
+{
+    public override string Description => "";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+
+        uctx.ForeachCodeFile(@"*.csproj", file =>
+        {
+            file.UpdateNugetReferences("""
+                <PackageReference Include="Microsoft.Graph" Version="5.75.0" />
+                <PackageReference Include="Selenium.Support" Version="4.30.0" />
+                <PackageReference Include="Selenium.WebDriver" Version="4.30.0" />
+                """);
+        });
+
+    }
+
+ 
+}
+
+
+
