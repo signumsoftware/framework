7a6fece3835f51768fbe321bc29fe34f2fbdb77c
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2025-06-04 11:28:41 +0200

    add Upgrade_20250604_UpdateNuges2

---
+++ Signum.Upgrade/Upgrades/Upgrade_20250604_UpdateNuges2.cs
@@ -0,0 +1,27 @@
+using Signum.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Signum.Upgrade.Upgrades;
+
+class Upgrade_20250604_UpdateNuges2 : CodeUpgradeBase
+{
+    public override string Description => "Update nugets";
+
+    public override void Execute(UpgradeContext uctx)
+    {
+
+        uctx.ForeachCodeFile(@"*.csproj", file =>
+        {
+            file.UpdateNugetReferences("""
+                <PackageReference Include="Microsoft.Graph" Version="5.80.0" />
+                <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.14.1" />
+                <PackageReference Include="Microsoft.Graph" Version="5.80.0" />
+                """);
+
+        });
+    }
+}
+
+
