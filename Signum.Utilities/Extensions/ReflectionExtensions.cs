657a87a00019b3dab0478e010f980de7b3fa8df3
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2025-07-10 12:02:40 +0200

    fix warning

--- Signum.Utilities/Extensions/ReflectionExtensions.cs
+++ Signum.Utilities/Extensions/ReflectionExtensions.cs
@@ -143,7 +143,7 @@ public static class ReflectionExtensions
             try
             {
                 s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
-                s.Read(b, 0, 2048);
+                s.ReadExactly(b, 0, 2048);
             }
             finally
             {
