dd818768841e281e4af41af689eb48495ffdf9e3
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2025-07-15 15:29:54 +0200

    fix SelectTree ToObservableCollection

--- Signum.Utilities/Extensions/EnumerableExtensions.cs
+++ Signum.Utilities/Extensions/EnumerableExtensions.cs
@@ -1045,10 +1045,9 @@ public static class EnumerableExtensions
         }
     }
 
-    public static ObservableCollection<T>? ToObservableCollection<T>(this IEnumerable<T>? collection)
+    public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> collection)
     {
-        return collection == null ? null :
-          collection as ObservableCollection<T> ?? new ObservableCollection<T>(collection);
+        return collection as ObservableCollection<T> ?? new ObservableCollection<T>(collection);
     }
 
     public static IEnumerable<T> AsThreadSafe<T>(this IEnumerable<T> source)
