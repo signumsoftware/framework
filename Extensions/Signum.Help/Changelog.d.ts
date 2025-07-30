dd7627cf455add9eb5378ee824638792a3715bd3
Robin Kirch <robin.kirch@bechtle.com>
2025-07-30 11:03:39 +0200

    fix for Help

--- Extensions/Signum.Help/Changelog.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ChangeLogDic } from "@framework/Basics/ChangeLogClient";
-declare const _default: ChangeLogDic;
-export default _default;
--- Extensions/Signum.Help/Pages/TypeHelpPage.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-export default function TypeHelpPage(): React.JSX.Element;
-export declare function Shortcut(p: {
-    text: string;
-}): React.JSX.Element;
