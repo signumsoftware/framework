a31d7f8b98547037809351d7b93903df59df3d82
Antonio Herranz <antonio.herranz@signumsoftware.com>
2025-10-06 11:33:56 +0200

    fix ToString and no args

--- Signum.Utilities/Profiler/HeavyProfiler.cs
+++ Signum.Utilities/Profiler/HeavyProfiler.cs
@@ -636,7 +636,7 @@ public readonly struct StructuredLogMessage
     public override string ToString()
     {
         var args = this.Arguments;
-        if (args == null)
+        if (args.IsNullOrEmpty())
             return this.Message;
 
         int i = 0;
