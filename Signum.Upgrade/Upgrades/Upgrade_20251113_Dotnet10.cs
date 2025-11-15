3458a70e1c93d53cc0a1b3e30a50d4fe2ad3b223
Olmo del Corral <olmo@signumsoftware.com>
2025-11-15 15:35:00 +0100

    finish .Net 10

--- Signum.Upgrade/Upgrades/Upgrade_20251113_Dotnet10.cs
+++ Signum.Upgrade/Upgrades/Upgrade_20251113_Dotnet10.cs
@@ -14,26 +14,29 @@ class Upgrade_20251113_Dotnet10 : CodeUpgradeBase
                 "<TargetFramework>net9.0</TargetFramework>",
                 "<TargetFramework>net10.0</TargetFramework> ");
 
-            file.UpdateNpmPackages("""
+            file.UpdateNugetReferences("""
+                <PackageReference Include="DeepL.net" Version="1.17.0" />
                 <PackageReference Include="Microsoft.Data.SqlClient" Version="6.1.3" />
                 <PackageReference Include="Microsoft.Extensions.Configuration" Version="10.0.0" />
                 <PackageReference Include="Microsoft.Extensions.Configuration.Binder" Version="10.0.0" />
                 <PackageReference Include="Microsoft.Extensions.Configuration.Json" Version="10.0.0" />
                 <PackageReference Include="Microsoft.Extensions.Configuration.UserSecrets" Version="10.0.0" />
+                <PackageReference Include="Microsoft.Extensions.Logging.Abstractions" Version="10.0.0" />
+                <PackageReference Include="Microsoft.Graph" Version="5.96.0" />
                 <PackageReference Include="Microsoft.NET.Test.Sdk" Version="18.0.1" />
-                <PackageReference Include="Selenium.WebDriver.ChromeDriver" Version="142.0.7444.16200" />
-                <PackageReference Include="Selenium.WebDriver" Version="4.38.0" />
                 <PackageReference Include="Selenium.Support" Version="4.38.0" />
-                <PackageReference Include="xunit.v3" Version="3.2.0" />
+                <PackageReference Include="Selenium.WebDriver" Version="4.38.0" />                
+                <PackageReference Include="Signum.TSGenerator" Version="10.0.1" />
+                <PackageReference Include="Signum.MSBuildTask" Version="10.0.0" />
+                <PackageReference Include="Selenium.WebDriver.ChromeDriver" Version="142.0.7444.16200" />
                 <PackageReference Include="Swashbuckle.AspNetCore" Version="10.0.1" />
                 <PackageReference Include="System.DirectoryServices" Version="10.0.0" />
+                <PackageReference Include="System.DirectoryServices" Version="10.0.0" />
                 <PackageReference Include="System.DirectoryServices.AccountManagement" Version="10.0.0" />
-                <PackageReference Include="Microsoft.Graph" Version="5.96.0" />
                 <PackageReference Include="System.Drawing.Common" Version="10.0.0" />
-                <PackageReference Include="DeepL.net" Version="1.17.0" />
-                <PackageReference Include="Microsoft.Extensions.Logging.Abstractions" Version="10.0.0" />
                 <PackageReference Include="System.Text.Encoding.CodePages" Version="10.0.0" />
-                """)
+                <PackageReference Include="xunit.v3" Version="3.2.0" />
+                """);
         });
     }
 }
