f3f55da3b99a8ab9bcd2dec2bf0d82da0169d068
Jafar Mirzaie <Jafar.Mirzaie@bechtle.com>
2025-12-12 12:12:01 +0100

    fix reseting selected Entity of Switcher

--- Extensions/Signum.Toolbar/Renderers/ToolbarRenderer.tsx
+++ Extensions/Signum.Toolbar/Renderers/ToolbarRenderer.tsx
@@ -374,12 +374,20 @@ function ToolbarMenuItemsEntityType(p: { response: ToolbarResponse<ToolbarMenuEn
 
   const entityType = p.response.entityType!;
   const selEntityRef = React.useRef<Lite<Entity> | null>(null);
+  const previousResponseRef = React.useRef<ToolbarResponse<ToolbarMenuEntity> | null>(null);
   const forceUpdate = useForceUpdate();
 
   function setSelectedEntity(entity: Lite<Entity> | null) {
     selEntityRef.current = entity;
     forceUpdate();
   }
+
+  React.useEffect(() => {
+    if (previousResponseRef.current && !is(previousResponseRef.current.content, p.response.content)) {
+      setSelectedEntity(null);
+    }
+    previousResponseRef.current = p.response;
+  }, [p.response]);
 
 
   const entities = useAPI(() => Finder.API.fetchAllLites({ types: entityType }), [entityType]);
@@ -394,12 +402,7 @@ function ToolbarMenuItemsEntityType(p: { response: ToolbarResponse<ToolbarMenuEn
       if (!is(active.menuWithEntity.entity, selEntityRef.current))
         setSelectedEntity(active.menuWithEntity.entity);
     }
-    //else if (active != null)
-    else if (active && !active.menuWithEntity)
-      setSelectedEntity(null);
-
   }, [active, p.response]);
-
   React.useEffect(() => {
 
     if (selEntityRef.current && !selEntityRef.current.model && entities) {
@@ -530,7 +533,6 @@ function ToolbarSwitcher(p: { response: ToolbarResponse<ToolbarSwitcherEntity>, 
       responseClick(autoSelect, null, undefined, p.ctx);
     }
   }
-
   React.useEffect(() => {
 
     if (p.ctx.active) {
