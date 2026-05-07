3d01ee80c01494b1d88608ef31f3a4fc0d254fd1
Olmo del Corral Cano <olmo.delcorralcano@bechtle.com>
2026-05-07 14:35:54 +0200

    D3Utils.ts

--- Extensions/Signum.Processes/ProcessClient.tsx
+++ Extensions/Signum.Processes/ProcessClient.tsx
@@ -21,7 +21,7 @@ import { ContextualItemsContext, ContextualMenuItem } from '../../Signum/React/S
 import { SearchControlLoaded } from '@framework/Search';
 import { Finder } from '@framework/Finder';
 import { DateTime } from 'luxon';
-import * as d3 from 'd3';
+import { buildDateScale } from '@framework/Basics/D3Utils';
 
 
 export namespace ProcessClient {
@@ -308,17 +308,3 @@ export interface ProcessDatesDTO {
   state: ProcessState;
 }
 
-export function buildDateScale(isoStrings: string[], max24Hours?: boolean): (d: string) => number {
-  const dates = isoStrings.map(d => new Date(d));
-  const minDate = d3.min(dates) ?? new Date();
-  const maxDate = new Date(Math.max((d3.max(dates) ?? new Date()).getTime(), minDate.getTime() + 1));
-
-  if (max24Hours && maxDate.getTime() - minDate.getTime() > 24 * 60 * 60 * 1000) {
-    const toTimeOfDay = (d: Date) => new Date(1970, 0, 1, d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds());
-    const scale = d3.scaleTime<number>().domain([new Date(1970, 0, 1, 0, 0, 0), new Date(1970, 0, 1, 23, 59, 0)]).range([0, 100]).clamp(true);
-    return d => scale(toTimeOfDay(new Date(d)));
-  }
-
-  const scale = d3.scaleTime<number>().domain([minDate, maxDate]).range([0, 100]).clamp(true);
-  return d => scale(new Date(d));
-}
